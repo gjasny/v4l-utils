b4861f09f37fcc18faf4e70fcfe00bdfb167c5b6
Mauro Carvalho Chehab <mchehab@infradead.org>
2007-07-12 06:16:17 -0300

    gen_keytables.pl is too verbose.
    From: Mauro Carvalho Chehab <mchehab@infradead.org>
    
    Make gen_keytables.pl less verbose
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@infradead.org>
    
    ---
     v4l2-apps/util/gen_keytables.pl |    4 +++-
     1 file changed, 3 insertions(+), 1 deletion(-)

--- utils/gen_keytables.pl
+++ utils/gen_keytables.pl
@@ -2,13 +2,15 @@
 use strict;
 
 my $keyname="";
+my $debug=0;
+
 while (<>) {
 #IR_KEYTAB_TYPE ir_codes_proteus_2309[IR_KEYTAB_SIZE] = {
 	if (m/IR_KEYTAB_TYPE\s+(\w[\w\d_]+)/) {
 		$keyname = $1;
 		$keyname =~ s/^ir_codes_//;
 
-		print "Generating keycodes/$keyname\n";
+		print "Generating keycodes/$keyname\n" if $debug;
 		open OUT, ">keycodes/$keyname";
 		next;
 	}
