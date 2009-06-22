8cb13d7eac90345022d68561212d67430998da72
hans@rhel5-devel.localdomain <hans@rhel5-devel.localdomain>
2009-06-22 10:15:35 +0200

    libv4l: update upside down devices table
    From: Hans de Goede <hdegoede@redhat.com>
    
    libv4l: update upside down devices table
    
    Priority: normal
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>

--- lib/libv4lconvert/control/libv4lcontrol.c
+++ lib/libv4lconvert/control/libv4lcontrol.c
@@ -74,7 +74,10 @@ static const struct v4lcontrol_flags_info v4lcontrol_flags[] = {
     V4LCONTROL_HFLIPPED|V4LCONTROL_VFLIPPED },
   { 0x174f, 0x5a35, 0, "ASUSTeK Computer Inc.        ", "F3Sr      ",
     V4LCONTROL_HFLIPPED|V4LCONTROL_VFLIPPED },
+  /* The G1S has been seen with 2 different board name strings */
   { 0x174f, 0x5a35, 0, "ASUSTeK Computer Inc.        ", "G1S                 ",
+    V4LCONTROL_HFLIPPED|V4LCONTROL_VFLIPPED },
+  { 0x174f, 0x5a35, 0, "ASUSTeK Computer Inc.        ", "G1S       ",
     V4LCONTROL_HFLIPPED|V4LCONTROL_VFLIPPED },
   { 0x174f, 0x5a35, 0, "ASUSTeK Computer Inc.        ", "G1Sn      ",
     V4LCONTROL_HFLIPPED|V4LCONTROL_VFLIPPED },
