27b5a13d531c7c25b54d421ff9538502bb22c5f4
Hans de Goede <hdegoede@redhat.com>
2009-09-30 10:05:38 +0200

    libv4l: update upside down devices table
    Priority: normal
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- lib/libv4lconvert/control/libv4lcontrol.c
+++ lib/libv4lconvert/control/libv4lcontrol.c
@@ -172,6 +172,8 @@ static const struct v4lcontrol_flags_info v4lcontrol_flags[] = {
     V4LCONTROL_HFLIPPED|V4LCONTROL_VFLIPPED },
   { 0x174f, 0xa311, 0, "ASUSTeK Computer Inc.        ", "A3F       ",
     V4LCONTROL_HFLIPPED|V4LCONTROL_VFLIPPED },
+  { 0x1d4d, 0x1002, 0, "ASUSTeK Computer Inc.        ", "B50A      ",
+    V4LCONTROL_HFLIPPED|V4LCONTROL_VFLIPPED },
   { 0x5986, 0x0200, 0, "LENOVO", "SPEEDY    ",
     V4LCONTROL_HFLIPPED|V4LCONTROL_VFLIPPED, 0, NULL, NULL, NULL,
     "Lenovo IdeaPad Y510" },
