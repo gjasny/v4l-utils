6c35983f3377c2b64d2da9b0948c27b6c817c4ea
Mauro Carvalho Chehab <mchehab@redhat.com>
2009-11-12 06:52:51 +0000

    decode_tm6000: fix include path
    From: Márton Németh <nm127@freemail.hu>
    
    The include path is changed from ../lib to ../lib4vl2util .
    
    Signed-off-by: Márton Németh <nm127@freemail.hu>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- utils/decode_tm6000.c
+++ utils/decode_tm6000.c
@@ -16,7 +16,7 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
-#include "../lib/v4l2_driver.h"
+#include "../libv4l2util/v4l2_driver.h"
 #include <stdio.h>
 #include <string.h>
 #include <argp.h>
