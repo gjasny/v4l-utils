a63ec45355c0f3de924fde2c9fe90403701f9e5f
Hans de Goede <hdegoede@redhat.com>
2010-01-27 11:30:06 +0100

    libv4l: update upsidedown device list
    Priority: normal
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- lib/libv4lconvert/control/libv4lcontrol.c
+++ lib/libv4lconvert/control/libv4lcontrol.c
@@ -157,7 +157,7 @@ static const struct v4lcontrol_flags_info v4lcontrol_flags[] = {
     V4LCONTROL_HFLIPPED|V4LCONTROL_VFLIPPED },
   { 0x04f2, 0xb071, 0, "PEGATRON CORP.", NULL,
     V4LCONTROL_HFLIPPED|V4LCONTROL_VFLIPPED, 0,
-    "Philco", "S.rie PHN10050" },
+    "Philco", "S\202rie PHN10050" },
   { 0x04f2, 0xb072, 0, "ASUSTeK Computer Inc.        ", "K50IJ     ",
     V4LCONTROL_HFLIPPED|V4LCONTROL_VFLIPPED },
   { 0x04f2, 0xb106, 0, "ASUSTeK Computer Inc.        ", "N50Vc      ",
