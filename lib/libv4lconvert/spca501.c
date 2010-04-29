a3e38d272dabef9e4337885bb2fe31d9b1116d75
Hans Verkuil <hverkuil@xs4all.nl>
2010-04-29 09:17:48 +0200

    spca501.c: run through checkpatch.pl
    Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>

--- lib/libv4lconvert/spca501.c
+++ lib/libv4lconvert/spca501.c
@@ -14,131 +14,133 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
+ */
 
 #include "libv4lconvert-priv.h"
 
 /* YUYV per line */
 void v4lconvert_spca501_to_yuv420(const unsigned char *src, unsigned char *dst,
-  int width, int height, int yvu)
+		int width, int height, int yvu)
 {
-  int i,j;
-  unsigned long *lsrc = (unsigned long *)src;
+	int i, j;
+	unsigned long *lsrc = (unsigned long *)src;
 
-  for (i = 0; i < height; i += 2) {
-    /* -128 - 127 --> 0 - 255 and copy first line Y */
-    unsigned long *ldst = (unsigned long *)(dst + i * width);
-    for (j = 0; j < width; j += sizeof(long)) {
-      *ldst = *lsrc++;
-      *ldst++ ^= 0x8080808080808080ULL;
-    }
+	for (i = 0; i < height; i += 2) {
+		/* -128 - 127 --> 0 - 255 and copy first line Y */
+		unsigned long *ldst = (unsigned long *)(dst + i * width);
 
-    /* -128 - 127 --> 0 - 255 and copy 1 line U */
-    if (yvu)
-      ldst = (unsigned long *)(dst + (width * height * 5) / 4 + i * width / 4);
-    else
-      ldst = (unsigned long *)(dst + width * height + i * width / 4);
-    for (j = 0; j < width/2; j += sizeof(long)) {
-      *ldst = *lsrc++;
-      *ldst++ ^= 0x8080808080808080ULL;
-    }
+		for (j = 0; j < width; j += sizeof(long)) {
+			*ldst = *lsrc++;
+			*ldst++ ^= 0x8080808080808080ULL;
+		}
 
-    /* -128 - 127 --> 0 - 255 and copy second line Y */
-    ldst = (unsigned long *)(dst + i * width + width);
-    for (j = 0; j < width; j += sizeof(long)) {
-      *ldst = *lsrc++;
-      *ldst++ ^= 0x8080808080808080ULL;
-    }
+		/* -128 - 127 --> 0 - 255 and copy 1 line U */
+		if (yvu)
+			ldst = (unsigned long *)(dst + (width * height * 5) / 4 + i * width / 4);
+		else
+			ldst = (unsigned long *)(dst + width * height + i * width / 4);
+		for (j = 0; j < width/2; j += sizeof(long)) {
+			*ldst = *lsrc++;
+			*ldst++ ^= 0x8080808080808080ULL;
+		}
 
-    /* -128 - 127 --> 0 - 255 and copy 1 line V */
-    if (yvu)
-      ldst = (unsigned long *)(dst + width * height + i * width / 4);
-    else
-      ldst = (unsigned long *)(dst + (width * height * 5) / 4 + i * width / 4);
-    for (j = 0; j < width/2; j += sizeof(long)) {
-      *ldst = *lsrc++;
-      *ldst++ ^= 0x8080808080808080ULL;
-    }
-  }
+		/* -128 - 127 --> 0 - 255 and copy second line Y */
+		ldst = (unsigned long *)(dst + i * width + width);
+		for (j = 0; j < width; j += sizeof(long)) {
+			*ldst = *lsrc++;
+			*ldst++ ^= 0x8080808080808080ULL;
+		}
+
+		/* -128 - 127 --> 0 - 255 and copy 1 line V */
+		if (yvu)
+			ldst = (unsigned long *)(dst + width * height + i * width / 4);
+		else
+			ldst = (unsigned long *)(dst + (width * height * 5) / 4 + i * width / 4);
+		for (j = 0; j < width/2; j += sizeof(long)) {
+			*ldst = *lsrc++;
+			*ldst++ ^= 0x8080808080808080ULL;
+		}
+	}
 }
 
 /* YYUV per line */
 void v4lconvert_spca505_to_yuv420(const unsigned char *src, unsigned char *dst,
-  int width, int height, int yvu)
+		int width, int height, int yvu)
 {
-  int i,j;
-  unsigned long *lsrc = (unsigned long *)src;
+	int i, j;
+	unsigned long *lsrc = (unsigned long *)src;
 
-  for (i = 0; i < height; i += 2) {
-    /* -128 - 127 --> 0 - 255 and copy 2 lines of Y */
-    unsigned long *ldst = (unsigned long *)(dst + i * width);
-    for (j = 0; j < width*2; j += sizeof(long)) {
-      *ldst = *lsrc++;
-      *ldst++ ^= 0x8080808080808080ULL;
-    }
+	for (i = 0; i < height; i += 2) {
+		/* -128 - 127 --> 0 - 255 and copy 2 lines of Y */
+		unsigned long *ldst = (unsigned long *)(dst + i * width);
 
-    /* -128 - 127 --> 0 - 255 and copy 1 line U */
-    if (yvu)
-      ldst = (unsigned long *)(dst + (width * height * 5) / 4 + i * width / 4);
-    else
-      ldst = (unsigned long *)(dst + width * height + i * width / 4);
-    for (j = 0; j < width/2; j += sizeof(long)) {
-      *ldst = *lsrc++;
-      *ldst++ ^= 0x8080808080808080ULL;
-    }
+		for (j = 0; j < width*2; j += sizeof(long)) {
+			*ldst = *lsrc++;
+			*ldst++ ^= 0x8080808080808080ULL;
+		}
 
-    /* -128 - 127 --> 0 - 255 and copy 1 line V */
-    if (yvu)
-      ldst = (unsigned long *)(dst + width * height + i * width / 4);
-    else
-      ldst = (unsigned long *)(dst + (width * height * 5) / 4 + i * width / 4);
-    for (j = 0; j < width/2; j += sizeof(long)) {
-      *ldst = *lsrc++;
-      *ldst++ ^= 0x8080808080808080ULL;
-    }
-  }
+		/* -128 - 127 --> 0 - 255 and copy 1 line U */
+		if (yvu)
+			ldst = (unsigned long *)(dst + (width * height * 5) / 4 + i * width / 4);
+		else
+			ldst = (unsigned long *)(dst + width * height + i * width / 4);
+		for (j = 0; j < width/2; j += sizeof(long)) {
+			*ldst = *lsrc++;
+			*ldst++ ^= 0x8080808080808080ULL;
+		}
+
+		/* -128 - 127 --> 0 - 255 and copy 1 line V */
+		if (yvu)
+			ldst = (unsigned long *)(dst + width * height + i * width / 4);
+		else
+			ldst = (unsigned long *)(dst + (width * height * 5) / 4 + i * width / 4);
+		for (j = 0; j < width/2; j += sizeof(long)) {
+			*ldst = *lsrc++;
+			*ldst++ ^= 0x8080808080808080ULL;
+		}
+	}
 }
 
 /* YUVY per line */
 void v4lconvert_spca508_to_yuv420(const unsigned char *src, unsigned char *dst,
-  int width, int height, int yvu)
+		int width, int height, int yvu)
 {
-  int i,j;
-  unsigned long *lsrc = (unsigned long *)src;
+	int i, j;
+	unsigned long *lsrc = (unsigned long *)src;
 
-  for (i = 0; i < height; i += 2) {
-    /* -128 - 127 --> 0 - 255 and copy first line Y */
-    unsigned long *ldst = (unsigned long *)(dst + i * width);
-    for (j = 0; j < width; j += sizeof(long)) {
-      *ldst = *lsrc++;
-      *ldst++ ^= 0x8080808080808080ULL;
-    }
+	for (i = 0; i < height; i += 2) {
+		/* -128 - 127 --> 0 - 255 and copy first line Y */
+		unsigned long *ldst = (unsigned long *)(dst + i * width);
+		for (j = 0; j < width; j += sizeof(long)) {
+			*ldst = *lsrc++;
+			*ldst++ ^= 0x8080808080808080ULL;
+		}
 
-    /* -128 - 127 --> 0 - 255 and copy 1 line U */
-    if (yvu)
-      ldst = (unsigned long *)(dst + (width * height * 5) / 4 + i * width / 4);
-    else
-      ldst = (unsigned long *)(dst + width * height + i * width / 4);
-    for (j = 0; j < width/2; j += sizeof(long)) {
-      *ldst = *lsrc++;
-      *ldst++ ^= 0x8080808080808080ULL;
-    }
+		/* -128 - 127 --> 0 - 255 and copy 1 line U */
+		if (yvu)
+			ldst = (unsigned long *)(dst + (width * height * 5) / 4 + i * width / 4);
+		else
+			ldst = (unsigned long *)(dst + width * height + i * width / 4);
+		for (j = 0; j < width/2; j += sizeof(long)) {
+			*ldst = *lsrc++;
+			*ldst++ ^= 0x8080808080808080ULL;
+		}
 
-    /* -128 - 127 --> 0 - 255 and copy 1 line V */
-    if (yvu)
-      ldst = (unsigned long *)(dst + width * height + i * width / 4);
-    else
-      ldst = (unsigned long *)(dst + (width * height * 5) / 4 + i * width / 4);
-    for (j = 0; j < width/2; j += sizeof(long)) {
-      *ldst = *lsrc++;
-      *ldst++ ^= 0x8080808080808080ULL;
-    }
+		/* -128 - 127 --> 0 - 255 and copy 1 line V */
+		if (yvu)
+			ldst = (unsigned long *)(dst + width * height + i * width / 4);
+		else
+			ldst = (unsigned long *)(dst + (width * height * 5) / 4 + i * width / 4);
+		for (j = 0; j < width/2; j += sizeof(long)) {
+			*ldst = *lsrc++;
+			*ldst++ ^= 0x8080808080808080ULL;
+		}
 
-    /* -128 - 127 --> 0 - 255 and copy second line Y */
-    ldst = (unsigned long *)(dst + i * width + width);
-    for (j = 0; j < width; j += sizeof(long)) {
-      *ldst = *lsrc++;
-      *ldst++ ^= 0x8080808080808080ULL;
-    }
-  }
+		/* -128 - 127 --> 0 - 255 and copy second line Y */
+		ldst = (unsigned long *)(dst + i * width + width);
+		for (j = 0; j < width; j += sizeof(long)) {
+			*ldst = *lsrc++;
+			*ldst++ ^= 0x8080808080808080ULL;
+		}
+	}
 }
