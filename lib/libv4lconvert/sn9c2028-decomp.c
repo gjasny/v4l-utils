a821c9eacf0e70365cc9280a741e5f2b22032323
Hans Verkuil <hverkuil@xs4all.nl>
2010-04-29 09:11:04 +0200

    sn9c2028-decomp.c: run through checkpatch.pl
    Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>

--- lib/libv4lconvert/sn9c2028-decomp.c
+++ lib/libv4lconvert/sn9c2028-decomp.c
@@ -40,16 +40,14 @@
 
 /* Four defines for bitstream operations, used in the decode function */
 
-#define PEEK_BITS(num,to) {\
-    if (bitBufCount < num) {\
-	do {\
-	    bitBuf = (bitBuf << 8)|(*(src++));\
-	    bitBufCount += 8; \
+#define PEEK_BITS(num, to) { \
+	if (bitBufCount < num) { \
+		do { \
+			bitBuf = (bitBuf << 8) | (*(src++)); \
+			bitBufCount += 8; \
+		} while (bitBufCount < 24); \
 	} \
-	while\
-	    (bitBufCount < 24);\
-    } \
-    to = bitBuf >> (bitBufCount-num);\
+	to = bitBuf >> (bitBufCount - num); \
 }
 
 /*
@@ -67,91 +65,91 @@
  */
 
 #define PARSE_PIXEL(val) {\
-    PEEK_BITS(10, bits);\
-    if ((bits&0x200) == 0) {\
-	EAT_BITS(1);\
-    } \
-    else if ((bits&0x380) == 0x280) {\
-	EAT_BITS(3);\
-	val += 3;\
-	if (val > 255)\
-	    val = 255;\
-    } \
-    else if ((bits&0x380) == 0x300) {\
-	EAT_BITS(3);\
-	val -= 3;\
-	if (val < 0)\
-	    val = 0;\
-    } \
-    else if ((bits&0x3c0) == 0x200) {\
-	EAT_BITS(4);\
-	val += 8;\
-	if (val > 255)\
-	    val = 255;\
-    } \
-    else if ((bits&0x3c0) == 0x240) {\
-	EAT_BITS(4);\
-	val -= 8;\
-	if (val < 0)\
-	    val = 0;\
-    } \
-    else if ((bits&0x3c0) == 0x3c0) {\
-	EAT_BITS(4);\
-	val -= 20;\
-	if (val < 0)\
-	    val = 0;\
-    } \
-    else if ((bits&0x3e0) == 0x380) {\
-	EAT_BITS(5);\
-	val += 20;\
-	if (val > 255)\
-	    val = 255;\
-    } \
-    else {\
-	EAT_BITS(10);\
-	val = 8*(bits&0x1f)+0;\
-    } \
+	PEEK_BITS(10, bits);\
+	if ((bits & 0x200) == 0) {\
+		EAT_BITS(1);\
+	} \
+	else if ((bits & 0x380) == 0x280) {\
+		EAT_BITS(3);\
+		val += 3;\
+		if (val > 255)\
+			val = 255;\
+	} \
+	else if ((bits & 0x380) == 0x300) {\
+		EAT_BITS(3);\
+		val -= 3;\
+		if (val < 0)\
+			val = 0;\
+	} \
+	else if ((bits & 0x3c0) == 0x200) {\
+		EAT_BITS(4);\
+		val += 8;\
+		if (val > 255)\
+			val = 255;\
+	} \
+	else if ((bits & 0x3c0) == 0x240) {\
+		EAT_BITS(4);\
+		val -= 8;\
+		if (val < 0)\
+			val = 0;\
+	} \
+	else if ((bits & 0x3c0) == 0x3c0) {\
+		EAT_BITS(4);\
+		val -= 20;\
+		if (val < 0)\
+			val = 0;\
+	} \
+	else if ((bits & 0x3e0) == 0x380) {\
+		EAT_BITS(5);\
+		val += 20;\
+		if (val > 255)\
+			val = 255;\
+	} \
+	else {\
+		EAT_BITS(10);\
+		val = 8 * (bits & 0x1f);\
+	} \
 }
 
 
 #define PUT_PIXEL_PAIR {\
-    long pp;\
-    pp = (c1val<<8)+c2val;\
-    *((unsigned short *) (dst+dst_index)) = pp;\
-    dst_index += 2;\
+	long pp;\
+	pp = (c1val << 8) + c2val;\
+	*((unsigned short *)(dst + dst_index)) = pp;\
+	dst_index += 2;\
 }
 
 /* Now the decode function itself */
 
 void v4lconvert_decode_sn9c2028(const unsigned char *src, unsigned char *dst,
-  int width, int height)
+		int width, int height)
 {
-    long dst_index = 0;
-    int starting_row = 0;
-    unsigned short bits;
-    short c1val, c2val;
-    int x, y;
-    unsigned long bitBuf = 0;
-    unsigned long bitBufCount = 0;
-    unsigned long bits_eaten = 0;
+	long dst_index = 0;
+	int starting_row = 0;
+	unsigned short bits;
+	short c1val, c2val;
+	int x, y;
+	unsigned long bitBuf = 0;
+	unsigned long bitBufCount = 0;
+	unsigned long bits_eaten = 0;
 
-    src += 12;    /* Remove the header */
+	src += 12;    /* Remove the header */
 
-    for (y = starting_row; y < height; y++) {
-	PEEK_BITS(8, bits);
-	EAT_BITS(8);
-	c2val = (bits & 0xff);
-	PEEK_BITS(8, bits);
-	EAT_BITS(8);
-	c1val = (bits & 0xff);
+	for (y = starting_row; y < height; y++) {
+		PEEK_BITS(8, bits);
+		EAT_BITS(8);
+		c2val = (bits & 0xff);
+		PEEK_BITS(8, bits);
+		EAT_BITS(8);
+		c1val = (bits & 0xff);
 
-	PUT_PIXEL_PAIR;
+		PUT_PIXEL_PAIR;
 
-	for (x = 2; x < width ; x += 2) {
-	    /* The compression reversed the even and odd columns.*/
-	    PARSE_PIXEL(c2val);
-	    PARSE_PIXEL(c1val);
-	    PUT_PIXEL_PAIR;
+		for (x = 2; x < width ; x += 2) {
+			/* The compression reversed the even and odd columns.*/
+			PARSE_PIXEL(c2val);
+			PARSE_PIXEL(c1val);
+			PUT_PIXEL_PAIR;
+		}
 	}
-    }
 }
