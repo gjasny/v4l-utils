85d6515f01daeac61c7fb6f6cde3ed311b7c678d
Hans Verkuil <hverkuil@xs4all.nl>
2010-04-29 14:29:33 +0200

    stv0680.c: run through checkpatch.pl
    Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>

--- lib/libv4lconvert/stv0680.c
+++ lib/libv4lconvert/stv0680.c
@@ -24,18 +24,18 @@
    and then all the green/blue pixels in that line, shuffle this to a regular
    RGGB bayer pattern. */
 void v4lconvert_decode_stv0680(const unsigned char *src, unsigned char *dst,
-  int width, int height)
+		int width, int height)
 {
-  int x, y;
-  const unsigned char *src1 = src;
-  const unsigned char *src2 = src + width / 2;
+	int x, y;
+	const unsigned char *src1 = src;
+	const unsigned char *src2 = src + width / 2;
 
-  for (y = 0; y < height; y++) {
-    for (x = 0; x < width / 2; x++) {
-      *dst++ = *src1++;
-      *dst++ = *src2++;
-    }
-    src1 += width / 2;
-    src2 += width / 2;
-  }
+	for (y = 0; y < height; y++) {
+		for (x = 0; x < width / 2; x++) {
+			*dst++ = *src1++;
+			*dst++ = *src2++;
+		}
+		src1 += width / 2;
+		src2 += width / 2;
+	}
 }
