60037fa2d2030d26cabc3c5349ce957ddab9d9a0
Hans de Goede <hdegoede@redhat.com>
2010-07-22 14:43:54 +0200

    libv4l: update upside-down device table
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>

--- lib/libv4lconvert/control/libv4lcontrol.c
+++ lib/libv4lconvert/control/libv4lcontrol.c
@@ -207,6 +207,8 @@ static const struct v4lcontrol_flags_info v4lcontrol_flags[] = {
 		V4LCONTROL_HFLIPPED | V4LCONTROL_VFLIPPED },
 	{ 0x04f2, 0xb1bb, 0, "ASUSTeK Computer Inc.        ", "N82JV",
 		V4LCONTROL_HFLIPPED | V4LCONTROL_VFLIPPED },
+	{ 0x04f2, 0xb1be, 0, "ASUSTeK Computer Inc.        ", "UL30JT",
+		V4LCONTROL_HFLIPPED | V4LCONTROL_VFLIPPED },
 	/* Note no whitespace padding for board vendor, this is not a typo */
 	{ 0x064e, 0xa111, 0, "ASUSTeK Computer Inc.", "F5RL      ",
 		V4LCONTROL_HFLIPPED | V4LCONTROL_VFLIPPED },
