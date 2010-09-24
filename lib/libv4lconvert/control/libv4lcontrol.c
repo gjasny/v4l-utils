5dd23169efbfe66a5364f03d84b3defc945b3223
Hans de Goede <hdegoede@redhat.com>
2010-09-24 15:08:51 +0200

    libv4l: update upside down table
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>

--- lib/libv4lconvert/control/libv4lcontrol.c
+++ lib/libv4lconvert/control/libv4lcontrol.c
@@ -342,6 +342,8 @@ static const struct v4lcontrol_flags_info v4lcontrol_flags[] = {
 		V4LCONTROL_HFLIPPED | V4LCONTROL_VFLIPPED },
 	{ 0x13d3, 0x5130, 0, "ASUSTeK Computer INC.", "K40AE",
 		V4LCONTROL_HFLIPPED | V4LCONTROL_VFLIPPED },
+	{ 0x13d3, 0x5130, 0, "ASUSTeK Computer INC.", "K40AF",
+		V4LCONTROL_HFLIPPED | V4LCONTROL_VFLIPPED },
 	{ 0x13d3, 0x5130, 0, "ASUSTeK Computer INC.", "K50AF",
 		V4LCONTROL_HFLIPPED | V4LCONTROL_VFLIPPED },
 	{ 0x13d3, 0x5130, 0, "ASUSTeK Computer Inc.        ", "K50ID     ",
