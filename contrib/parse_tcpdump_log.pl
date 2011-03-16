f0e0f759c2bd4cdd26033061d61bffe5a106215a
Mauro Carvalho Chehab <mchehab@redhat.com>
2011-03-16 15:03:08 -0300

    parse_tcpdump_log.pl: Fix most warnings and cleanup the code
    There were several warnings happening with "use warnings".
    Fix most of them. Also, use unpack to retrieve data from
    the captured info, and remove some code redundancy.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- contrib/parse_tcpdump_log.pl
+++ contrib/parse_tcpdump_log.pl
@@ -313,8 +313,8 @@ my $last_time;
 
 sub print_frame($$)
 {
-	my %req = %{ @_[0] };
-	my %resp = %{ @_[1] };
+	my %req = %{ $_[0] };
+	my %resp = %{ $_[1] };
 
 	if (!$initial_time) {
 		$initial_time = $req{"Time"};
@@ -348,13 +348,13 @@ sub print_frame($$)
 		printf " >>>";
 	}
 
-	my $app_data = substr($req{"Payload"}, 24 * 2);
+	$app_data = substr($req{"Payload"}, 24 * 2);
 	while ($app_data ne "") {
 		printf " %s", substr($app_data, 0, 2);
 		$app_data = substr($app_data, 2);
 	}
 
-	my $app_data = substr($resp{"Payload"}, 24 * 2);
+	$app_data = substr($resp{"Payload"}, 24 * 2);
 	while ($app_data ne "") {
 		printf " %s", substr($app_data, 0, 2);
 		$app_data = substr($app_data, 2);
@@ -375,7 +375,7 @@ sub print_frame($$)
 }
 
 sub process_frame($) {
-	my %frame = %{ @_[0] };
+	my %frame = %{ $_[0] };
 
 	if ($debug & 2) {
 		my ($key, $value);
@@ -395,11 +395,11 @@ sub process_frame($) {
 	# Seek for operation origin
 	my $related = $frame{"ID"};
 	if (!$related) {
-		print "URB %d incomplete\n", $frame{"ID"};
+		print "URB %s incomplete\n", $frame{"ID"};
 		return;
 	}
 	for (my $i = 0; $i < scalar(@pending); $i++) {
-		if ($related == $pending[$i]{"ID"}) {
+		if ($related eq $pending[$i]{"ID"}) {
 			my %req = %{$pending[$i]};
 
 			print_frame (\%req, \%frame);
@@ -409,189 +409,72 @@ sub process_frame($) {
 			return;
 		}
 	}
-	printf "URB %d incomplete: Couldn't find related URB %d\n", $related;
+	printf "URB %s incomplete: Couldn't find related URB\n", $related;
 	return;
 }
 
-sub parse_file
-{
-	my $log = shift;
-	my $fh = shift;
 
-	while ($log->get_packet($fh) == 0) {
-		my %frame;
-		my ($length_orig,$length_incl,$drops,$secs,$msecs,$more,$strdata) = $log->packet();
-		$frame{"Time"} = sprintf "%d.%06d", $secs,$msecs;
-
-		my @data=unpack('C*', $strdata);
-
-		if ($debug & 4) {
-			print "RAW DATA: ";
-			for (my $i = 0; $i < scalar(@data); $i++) {
-				printf " %02x", $data[$i];
-			}
-			print "\n";
-		}
-
-	#typedef struct _usb_header_mmapped {
-	#	u_int64_t id;
-	#	u_int8_t event_type;
-	#	u_int8_t transfer_type;
-	#	u_int8_t endpoint_number;
-	#	u_int8_t device_address;
-	#	u_int16_t bus_id;
-	#	char setup_flag;/*if !=0 the urb setup header is not present*/
-	#	char data_flag; /*if !=0 no urb data is present*/
-	#	int64_t ts_sec;
-	#	int32_t ts_usec;
-	#	int32_t status;
-	#	u_int32_t urb_len;
-	#	u_int32_t data_len; /* amount of urb data really present in this event*/
-	#	union {
-	#		pcap_usb_setup setup;
-	#		iso_rec iso;
-	#	} s;
-	#	int32_t	interval;	/* for Interrupt and Isochronous events */
-	#	int32_t start_frame;	/* for Isochronous events */
-	#	u_int32_t xfer_flags;	/* copy of URB's transfer flags */
-	#	u_int32_t ndesc;	/* number of isochronous descriptors */
-	#} pcap_usb_header_mmapped;
-
-		$frame{"ID"} = sprintf("0x%02x%02x%02x%02x%02x%02x%02x%02x",
-					$data[7], $data[6], $data[5], $data[4],
-					$data[3], $data[2], $data[1], $data[0]);
-		$frame{"Type"} = chr($data[8]);
-		$frame{"TransferType"} = $data[9];
-		$frame{"Endpoint"} = $data[10];
-		$frame{"Device"} = $data[11];
-		$frame{"BusID"} = $data[12] | $data[13] << 8;
-		if ($data[14] == 0) {
-			$frame{"SetupRequest"} = "present";
-		} else {
-			$frame{"SetupRequest"} = "not present";
-		}
-		if ($data[15] == 0) {
-			$frame{"HasData"} = "present";
-		} else {
-			$frame{"HasData"} = "not present";
-		}
-		# Not sure if this would work on 32-bits machines
-		my $tsSec = $data[16]       | $data[17] << 8 |
-			$data[18] << 16 | $data[19] << 24 |
-			$data[20] << 32 | $data[21] << 40 |
-			$data[22] << 48 | $data[23] << 56;
-		my $tsUsec = $data[24]       | $data[25] << 8 |
-			$data[26] << 16 | $data[27] << 24;
-		$frame{"ArrivalTime"} = sprintf "%d.%06d", $tsSec,$tsUsec;
-
-		# Status is signed with 32 bits. Fix signal, as errors are negative
-		$frame{"Status"} = $data[28]       | $data[29] << 8 |
-				$data[30] << 16 | $data[31] << 24;
-		$frame{"Status"} = $frame{"Status"} - 0x100000000 if ($frame{"Status"} & 0x80000000);
-
-		$frame{"URBLength"} = $data[32]       | $data[33] << 8 |
-				$data[34] << 16 | $data[35] << 24;
-		$frame{"DataLength"} = $data[36]       | $data[37] << 8 |
-				$data[38] << 16 | $data[39] << 24;
-
-		my $payload;
-		my $payload_size;
-		for (my $i = 40; $i < scalar(@data); $i++) {
-			$payload .= sprintf "%02x", $data[$i];
-			$payload_size++;
-		}
-#		$frame{"More"} = $more;
-		$frame{"Payload"} = $payload;
-		$frame{"PayloadSize"} = $payload_size;
-
-		process_frame(\%frame);
-	}
-}
-
-my $caplog;
-sub handle_pcap_packet($$$)
-{
-	my $user_data = @_[0];
-	my %hdr = %{ @_[1] };
-	my $strdata = @_[2];
-
+# Decode an USB header mapped frame. The frame is defined at libpcap as:
+#
+#typedef struct _usb_header_mmapped {
+#	u_int64_t id;
+#	u_int8_t event_type;
+#	u_int8_t transfer_type;
+#	u_int8_t endpoint_number;
+#	u_int8_t device_address;
+#	u_int16_t bus_id;
+#	char setup_flag;/*if !=0 the urb setup header is not present*/
+#	char data_flag; /*if !=0 no urb data is present*/
+#	int64_t ts_sec;
+#	int32_t ts_usec;
+#	int32_t status;
+#	u_int32_t urb_len;
+#	u_int32_t data_len; /* amount of urb data really present in this event*/
+#	union {
+#		pcap_usb_setup setup;
+#		iso_rec iso;
+#	} s;
+#	int32_t	interval;	/* for Interrupt and Isochronous events */
+#	int32_t start_frame;	/* for Isochronous events */
+#	u_int32_t xfer_flags;	/* copy of URB's transfer flags */
+#	u_int32_t ndesc;	/* number of isochronous descriptors */
+#} pcap_usb_header_mmapped;
+sub decode_frame($) {
+	my $strdata = shift;
 	my %frame;
 
-#	my ($length_orig,$length_incl,$drops,$secs,$msecs,$more,$strdata) = $caplog->packet();
-	$frame{"Time"} = sprintf "%d.%06d", $hdr{tv_sec}, $hdr{tv_usec};
-
 	my @data=unpack('C*', "$strdata");
-
 	if ($debug & 4) {
-		printf "RAW DATA (len %d, cap len %d, %d.%06d): ",
-			$hdr{len}, $hdr{caplen}, $hdr{tv_sec}, $hdr{tv_usec};
+		print "RAW DATA: ";
 		for (my $i = 0; $i < scalar(@data); $i++) {
 			printf " %02x", $data[$i];
 		}
 		print "\n";
 	}
 
-	#typedef struct _usb_header_mmapped {
-	#	u_int64_t id;
-	#	u_int8_t event_type;
-	#	u_int8_t transfer_type;
-	#	u_int8_t endpoint_number;
-	#	u_int8_t device_address;
-	#	u_int16_t bus_id;
-	#	char setup_flag;/*if !=0 the urb setup header is not present*/
-	#	char data_flag; /*if !=0 no urb data is present*/
-	#	int64_t ts_sec;
-	#	int32_t ts_usec;
-	#	int32_t status;
-	#	u_int32_t urb_len;
-	#	u_int32_t data_len; /* amount of urb data really present in this event*/
-	#	union {
-	#		pcap_usb_setup setup;
-	#		iso_rec iso;
-	#	} s;
-	#	int32_t	interval;	/* for Interrupt and Isochronous events */
-	#	int32_t start_frame;	/* for Isochronous events */
-	#	u_int32_t xfer_flags;	/* copy of URB's transfer flags */
-	#	u_int32_t ndesc;	/* number of isochronous descriptors */
-	#} pcap_usb_header_mmapped;
+	my ($frame_id, $setuprequest, $hasdata, $tsSecHigh, $tsSecLow, $tsUsec);
 
-	$frame{"ID"} = sprintf("0x%02x%02x%02x%02x%02x%02x%02x%02x",
-				$data[7], $data[6], $data[5], $data[4],
-				$data[3], $data[2], $data[1], $data[0]);
-	$frame{"Type"} = chr($data[8]);
-	$frame{"TransferType"} = $data[9];
-	$frame{"Endpoint"} = $data[10];
-	$frame{"Device"} = $data[11];
-	$frame{"BusID"} = $data[12] | $data[13] << 8;
-	if ($data[14] == 0) {
+	($frame_id, $frame{"Type"}, $frame{"TransferType"},
+		$frame{"Endpoint"}, $frame{"Device"}, $frame{"BusID"},
+		$setuprequest, $hasdata, $tsSecHigh, $tsSecLow, $tsUsec,
+		$frame{"Status"}, $frame{"URBLength"},
+		$frame{"DataLength"}) = unpack("A8CCCCvCCVVVlVV", $strdata);
+	$frame{"ID"} = "0x";
+	for (my $i = 7; $i >= 0; $i--) {
+		$frame{"ID"} .= sprintf "%02x", ord(substr($frame_id, $i, 1));
+	}
+	if ($setuprequest == 0) {
 		$frame{"SetupRequest"} = "present";
 	} else {
 		$frame{"SetupRequest"} = "not present";
 	}
-	if ($data[15] == 0) {
+	if ($hasdata == 0) {
 		$frame{"HasData"} = "present";
 	} else {
 		$frame{"HasData"} = "not present";
 	}
-	# Not sure if this would work on 32-bits machines
-	my $tsSec = $data[16]       | $data[17] << 8 |
-		$data[18] << 16 | $data[19] << 24 |
-		$data[20] << 32 | $data[21] << 40 |
-		$data[22] << 48 | $data[23] << 56;
-	my $tsUsec = $data[24]       | $data[25] << 8 |
-		$data[26] << 16 | $data[27] << 24;
-	$frame{"ArrivalTime"} = sprintf "%d.%06d", $tsSec,$tsUsec;
-
-	# Status is signed with 32 bits. Fix signal, as errors are negative
-	$frame{"Status"} = $data[28]       | $data[29] << 8 |
-			$data[30] << 16 | $data[31] << 24;
-	$frame{"Status"} = $frame{"Status"} - 0x100000000 if ($frame{"Status"} & 0x80000000);
-
-	$frame{"URBLength"} = $data[32]       | $data[33] << 8 |
-			$data[34] << 16 | $data[35] << 24;
-	$frame{"DataLength"} = $data[36]       | $data[37] << 8 |
-			$data[38] << 16 | $data[39] << 24;
-#	$frame{"More"} = $more;
+	$frame{"Type"} = chr($frame{"Type"});
+	$frame{"ArrivalTime"} = sprintf "%d.%06d", $tsSecHigh << 32 | $tsSecLow, $tsUsec;
 
 	my $payload;
 	my $payload_size;
@@ -599,9 +482,34 @@ sub handle_pcap_packet($$$)
 		$payload .= sprintf "%02x", $data[$i];
 		$payload_size++;
 	}
+#		$frame{"More"} = $more;
 	$frame{"Payload"} = $payload;
 	$frame{"PayloadSize"} = $payload_size;
 
+	return %frame;
+}
+
+sub parse_file($$)
+{
+	my $log = shift;
+	my $fh = shift;
+
+	while ($log->get_packet($fh) == 0) {
+		my ($length_orig,$length_incl,$drops,$secs,$msecs,$more,$strdata) = $log->packet();
+		my %frame = decode_frame($strdata);
+		$frame{"Time"} = sprintf "%d.%06d", $secs,$msecs;
+		process_frame(\%frame);
+	}
+}
+
+sub handle_pcap_packet($$$)
+{
+	my $user_data = $_[0];
+	my %hdr = %{ $_[1] };
+	my $strdata = $_[2];
+
+	my %frame = decode_frame($strdata);
+	$frame{"Time"} = sprintf "%d.%06d", $hdr{tv_sec}, $hdr{tv_usec};
 	process_frame(\%frame);
 }
 
