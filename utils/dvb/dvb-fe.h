d4e442c65d17735518c40b45eb44890ebc875ff6
Mauro Carvalho Chehab <mchehab@redhat.com>
2011-10-09 08:26:42 -0300

    utils/dvb: Add some comments about the API and what is missing there
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- utils/dvb/dvb-fe.h
+++ utils/dvb/dvb-fe.h
@@ -17,6 +17,12 @@
 
 #define MAX_DELIVERY_SYSTEMS	20
 
+/*
+ * Implement the stats properties as if they were defined via S2API style.
+ * This simplifies the addition of newer stats, and helps to port the
+ * code to v5 style, if such change gets merged upstream.
+ */
+
 #define DTV_MAX_STATS 5
 #define DTV_STATUS			(DTV_MAX_COMMAND + 100)
 #define DTV_BER				(DTV_MAX_COMMAND + 101)
@@ -43,9 +49,13 @@ struct dvb_v5_fe_parms {
 	struct dvb_v5_stats		stats;
 };
 
+/* Open/close methods */
+
 struct dvb_v5_fe_parms *dvb_fe_open(int adapter, int frontend,
 				    unsigned verbose);
 void dvb_fe_close(struct dvb_v5_fe_parms *parms);
+
+/* Get/set delivery system parameters */
 
 int dvb_fe_retrieve_parm(struct dvb_v5_fe_parms *parms,
 			unsigned cmd, uint32_t *value);
@@ -56,13 +66,40 @@ int dvb_set_sys(struct dvb_v5_fe_parms *parms,
 void dvb_fe_prt_parms(struct dvb_v5_fe_parms *parms);
 int dvb_fe_set_parms(struct dvb_v5_fe_parms *parms);
 
+/* Get statistics */
+
 int dvb_fe_retrieve_stats(struct dvb_v5_fe_parms *parms,
 			   unsigned cmd, uint32_t *value);
 int dvb_fe_store_stats(struct dvb_v5_fe_parms *parms,
 			unsigned cmd, uint32_t value);
 int dvb_fe_get_stats(struct dvb_v5_fe_parms *parms);
 
+/* Get both status statistics and dvb parameters */
+
 int dvb_fe_get_event(struct dvb_v5_fe_parms *parms);
+
+/* Other functions, associated to SEC/LNB/DISEqC */
+
+/*
+ * NOTE: It currently lacks support for two ioctl's:
+ * FE_DISEQC_RESET_OVERLOAD	used only on av7110.
+ * Spec says:
+ *   If the bus has been automatically powered off due to power overload,
+ *   this ioctl call restores the power to the bus. The call requires read/write
+ *   access to the device. This call has no effect if the device is manually
+ *   powered off. Not all DVB adapters support this ioctl.
+ *
+ * FE_DISHNETWORK_SEND_LEGACY_CMD is used on av7110, budget, gp8psk and stv0299
+ * Spec says:
+ *   WARNING: This is a very obscure legacy command, used only at stv0299
+ *   driver. Should not be used on newer drivers.
+ *   It provides a non-standard method for selecting Diseqc voltage on the
+ *   frontend, for Dish Network legacy switches.
+ *   As support for this ioctl were added in 2004, this means that such dishes
+ *   were already legacy in 2004.
+ *
+ * So, it doesn't make much sense on implementing support for them.
+ */
 
 int dvb_fe_sec_voltage(struct dvb_v5_fe_parms *parms, int on, int v18);
 int dvb_fe_sec_tone(struct dvb_v5_fe_parms *parms, int on);
