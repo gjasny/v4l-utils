74248f5e3b46630e8cef570d3eb965098d8e005c
Mauro Carvalho Chehab <mchehab@redhat.com>
2011-12-29 15:11:43 -0300

    utils/dvb: Remove some unused headers
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- utils/dvb/dvbzap.c
+++ utils/dvb/dvbzap.c
@@ -28,15 +28,10 @@
 #define _LARGEFILE_SOURCE 1
 #define _LARGEFILE64_SOURCE 1
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/ioctl.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <stdint.h>
 #include <string.h>
 #include <stdio.h>
-#include <fcntl.h>
 #include <ctype.h>
 #include <errno.h>
 #include <signal.h>
