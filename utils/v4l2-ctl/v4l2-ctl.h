2b763a32b2ec1eeccd06aa01cc97c288285d1f76
Hans Verkuil <hans.verkuil@cisco.com>
2012-07-26 10:32:06 +0200

    v4l2-ctl: split up this overly large source.
    Phase one: split off the handling of the common options.
    
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    (cherry picked from commit 052dced533310da8e23010b4fe0fa802a0a541f0)
    
    Signed-off-by: Gregor Jasny <gjasny@googlemail.com>

---
+++ utils/v4l2-ctl/v4l2-ctl.h
@@ -0,0 +1,174 @@
+#ifndef _V4L2_CTL_H
+#define _V4L2_CTL_H
+
+/* Available options.
+
+   Please keep the first part (options < 128) in alphabetical order.
+   That makes it easier to see which short options are still free.
+
+   In general the lower case is used to set something and the upper
+   case is used to retrieve a setting. */
+enum Option {
+	OptGetSlicedVbiFormat = 'B',
+	OptSetSlicedVbiFormat = 'b',
+	OptGetCtrl = 'C',
+	OptSetCtrl = 'c',
+	OptSetDevice = 'd',
+	OptGetDriverInfo = 'D',
+	OptGetFreq = 'F',
+	OptSetFreq = 'f',
+	OptHelp = 'h',
+	OptGetInput = 'I',
+	OptSetInput = 'i',
+	OptListCtrls = 'l',
+	OptListCtrlsMenus = 'L',
+	OptListOutputs = 'N',
+	OptListInputs = 'n',
+	OptGetOutput = 'O',
+	OptSetOutput = 'o',
+	OptGetParm = 'P',
+	OptSetParm = 'p',
+	OptGetStandard = 'S',
+	OptSetStandard = 's',
+	OptGetTuner = 'T',
+	OptSetTuner = 't',
+	OptGetVideoFormat = 'V',
+	OptSetVideoFormat = 'v',
+	OptUseWrapper = 'w',
+
+	OptGetVideoMplaneFormat = 128,
+	OptSetVideoMplaneFormat,
+	OptGetSlicedVbiOutFormat,
+	OptGetOverlayFormat,
+	OptGetOutputOverlayFormat,
+	OptGetVbiFormat,
+	OptGetVbiOutFormat,
+	OptGetVideoOutFormat,
+	OptGetVideoOutMplaneFormat,
+	OptSetSlicedVbiOutFormat,
+	OptSetOutputOverlayFormat,
+	OptSetOverlayFormat,
+	//OptSetVbiFormat, TODO
+	//OptSetVbiOutFormat, TODO
+	OptSetVideoOutFormat,
+	OptSetVideoOutMplaneFormat,
+	OptTryVideoOutFormat,
+	OptTryVideoOutMplaneFormat,
+	OptTrySlicedVbiOutFormat,
+	OptTrySlicedVbiFormat,
+	OptTryVideoFormat,
+	OptTryVideoMplaneFormat,
+	OptTryOutputOverlayFormat,
+	OptTryOverlayFormat,
+	//OptTryVbiFormat, TODO
+	//OptTryVbiOutFormat, TODO
+	OptAll,
+	OptStreamOff,
+	OptStreamOn,
+	OptListStandards,
+	OptListFormats,
+	OptListMplaneFormats,
+	OptListFormatsExt,
+	OptListMplaneFormatsExt,
+	OptListFrameSizes,
+	OptListFrameIntervals,
+	OptListOverlayFormats,
+	OptListOutFormats,
+	OptListOutMplaneFormats,
+	OptLogStatus,
+	OptVerbose,
+	OptSilent,
+	OptGetSlicedVbiCap,
+	OptGetSlicedVbiOutCap,
+	OptGetFBuf,
+	OptSetFBuf,
+	OptGetCrop,
+	OptSetCrop,
+	OptGetOutputCrop,
+	OptSetOutputCrop,
+	OptGetOverlayCrop,
+	OptSetOverlayCrop,
+	OptGetOutputOverlayCrop,
+	OptSetOutputOverlayCrop,
+	OptGetSelection,
+	OptSetSelection,
+	OptGetOutputSelection,
+	OptSetOutputSelection,
+	OptGetAudioInput,
+	OptSetAudioInput,
+	OptGetAudioOutput,
+	OptSetAudioOutput,
+	OptListAudioOutputs,
+	OptListAudioInputs,
+	OptGetCropCap,
+	OptGetOutputCropCap,
+	OptGetOverlayCropCap,
+	OptGetOutputOverlayCropCap,
+	OptOverlay,
+	OptSleep,
+	OptGetJpegComp,
+	OptSetJpegComp,
+	OptGetModulator,
+	OptSetModulator,
+	OptListDevices,
+	OptGetOutputParm,
+	OptSetOutputParm,
+	OptQueryStandard,
+	OptPollForEvent,
+	OptWaitForEvent,
+	OptGetPriority,
+	OptSetPriority,
+	OptListDvPresets,
+	OptSetDvPreset,
+	OptGetDvPreset,
+	OptQueryDvPreset,
+	OptListDvTimings,
+	OptQueryDvTimings,
+	OptGetDvTimings,
+	OptSetDvBtTimings,
+	OptGetDvTimingsCap,
+	OptFreqSeek,
+	OptEncoderCmd,
+	OptTryEncoderCmd,
+	OptDecoderCmd,
+	OptTryDecoderCmd,
+	OptTunerIndex,
+	OptHelpTuner,
+	OptHelpIO,
+	OptHelpStds,
+	OptHelpVidCap,
+	OptHelpVidOut,
+	OptHelpOverlay,
+	OptHelpVbi,
+	OptHelpSelection,
+	OptHelpMisc,
+	OptHelpAll,
+	OptLast = 256
+};
+
+extern char options[OptLast];
+
+typedef struct {
+	unsigned flag;
+	const char *str;
+} flag_def;
+
+// v4l2-ctl.cpp
+int doioctl_name(int fd, unsigned long int request, void *parm, const char *name);
+int test_ioctl(int fd, int cmd, void *arg);
+std::string flags2s(unsigned val, const flag_def *def);
+
+#define doioctl(n, r, p) doioctl_name(n, r, p, #r)
+
+// v4l2-ctl-common.cpp
+void common_usage(void);
+void common_cmd(int ch, char *optarg);
+void common_set(int fd);
+void common_get(int fd);
+void common_list(int fd);
+void common_process_controls(int fd);
+void common_control_event(const struct v4l2_event *ev);
+int common_find_ctrl_id(const char *name);
+
+
+#endif
