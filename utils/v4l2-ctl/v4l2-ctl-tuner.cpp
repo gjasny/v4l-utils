1980e5578f7a9fb55f0890890c0a5627abe2669f
Hans Verkuil <hans.verkuil@cisco.com>
2012-08-26 10:43:10 -0700

    v4l2-ctl/rds-ctl: fix tuner capabilities list
    v4l2-ctl was missing some RDS caps and rds-ctl did have those caps, but printed
    it with spaces instead of dashes, making it hard to read.
    
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    (cherry picked from commit 0ca363a5f22f884bae182626931a955e481d5d68)
    
    Signed-off-by: Gregor Jasny <gjasny@googlemail.com>

--- utils/v4l2-ctl/v4l2-ctl-tuner.cpp
+++ utils/v4l2-ctl/v4l2-ctl-tuner.cpp
@@ -131,6 +131,10 @@ static std::string tcap2s(unsigned cap)
 		s += "lang2 ";
 	if (cap & V4L2_TUNER_CAP_RDS)
 		s += "rds ";
+	if (cap & V4L2_TUNER_CAP_RDS_BLOCK_IO)
+		s += "rds-block-I/O ";
+	if (cap & V4L2_TUNER_CAP_RDS_CONTROLS)
+		s += "rds-controls ";
 	if (cap & V4L2_TUNER_CAP_FREQ_BANDS)
 		s += "freq-bands ";
 	if (cap & V4L2_TUNER_CAP_HWSEEK_PROG_LIM)
