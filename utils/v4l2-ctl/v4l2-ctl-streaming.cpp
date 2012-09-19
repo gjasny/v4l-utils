fb1692e6d8f37ebfb02a1a488191fcb16adf22d7
Hans Verkuil <hans.verkuil@cisco.com>
2012-09-19 09:37:20 +0200

    v4l2-compliance: fix incorrect string compare.
    OptSilent should be turned on when streaming to stdout. Unfortunately
    the test for filename '-' (== stdout) was wrong, causing it to be turned
    on when *not* streaming to stdout.
    
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>

--- utils/v4l2-ctl/v4l2-ctl-streaming.cpp
+++ utils/v4l2-ctl/v4l2-ctl-streaming.cpp
@@ -177,7 +177,7 @@ void streaming_cmd(int ch, char *optarg)
 		break;
 	case OptStreamTo:
 		file = optarg;
-		if (strcmp(file, "-"))
+		if (!strcmp(file, "-"))
 			options[OptSilent] = true;
 		break;
 	case OptStreamFrom:
