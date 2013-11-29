586cec8dc6d72db7c148f505ee783ef0c07fe135
Mauro Carvalho Chehab <m.chehab@samsung.com>
2013-11-29 09:21:18 -0200

    libdvbv5: simplify NIT table parser
    Do a cleanup at the code, simplifying it, and removing
    the second pointer for the linked list.
    
    That makes the code easier to analyse and review.
    
    Signed-off-by: Mauro Carvalho Chehab <m.chehab@samsung.com>

--- lib/libdvbv5/descriptors/nit.c
+++ lib/libdvbv5/descriptors/nit.c
@@ -22,23 +22,21 @@
 #include "descriptors/nit.h"
 #include "dvb-fe.h"
 
-void dvb_table_nit_init(struct dvb_v5_fe_parms *parms, const uint8_t *buf, ssize_t buflen, uint8_t *table, ssize_t *table_length)
+void dvb_table_nit_init(struct dvb_v5_fe_parms *parms, const uint8_t *buf,
+			ssize_t buflen, uint8_t *table, ssize_t *table_length)
 {
 	const uint8_t *p = buf, *endbuf = buf + buflen - 4;
-	struct dvb_table_nit *nit = (struct dvb_table_nit *) table;
-	struct dvb_desc **head_desc;
-	struct dvb_table_nit_transport **head;
-	struct dvb_table_nit_transport *last = NULL;
+	struct dvb_table_nit *nit = (void *)table;
+	struct dvb_desc **head_desc = &nit->descriptor;
+	struct dvb_table_nit_transport **head = &nit->transport;
 	size_t size;
 
 	if (*table_length > 0) {
 		struct dvb_table_nit *t;
 
 		/* find end of current lists */
-		head_desc = &nit->descriptor;
 		while (*head_desc != NULL)
 			head_desc = &(*head_desc)->next;
-		head = &nit->transport;
 		while (*head != NULL)
 			head = &(*head)->next;
 
@@ -66,8 +64,6 @@ void dvb_table_nit_init(struct dvb_v5_fe_parms *parms, const uint8_t *buf, ssize
 
 		nit->descriptor = NULL;
 		nit->transport = NULL;
-		head_desc = &nit->descriptor;
-		head = &nit->transport;
 
 		bswap16(nit->bitfield);
 		size = nit->desc_length;
@@ -106,23 +102,19 @@ void dvb_table_nit_init(struct dvb_v5_fe_parms *parms, const uint8_t *buf, ssize
 		transport->descriptor = NULL;
 		transport->next = NULL;
 
-		if(!*head)
-			*head = transport;
-		if(last)
-			last->next = transport;
+		*head = transport;
+		head = &(*head)->next;
 
 		/* get the descriptors for each transport */
 		head_desc = &transport->descriptor;
 
 		if (p + transport->section_length > endbuf) {
 			dvb_logerr("NIT table was truncated while getting NIT transport descriptors. Need %u bytes, but has only %zu.",
-				transport->section_length, endbuf - p);
+				   transport->section_length, endbuf - p);
 			return;
 		}
 		dvb_parse_descriptors(parms, p, transport->section_length, head_desc);
 		p += transport->section_length;
-
-		last = transport;
 	}
 	if (endbuf - p)
 		dvb_logerr("NIT table has %zu spurious bytes at the end.",
