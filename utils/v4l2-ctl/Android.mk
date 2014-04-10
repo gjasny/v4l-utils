584ba86a580a1a4f096bcdd257e002aa7c998655
Hans-Christian Egtvedt <hegtvedt@cisco.com>
2014-04-10 17:51:43 +0200

    v4l2-ctl: add Android.mk makefile
    This Android.mk makefile is used by the Android build system to compile
    and link the v4l2-ctl utility.
    
    Signed-off-by: Hans-Christian Egtvedt <hegtvedt@cisco.com>
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>

---
+++ utils/v4l2-ctl/Android.mk
@@ -0,0 +1,25 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := v4l2-ctl
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_CFLAGS += -DNO_LIBV4L2
+LOCAL_CFLAGS += -Wno-missing-field-initializers
+LOCAL_C_INCLUDES := \
+    $(LOCAL_PATH)/../.. \
+    $(LOCAL_PATH)/../../include \
+    bionic \
+    external/stlport/stlport
+
+LOCAL_SHARED_LIBRARIES := libstlport
+
+LOCAL_SRC_FILES := \
+    v4l2-ctl.cpp v4l2-ctl.h v4l2-ctl-common.cpp v4l2-ctl-tuner.cpp \
+    v4l2-ctl-io.cpp v4l2-ctl-stds.cpp v4l2-ctl-vidcap.cpp v4l2-ctl-vidout.cpp \
+    v4l2-ctl-overlay.cpp v4l2-ctl-vbi.cpp v4l2-ctl-selection.cpp v4l2-ctl-misc.cpp \
+    v4l2-ctl-streaming.cpp v4l2-ctl-test-patterns.cpp v4l2-ctl-sdr.cpp \
+    v4l2-ctl-edid.cpp
+
+include $(BUILD_EXECUTABLE)
