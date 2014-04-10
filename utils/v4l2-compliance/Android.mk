42f031f90e08227d147f17ece2a33853a5ab8228
Hans-Christian Egtvedt <hegtvedt@cisco.com>
2014-04-10 17:51:42 +0200

    v4l2-compliance: add Android.mk makefile
    This Android.mk makefile is used by the Android build system to compile
    and link the v4l2-compliance utility.
    
    Signed-off-by: Hans-Christian Egtvedt <hegtvedt@cisco.com>
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>

---
+++ utils/v4l2-compliance/Android.mk
@@ -0,0 +1,24 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := v4l2-compliance
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_CFLAGS += -DNO_LIBV4L2
+LOCAL_CFLAGS += -Wno-missing-braces
+LOCAL_CFLAGS += -Wno-missing-field-initializers
+LOCAL_C_INCLUDES := \
+    $(LOCAL_PATH)/../.. \
+    $(LOCAL_PATH)/../../include \
+    bionic \
+    external/stlport/stlport
+
+LOCAL_SHARED_LIBRARIES := libstlport
+
+LOCAL_SRC_FILES := \
+    v4l2-compliance.cpp v4l2-test-debug.cpp v4l2-test-input-output.cpp \
+    v4l2-test-controls.cpp v4l2-test-io-config.cpp v4l2-test-formats.cpp \
+    v4l2-test-buffers.cpp v4l2-test-codecs.cpp
+
+include $(BUILD_EXECUTABLE)
