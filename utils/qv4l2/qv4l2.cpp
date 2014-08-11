gjasny/v4l-utils

commit fb1334e60cf2541c630e15c4883cd0eabf0581b5

qv4l2: fix audio looping
The audio loopback never really works well with qv4l2. After comparing it
with the xawtv3 implementation it became clear that the core problem was
that the wrong alsa device was chosen as the capture device.

The alsa handling has been simplified and only 'hw' devices are shown as
this is what the alsa loop code wants. The "Show All Audio Devices" menu
option has been removed.

In addition, a bug was fixed for radio devices: no associated alsa device
was found and the 'Play' button was disabled, so the audio loop was never
started.

Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>

utils/qv4l2/qv4l2.cpp
@@ -207,12 +207,6 @@ ApplicationWindow::ApplicationWindow() :
 #ifdef HAVE_ALSA
 	captureMenu->addSeparator();
 
-	m_showAllAudioAct = new QAction("Show All Audio &Devices", this);
-	m_showAllAudioAct->setStatusTip("Show all audio input and output devices if set");
-	m_showAllAudioAct->setCheckable(true);
-	m_showAllAudioAct->setChecked(false);
-	captureMenu->addAction(m_showAllAudioAct);
-
 	m_audioBufferAct = new QAction("Set Audio &Buffer Capacity...", this);
 	m_audioBufferAct->setStatusTip("Set audio buffer capacity in amount of ms than can be stored");
 	connect(m_audioBufferAct, SIGNAL(triggered()), this, SLOT(setAudioBufferSize()));
@@ -248,7 +242,6 @@ void ApplicationWindow::setDevice(const QString &device, bool rawOpen)
 
 	if (open(device.toLatin1(), true) < 0) {
 #ifdef HAVE_ALSA
-		m_showAllAudioAct->setEnabled(false);
 		m_audioBufferAct->setEnabled(false);
 #endif
 		return;
@@ -262,12 +255,9 @@ void ApplicationWindow::setDevice(const QString &device, bool rawOpen)
 
 #ifdef HAVE_ALSA
 	if (m_genTab->hasAlsaAudio()) {
-		connect(m_showAllAudioAct, SIGNAL(toggled(bool)), m_genTab, SLOT(showAllAudioDevices(bool)));
 		connect(m_genTab, SIGNAL(audioDeviceChanged()), this, SLOT(changeAudioDevice()));
-		m_showAllAudioAct->setEnabled(true);
 		m_audioBufferAct->setEnabled(true);
 	} else {
-		m_showAllAudioAct->setEnabled(false);
 		m_audioBufferAct->setEnabled(false);
 	}
 #endif
@@ -299,7 +289,7 @@ void ApplicationWindow::setDevice(const QString &device, bool rawOpen)
 	m_tabs->setFocus();
 	m_convertData = v4lconvert_create(g_fd());
 	bool canStream = g_fd() >= 0 && (v4l_type_is_capture(g_type()) || has_vid_out()) &&
-					!has_radio_rx() && !has_radio_tx();
+					 !has_radio_tx();
 	m_capStartAct->setEnabled(canStream);
 	m_saveRawAct->setEnabled(canStream);
 	m_snapshotAct->setEnabled(canStream && has_vid_cap());
@@ -1166,6 +1156,13 @@ void ApplicationWindow::outStart(bool start)
 
 void ApplicationWindow::capStart(bool start)
 {
+	if (m_genTab->isRadio()) {
+		if (start)
+			startAudio();
+		else
+			stopAudio();
+		return;
+	}
 	if (has_vid_out()) {
 		outStart(start);
 		return;

utils/qv4l2/general-tab.cpp
@@ -972,65 +972,10 @@ void GeneralTab::setHaveBuffers(bool haveBuffers)
 		m_vbiMethods->setDisabled(haveBuffers);
 }
 
-void GeneralTab::showAllAudioDevices(bool use)
+bool GeneralTab::filterAudioDevice(QString &deviceName)
 {
-	QString oldIn(m_audioInDevice->currentText());
-	QString oldOut(m_audioOutDevice->currentText());
-
-	m_fullAudioName = use;
-	if (oldIn == NULL || oldOut == NULL || !createAudioDeviceList())
-		return;
-
-	// Select a similar device as before the listings method change
-	// check by comparing old selection with any matching in the new list
-	bool setIn = false, setOut = false;
-	int listSize = std::max(m_audioInDevice->count(), m_audioOutDevice->count());
-
-	for (int i = 0; i < listSize; i++) {
-		QString oldInCmp(oldIn.left(std::min(m_audioInDevice->itemText(i).length(), oldIn.length())));
-		QString oldOutCmp(oldOut.left(std::min(m_audioOutDevice->itemText(i).length(), oldOut.length())));
-
-		if (!setIn && i < m_audioInDevice->count()
-		    && m_audioInDevice->itemText(i).startsWith(oldInCmp)) {
-			setIn = true;
-			m_audioInDevice->setCurrentIndex(i);
-		}
-
-		if (!setOut && i < m_audioOutDevice->count()
-		    && m_audioOutDevice->itemText(i).startsWith(oldOutCmp)) {
-			setOut = true;
-			m_audioOutDevice->setCurrentIndex(i);
-		}
-	}
-}
-
-bool GeneralTab::filterAudioInDevice(QString &deviceName)
-{
-	// Removes S/PDIF, front speakers and surround from input devices
-	// as they are output devices, not input
-	if (deviceName.contains("surround")
-	    || deviceName.contains("front")
-	    || deviceName.contains("iec958"))
-		return false;
-
-	// Removes sysdefault too if not full audio mode listings
-	if (!m_fullAudioName && deviceName.contains("sysdefault"))
-		return false;
-
-	return true;
-}
-
-bool GeneralTab::filterAudioOutDevice(QString &deviceName)
-{
-	// Removes advanced options if not full audio mode listings
-	if (!m_fullAudioName && (deviceName.contains("surround")
-				 || deviceName.contains("front")
-				 || deviceName.contains("iec958")
-				 || deviceName.contains("sysdefault"))) {
-		return false;
-	}
-
-	return true;
+	// Only show hw devices
+	return deviceName.contains("hw") && !deviceName.contains("plughw");
 }
 
 int GeneralTab::addAudioDevice(void *hint, int deviceNum)
@@ -1075,13 +1020,13 @@ int GeneralTab::addAudioDevice(void *hint, int deviceNum)
 			listName.append(QString(" %1").arg(devNo));
 	}
 
-	if ((iotype == NULL || strncmp(iotype, "Input", 5) == 0) && filterAudioInDevice(deviceName)) {
+	if ((iotype == NULL || strncmp(iotype, "Input", 5) == 0) && filterAudioDevice(deviceName)) {
 		m_audioInDevice->addItem(listName);
 		m_audioInDeviceMap[listName] = snd_device_name_get_hint(hint, "NAME");
 		added += AUDIO_ADD_READ;
 	}
 
-	if ((iotype == NULL || strncmp(iotype, "Output", 6) == 0)  && filterAudioOutDevice(deviceName)) {
+	if ((iotype == NULL || strncmp(iotype, "Output", 6) == 0)  && filterAudioDevice(deviceName)) {
 		m_audioOutDevice->addItem(listName);
 		m_audioOutDeviceMap[listName] = snd_device_name_get_hint(hint, "NAME");
 		added += AUDIO_ADD_WRITE;
@@ -2304,11 +2249,12 @@ int GeneralTab::getAudioDeviceBufferSize()
 }
 
 #ifdef HAVE_ALSA
-int GeneralTab::checkMatchAudioDevice(void *md, const char *vid, const enum device_type type)
+int GeneralTab::checkMatchAudioDevice(void *md, const char *vid, enum device_type type)
 {
 	const char *devname = NULL;
+	enum device_type dtype = isRadio() ? MEDIA_V4L_RADIO : MEDIA_V4L_VIDEO;
 
-	while ((devname = get_associated_device(md, devname, type, vid, MEDIA_V4L_VIDEO)) != NULL) {
+	while ((devname = get_associated_device(md, devname, type, vid, dtype)) != NULL) {
 		if (type == MEDIA_SND_CAP) {
 			QStringList devAddr = QString(devname).split(QRegExp("[:,]"));
 			return devAddr.value(1).toInt();
@@ -2321,19 +2267,13 @@ int GeneralTab::matchAudioDevice()
 {
 	QStringList devPath = m_device.split("/");
 	QString curDev = devPath.value(devPath.count() - 1);
-
 	void *media;
-	const char *video = NULL;
 	int match;
 
 	media = discover_media_devices();
 
-	while ((video = get_associated_device(media, video, MEDIA_V4L_VIDEO, NULL, NONE)) != NULL)
-		if (curDev.compare(video) == 0)
-			for (int i = 0; i <= MEDIA_SND_HW; i++)
-				if ((match = checkMatchAudioDevice(media, video, static_cast<device_type>(i))) != -1)
-					return match;
-
+	if ((match = checkMatchAudioDevice(media, curDev.toAscii(), MEDIA_SND_CAP)) != -1)
+		return match;
 	return -1;
 }
 #endif

utils/qv4l2/general-tab.h
@@ -84,9 +84,6 @@ public:
 	unsigned getColorspace() const;
 	int getWidth();
 
-public slots:
-	void showAllAudioDevices(bool use);
-
 signals:
 	void audioDeviceChanged();
 	void pixelAspectRatioChanged();
@@ -160,12 +157,11 @@ private:
 			updateVidCapFormat();
 	}
 	int addAudioDevice(void *hint, int deviceNum);
-	bool filterAudioInDevice(QString &deviceName);
-	bool filterAudioOutDevice(QString &deviceName);
+	bool filterAudioDevice(QString &deviceName);
 	bool createAudioDeviceList();
 #ifdef HAVE_ALSA
 	int matchAudioDevice();
-	int checkMatchAudioDevice(void *md, const char *vid, const enum device_type type);
+	int checkMatchAudioDevice(void *md, const char *vid, enum device_type type);
 #endif
 
 	void addWidget(QWidget *w, Qt::Alignment align = Qt::AlignLeft);

utils/qv4l2/qv4l2.h
@@ -248,7 +248,6 @@ private:
 	QAction *m_capStartAct;
 	QAction *m_saveRawAct;
 	QAction *m_useGLAct;
-	QAction *m_showAllAudioAct;
 	QAction *m_audioBufferAct;
 	QAction *m_scalingAct;
 	QAction *m_makeFullScreenAct;

commit 02b4e94eed8ae396e0b10a67e49acc2e0d4194de

qv4l2: fix broken SDR support
Commit fb1334e60cf2541c630e15c4883cd0eabf0581b5 broke the SDR support.

Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>

utils/qv4l2/qv4l2.cpp
@@ -1156,7 +1156,7 @@ void ApplicationWindow::outStart(bool start)
 
 void ApplicationWindow::capStart(bool start)
 {
-	if (m_genTab->isRadio()) {
+	if (m_genTab->isRadio() && !m_genTab->isSDR()) {
 		if (start)
 			startAudio();
 		else