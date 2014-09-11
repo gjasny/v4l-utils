62e0095e214f94e1d1b99fc479eb9945f782b17d
Mauro Carvalho Chehab <m.chehab@samsung.com>
2014-09-10 22:15:35 -0300

    libdvbv5: add doxygen tags to vct.h
    Add documentation for the ATSC VCT table parser
    
    Signed-off-by: Mauro Carvalho Chehab <m.chehab@samsung.com>

--- lib/include/libdvbv5/vct.h
+++ lib/include/libdvbv5/vct.h
@@ -19,6 +19,25 @@
  *
  */
 
+/**
+ * @file vct.h
+ * @brief Provides the descriptors for TVCT and CVCT tables
+ * @copyright GNU General Public License version 2 (GPLv2)
+ * @author Mauro Carvalho Chehab
+ * @author Andre Roth
+ *
+ * @par Bug Report
+ * Please submit bug report and patches to linux-media@vger.kernel.org
+ *
+ * @par Descriptors
+ * The descriptors herein are defined on:
+ * - ATSC A/65:2009 spec
+ *
+ * @see http://www.etherguidesystems.com/help/sdos/atsc/syntax/tablesections/TVCT.aspx
+ * @see http://www.etherguidesystems.com/help/sdos/atsc/syntax/tablesections/CVCT.aspx
+ */
+
+
 #ifndef _VCT_H
 #define _VCT_H
 
@@ -27,10 +46,55 @@
 
 #include <libdvbv5/atsc_header.h>
 
+/**
+ * @def ATSC_TABLE_TVCT
+ *	@brief TVCT table ID
+ * @def ATSC_TABLE_CVCT
+ *	@brief CVCT table ID
+ * @def ATSC_TABLE_VCT_PID
+ *	@brief Program ID with the VCT tables on it
+ */
 #define ATSC_TABLE_TVCT     0xc8
 #define ATSC_TABLE_CVCT     0xc9
 #define ATSC_TABLE_VCT_PID  0x1ffb
 
+/**
+ * @struct atsc_table_vct_channel
+ * @brief ATSC VCT channel table (covers both CVCT and TVCT)
+ *
+ * @param modulation_mode	modulation mode
+ * @param minor_channel_number	minor channel number
+ * @param major_channel_number	major channel number
+ * @param carrier_frequency	carrier frequency
+ * @param channel_tsid		channel tsid
+ * @param program_number	program number
+ * @param service_type		service type
+ * @param hide_guide		hide guide
+ * @param out_of_band		out of band (CVCT only)
+ * @param path_select		path select (CVCT only)
+ * @param hidden		hidden
+ * @param access_controlled	access controlled
+ * @param ETM_location		ETM location
+ * @param source_id		source ID
+ * @param descriptors_length	length of the descriptors
+ *
+ * @param descriptor		pointer to struct dvb_desc
+ * @param next pointer to another struct atsc_table_vct_channel
+ * @param descriptors_length	length of the descriptors
+ * @param short_name		short name. The __short_name is converted
+ *				from UTF-16 to locale charset when parsed
+ *
+ * This structure is used to store the original VCT channel table,
+ * converting the integer fields to the CPU endianness.
+ *
+ * The undocumented parameters are used only internally by the API and/or
+ * are fields that are reserved. They shouldn't be used, as they may change
+ * on future API releases.
+ *
+ * Everything after atsc_table_vct_channel::descriptor (including it) won't
+ * be bit-mapped * to the data parsed from the MPEG TS. So, metadata are
+ * added there.
+ */
 struct atsc_table_vct_channel {
 	uint16_t	__short_name[7];
 
@@ -72,8 +136,9 @@ struct atsc_table_vct_channel {
 	} __attribute__((packed));
 
 	/*
-	 * Everything after descriptor (including it) won't be bit-mapped
-	 * to the data parsed from the MPEG TS. So, metadata are added there
+	 * Everything after atsc_table_vct_channel::descriptor (including it)
+	 * won't be bit-mapped to the data parsed from the MPEG TS. So,
+	 * metadata are added there
 	 */
 	struct dvb_desc *descriptor;
 	struct atsc_table_vct_channel *next;
@@ -83,20 +148,35 @@ struct atsc_table_vct_channel {
 	char short_name[32];
 } __attribute__((packed));
 
+/**
+ * @struct atsc_table_vct
+ * @brief ATSC VCT table (covers both CVCT and TVCT)
+ *
+ * @param num_channels_in_section	num channels in section
+ *
+ * @param channel			pointer to struct channel
+ * @param descriptor			pointer to struct descriptor
+ *
+ * Everything after atsc_table_vct::channel (including it) won't be bit-mapped
+ * to the data parsed from the MPEG TS. So, metadata are added there
+ */
 struct atsc_table_vct {
 	ATSC_HEADER();
 
 	uint8_t num_channels_in_section;
 
-	/*
-	 * Everything after descriptor (including it) won't be bit-mapped
-	 * to the data parsed from the MPEG TS. So, metadata are added there
-	 */
 	struct atsc_table_vct_channel *channel;
 	struct dvb_desc *descriptor;
 } __attribute__((packed));
 
-
+/**
+ * @union atsc_table_vct_descriptor_length
+ * @brief ATSC VCT descriptor length
+ *
+ * @param descriptor_length	descriptor length
+ *
+ * Used internally by the library to parse the descriptor length endianness.
+ */
 union atsc_table_vct_descriptor_length {
 	uint16_t bitfield;
 	struct {
@@ -105,7 +185,13 @@ union atsc_table_vct_descriptor_length {
 	} __attribute__((packed));
 } __attribute__((packed));
 
-#define atsc_vct_channel_foreach(_channel, _vct) \
+/**
+ * @brief Macro used to find all channels on a VCT table
+ *
+ * @param _channel	channel to seek
+ * @param _vct		pointer to struct atsc_table_vct_channel
+ */
+ #define atsc_vct_channel_foreach(_channel, _vct) \
 	for (struct atsc_table_vct_channel *_channel = _vct->channel; _channel; _channel = _channel->next) \
 
 struct dvb_v5_fe_parms;
@@ -114,9 +200,37 @@ struct dvb_v5_fe_parms;
 extern "C" {
 #endif
 
-ssize_t atsc_table_vct_init (struct dvb_v5_fe_parms *parms, const uint8_t *buf, ssize_t buflen, struct atsc_table_vct **table);
-void atsc_table_vct_free(struct atsc_table_vct *vct);
-void atsc_table_vct_print(struct dvb_v5_fe_parms *parms, struct atsc_table_vct *vct);
+/**
+ * @brief Initializes and parses VCT table
+ *
+ * @param parms	struct dvb_v5_fe_parms pointer to the opened device
+ * @param buf buffer containing the VCT raw data
+ * @param buflen length of the buffer
+ * @param table pointer to struct atsc_table_vct to be allocated and filled
+ *
+ * This function allocates an ATSC VCT table and fills the fields inside
+ * the struct. It also makes sure that all fields will follow the CPU
+ * endianness. Due to that, the content of the buffer may change.
+ *
+ * @return On success, it returns the size of the allocated struct.
+ *	   A negative value indicates an error.
+ */
+ssize_t atsc_table_vct_init(struct dvb_v5_fe_parms *parms, const uint8_t *buf,
+			    ssize_t buflen, struct atsc_table_vct **table);
+/**
+ * @brief Frees all data allocated by the VCT table parser
+ *
+ * @param table pointer to struct atsc_table_vct to be freed
+ */
+void atsc_table_vct_free(struct atsc_table_vct *table);
+/**
+ * @brief Prints the content of the VCT table
+ *
+ * @param parms	struct dvb_v5_fe_parms pointer to the opened device
+ * @param table pointe to struct atsc_table_vct
+ */
+void atsc_table_vct_print(struct dvb_v5_fe_parms *parms,
+			  struct atsc_table_vct *table);
 
 #ifdef __cplusplus
 }
