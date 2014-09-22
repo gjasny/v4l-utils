1d5148834fdb0fb2b766e18c4d26a82db20c34ff
Mauro Carvalho Chehab <m.chehab@samsung.com>
2014-09-22 06:58:44 -0300

    libdvbv5: add documentation for ATSC service location
    Add corresponding doxygen documentation, adding it on a new
    module group (descriptors).
    
    Signed-off-by: Mauro Carvalho Chehab <m.chehab@samsung.com>

--- lib/include/libdvbv5/desc_atsc_service_location.h
+++ lib/include/libdvbv5/desc_atsc_service_location.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013 - Mauro Carvalho Chehab <m.chehab@samsung.com>
+ * Copyright (c) 2013-2014 - Mauro Carvalho Chehab <m.chehab@samsung.com>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -23,6 +23,32 @@
 
 #include <libdvbv5/descriptors.h>
 
+/**
+ * @file desc_atsc_service_location.h
+ * @ingroup descriptors
+ * @brief Provides the descriptors for ATSC service location
+ * @copyright GNU General Public License version 2 (GPLv2)
+ * @author Mauro Carvalho Chehab
+ *
+ * @par Relevant specs
+ * The descriptor described herein is defined at:
+ * -  ATSC A/53
+ *
+ * @see http://www.etherguidesystems.com/help/sdos/atsc/semantics/descriptors/ServiceLocation.aspx
+ *
+ * @par Bug Report
+ * Please submit bug reports and patches to linux-media@vger.kernel.org
+ */
+
+/**
+ * @struct atsc_desc_service_location_elementary
+ * @ingroup descriptors
+ * @brief service location elementary descriptors
+ *
+ * @param stream_type		stream type
+ * @param elementary_pid	elementary pid
+ * @param ISO_639_language_code	ISO 639 language code
+ */
 struct atsc_desc_service_location_elementary {
 	uint8_t stream_type;
 	union {
@@ -35,6 +61,18 @@ struct atsc_desc_service_location_elementary {
 	char ISO_639_language_code[3];
 } __attribute__((packed));
 
+/**
+ * @struct atsc_desc_service_location
+ * @ingroup descriptors
+ * @brief Describes the elementary streams inside a PAT table for ATSC
+ *
+ * @param type			descriptor tag
+ * @param length		descriptor length
+ * @param next			pointer to struct dvb_desc
+ * @param elementary		pointer to struct atsc_desc_service_location_elementary
+ * @param pcr_pid		PCR pid
+ * @param number_elements	number elements
+ */
 struct atsc_desc_service_location {
 	uint8_t type;
 	uint8_t length;
@@ -59,8 +97,41 @@ struct dvb_v5_fe_parms;
 extern "C" {
 #endif
 
-int atsc_desc_service_location_init (struct dvb_v5_fe_parms *parms, const uint8_t *buf, struct dvb_desc *desc);
-void atsc_desc_service_location_print(struct dvb_v5_fe_parms *parms, const struct dvb_desc *desc);
+/**
+ * @brief Initializes and parses the service location descriptor
+ * @ingroup descriptors
+ *
+ * @param parms	struct dvb_v5_fe_parms pointer to the opened device
+ * @param buf	buffer containing the descriptor's raw data
+ * @param desc	pointer to struct dvb_desc to be allocated and filled
+ *
+ * This function allocates a the descriptor and fills the fields inside
+ * the struct. It also makes sure that all fields will follow the CPU
+ * endianness. Due to that, the content of the buffer may change.
+ *
+ * @return On success, it returns the size of the allocated struct.
+ *	   A negative value indicates an error.
+ */
+int atsc_desc_service_location_init(struct dvb_v5_fe_parms *parms,
+				    const uint8_t *buf,
+				    struct dvb_desc *desc);
+
+/**
+ * @brief Prints the content of the service location descriptor
+ * @ingroup descriptors
+ *
+ * @param parms	struct dvb_v5_fe_parms pointer to the opened device
+ * @param desc	pointer to struct dvb_desc
+ */
+void atsc_desc_service_location_print(struct dvb_v5_fe_parms *parms,
+				      const struct dvb_desc *desc);
+
+/**
+ * @brief Frees all data allocated by the service location descriptor
+ * @ingroup descriptors
+ *
+ * @param desc pointer to struct dvb_desc to be freed
+ */
 void atsc_desc_service_location_free(struct dvb_desc *desc);
 
 #ifdef __cplusplus
