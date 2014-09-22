00d01e92be11bdb09db96a70d803b6e98033d356
Mauro Carvalho Chehab <m.chehab@samsung.com>
2014-09-22 09:32:25 -0300

    libdvbv5: add documentation for the frequency list descriptor
    Signed-off-by: Mauro Carvalho Chehab <m.chehab@samsung.com>

--- lib/include/libdvbv5/desc_frequency_list.h
+++ lib/include/libdvbv5/desc_frequency_list.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2012 - Mauro Carvalho Chehab
+ * Copyright (c) 2011-2014 - Mauro Carvalho Chehab
  * Copyright (c) 2012 - Andre Roth <neolynx@gmail.com>
  *
  * This program is free software; you can redistribute it and/or
@@ -23,7 +23,37 @@
 #define _DESC_FREQUENCY_LIST_H
 
 #include <libdvbv5/descriptors.h>
+/**
+ * @file desc_frequency_list.h
+ * @ingroup descriptors
+ * @brief Provides the descriptors for the frequency list descriptor.
+ *	  This descriptor lists the additional frequencies used in transmission
+ *	  of a multiplex on other frequencies.
+ * @copyright GNU General Public License version 2 (GPLv2)
+ * @author Mauro Carvalho Chehab
+ * @author Andre Roth
+ *
+ * @par Relevant specs
+ * The descriptor described herein is defined at:
+ * - ETSI EN 300 468 V1.11.1
+ *
+ * @par Bug Report
+ * Please submit bug reports and patches to linux-media@vger.kernel.org
+ */
 
+/**
+ * @struct dvb_desc_frequency_list
+ * @ingroup descriptors
+ * @brief Struct containing the frequency list descriptor
+ *
+ * @param type			descriptor tag
+ * @param length		descriptor length
+ * @param next			pointer to struct dvb_desc
+ * @param frequencies		number of frequencies in the frequency vector
+ * @param frequency		vector with the centre frequency
+ * @param freq_type		freq type, being: 0 = undefined,
+ * 				1 = satelite, 2 = cable or 3 = terrestrial.
+ */
 struct dvb_desc_frequency_list {
 	uint8_t type;
 	uint8_t length;
@@ -47,8 +77,34 @@ struct dvb_v5_fe_parms;
 extern "C" {
 #endif
 
-int dvb_desc_frequency_list_init (struct dvb_v5_fe_parms *parms, const uint8_t *buf, struct dvb_desc *desc);
-void dvb_desc_frequency_list_print(struct dvb_v5_fe_parms *parms, const struct dvb_desc *desc);
+/**
+ * @brief Initializes and parses the frequency list descriptor
+ * @ingroup descriptors
+ *
+ * @param parms	struct dvb_v5_fe_parms pointer to the opened device
+ * @param buf	buffer containing the descriptor's raw data
+ * @param desc	pointer to struct dvb_desc to be allocated and filled
+ *
+ * This function initializes and makes sure that all fields will follow the CPU
+ * endianness. Due to that, the content of the buffer may change.
+ *
+ * Currently, no memory is allocated internally.
+ *
+ * @return On success, it returns the size of the allocated struct.
+ *	   A negative value indicates an error.
+ */
+int dvb_desc_frequency_list_init(struct dvb_v5_fe_parms *parms,
+				 const uint8_t *buf, struct dvb_desc *desc);
+
+/**
+ * @brief Prints the content of the frequency list descriptor
+ * @ingroup descriptors
+ *
+ * @param parms	struct dvb_v5_fe_parms pointer to the opened device
+ * @param desc	pointer to struct dvb_desc
+ */
+void dvb_desc_frequency_list_print(struct dvb_v5_fe_parms *parms,
+				   const struct dvb_desc *desc);
 
 #ifdef __cplusplus
 }
