e1922b24624ad8b96e5779e941a4c680b43210cd
Mauro Carvalho Chehab <m.chehab@samsung.com>
2014-10-02 15:55:46 -0300

    dvb-file: Fix channel name when SDT is not found
    The frequency var was stored before reading it from the file
    structure, causing it to be bad filled.
    
    Change it do to the right thing. Also, as we already print all
    stored channels, add a print for each stored program without
    their corresponding SDT entry.
    
    Now, when SDT is not found, it will print something like:
    
    Scanning frequency #2 579000000
    Lock   (0x1f) Quality= Ok   Signal= 100.00% C/N= 36.30dB UCB= 65980 postBER= 1.06x10^-3 PER= 834x10^-6
    ERROR    dvb_read_sections: no data read on section filter
    ERROR    error while reading the SDT table
    WARNING: no SDT table - storing channel(s) without their names
    Storing Service ID 49: '579.00MHz#49'
    Storing Service ID 60: '579.00MHz#60'
    Storing Service ID 72: '579.00MHz#72'
    Storing Service ID 315: '579.00MHz#315'
    Storing Service ID 316: '579.00MHz#316'
    Storing Service ID 319: '579.00MHz#319'
    Storing Service ID 321: '579.00MHz#321'
    Storing Service ID 350: '579.00MHz#350'
    Storing Service ID 34: '579.00MHz#34'
    Storing Service ID 150: '579.00MHz#150'
    Storing Service ID 71: '579.00MHz#71'
    Storing Service ID 50: '579.00MHz#50'
    Storing Service ID 33: '579.00MHz#33'
    Storing Service ID 97: '579.00MHz#97'
    
    Signed-off-by: Mauro Carvalho Chehab <m.chehab@samsung.com>

--- lib/libdvbv5/dvb-file.c
+++ lib/libdvbv5/dvb-file.c
@@ -1130,19 +1130,18 @@ static int get_program_and_store(struct dvb_v5_fe_parms_priv *parms,
 		if (rc)
 			dvb_logerr("Couldn't get frontend props");
 	}
-	if (!*channel) {
-		r = asprintf(&channel, "%.2fMHz#%d", freq/1000000., service_id);
-		if (r < 0)
-			dvb_perror("asprintf");
-		if (parms->p.verbose)
-			dvb_log("Storing as: '%s'", channel);
-	}
 	for (j = 0; j < parms->n_props; j++) {
 		entry->props[j].cmd = parms->dvb_prop[j].cmd;
 		entry->props[j].u.data = parms->dvb_prop[j].u.data;
 
 		if (!*channel && entry->props[j].cmd == DTV_FREQUENCY)
 			freq = parms->dvb_prop[j].u.data;
+	}
+	if (!*channel) {
+		r = asprintf(&channel, "%.2fMHz#%d", freq/1000000., service_id);
+		if (r < 0)
+			dvb_perror("asprintf");
+		dvb_log("Storing Service ID %d: '%s'", service_id, channel);
 	}
 	entry->n_props = parms->n_props;
 	entry->channel = channel;
