a45c3f42028bdce77e14fc6f398a3ecf27d0353f
Mats Randgaard <matrandg@cisco.com>
2014-12-09 12:47:12 +0100

    v4l2-ctl: Remove file entry from Android.mk
    v4l2-ctl-test-patterns.cpp is removed, but still listed in Android.mk.
    
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>

--- utils/v4l2-ctl/Android.mk
+++ utils/v4l2-ctl/Android.mk
@@ -19,7 +19,6 @@ LOCAL_SRC_FILES := \
     v4l2-ctl.cpp v4l2-ctl.h v4l2-ctl-common.cpp v4l2-ctl-tuner.cpp \
     v4l2-ctl-io.cpp v4l2-ctl-stds.cpp v4l2-ctl-vidcap.cpp v4l2-ctl-vidout.cpp \
     v4l2-ctl-overlay.cpp v4l2-ctl-vbi.cpp v4l2-ctl-selection.cpp v4l2-ctl-misc.cpp \
-    v4l2-ctl-streaming.cpp v4l2-ctl-test-patterns.cpp v4l2-ctl-sdr.cpp \
-    v4l2-ctl-edid.cpp vivid-tpg-colors.c vivid-tpg.c
-
+    v4l2-ctl-streaming.cpp v4l2-ctl-sdr.cpp v4l2-ctl-edid.cpp vivid-tpg-colors.c \
+    vivid-tpg.c
 include $(BUILD_EXECUTABLE)
