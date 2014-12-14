d6f864d724db9c1072a4fd04fae2c46f49cf22d1
Mauro Carvalho Chehab <mchehab@osg.samsung.com>
2014-12-14 09:57:04 -0200

    bootstrap.sh: be less verbose
    Reduce verbosity of the script by only showing one message.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,19 +4,22 @@ autoreconf -vfi
 
 GETTEXTIZE=$(which gettextize)
 if [ "GETTEXTIZE" != "" ]; then
+	VER=$(gettextize --version|perl -ne 'print $1 if (m/(\d\.\d.*)/)')
 
 	cp build-aux/config.rpath build-aux/config.rpath.bak
 
 	sed "s,read dummy < /dev/tty,," < $GETTEXTIZE > ./gettextize
 	chmod 755 ./gettextize
 
-	./gettextize --force --copy --no-changelog --po-dir=v4l-utils-po
-	./gettextize --force --copy --no-changelog --po-dir=libdvbv5-po
+	echo "Generating locale v4l-utils-po build files for gettext version $VER"
+	./gettextize --force --copy --no-changelog --po-dir=v4l-utils-po >/dev/null
+
+	echo "Generating locale libdvbv5-po build files for gettext version $VER"
+	./gettextize --force --copy --no-changelog --po-dir=libdvbv5-po >/dev/null
 
 	for i in v4l-utils-po/Makefile.in.in libdvbv5-po/Makefile.in.in; do
 		sed 's,rm -f Makefile,rm -f,' $i >a && mv a $i
 	done
-	sed 's,rm -f Makefile , rm -f ,' $i >a && mv a $i
 	sed 's,PACKAGE = @PACKAGE@,PACKAGE = @LIBDVBV5_DOMAIN@,' <libdvbv5-po/Makefile.in.in >a && mv a libdvbv5-po/Makefile.in.in
 
 	mv build-aux/config.rpath.bak build-aux/config.rpath
