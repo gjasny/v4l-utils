5faef3eeb27e15d0618267e29aa4e69e5aa4aea8
Mauro Carvalho Chehab <mchehab@osg.samsung.com>
2014-12-14 09:35:02 -0200

    bootstrap.sh: remove an extra uneeded space from the script
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,7 +14,7 @@ if [ "GETTEXTIZE" != "" ]; then
 	./gettextize --force --copy --no-changelog --po-dir=libdvbv5-po
 
 	for i in v4l-utils-po/Makefile.in.in libdvbv5-po/Makefile.in.in; do
-		sed 's,rm -f Makefile, rm -f,' $i >a && mv a $i
+		sed 's,rm -f Makefile,rm -f,' $i >a && mv a $i
 	done
 	sed 's,rm -f Makefile , rm -f ,' $i >a && mv a $i
 	sed 's,PACKAGE = @PACKAGE@,PACKAGE = @LIBDVBV5_DOMAIN@,' <libdvbv5-po/Makefile.in.in >a && mv a libdvbv5-po/Makefile.in.in
