gjasny/v4l-utils

commit da56d6f1578ab79d81ddd92d26c68b17d66495f5

libdvbv5: create a new API for dvb-demux
The API defined by dvb-demux.h has several troubles:

1) error messages don't go to the DVB log facility;
2) the error messages can't be translated to locale language;
3) some things used by the callers don't have any wrapper
function.

Write a proper API to handle DVB demux and dvr, based on
struct dvb_device, and mark the old one as deprecated.

Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

utils/dvb/dvbv5-scan.c
@@ -249,7 +249,8 @@ static int run_scan(struct arguments *args, struct dvb_device *dvb)
 	if (!dvb_file)
 		return -2;
 
-	dmx_fd = dvb_dev_open(dvb, args->demux_dev, O_RDWR);
+	/* FIXME: should be replaced by dvb_dev_open() */
+	dmx_fd = open(args->demux_dev, O_RDWR);
 	if (dmx_fd < 0) {
 		perror(_("openening pat demux failed"));
 		return -3;
@@ -538,8 +539,7 @@ int main(int argc, char **argv)
 	if (!dvb_dev)
 		return -1;
 
-	err = dvb_dev_open(dvb, dvb_dev->sysname, O_RDWR);
-	if (err < 0) {
+	if (!dvb_dev_open(dvb, dvb_dev->sysname, O_RDWR)) {
 		free(args.demux_dev);
 		return -1;
 	}

lib/include/libdvbv5/dvb-demux.h
@@ -46,9 +46,12 @@ extern "C" {
  * @param adapter	DVB adapter number to open
  * @param demux		DVB demux number to open
  *
- * @details This is a wrapper function to open. File is always opened in blocking mode.
+ * @details This is a wrapper function to open(). File is always opened in
+ *	blocking mode.
  *
  * @return Returns a file descriptor on success, -1 otherwise.
+ *
+ * @warning Deprecated. Please use dvb_dev_open() instead.
  */
 int dvb_dmx_open(int adapter, int demux);
 
@@ -58,7 +61,9 @@ int dvb_dmx_open(int adapter, int demux);
  *
  * @param dmx_fd	File descriptor to close
  *
- * This is a wrapper function to open.
+ * This is a wrapper function to close().
+ *
+ * @warning Deprecated. Please use dvb_dev_close() instead.
  */
 void dvb_dmx_close(int dmx_fd);
 
@@ -68,7 +73,11 @@ void dvb_dmx_close(int dmx_fd);
  *
  * @param dmx_fd	File descriptor to close
  *
- * This is a wrapper function to open.
+ * This is a wrapper function to DMX_STOP ioctl.
+ * See http://linuxtv.org/downloads/v4l-dvb-apis/dvb_demux.html
+ * for more details.
+ *
+ * @warning Deprecated. Please use dvb_dev_dmx_stop() instead.
  */
 void dvb_dmx_stop(int dmx_fd);
 
@@ -90,6 +99,8 @@ void dvb_dmx_stop(int dmx_fd);
  * for more details.
  *
  * @return Retuns zero on success, -1 otherwise.
+ *
+ * @warning Deprecated. Please use dvb_dev_dmx_set_pesfilter() instead.
  */
 int dvb_set_pesfilter(int dmxfd, int pid, dmx_pes_type_t type,
 		      dmx_output_t output, int buffersize);
@@ -111,7 +122,10 @@ int dvb_set_pesfilter(int dmxfd, int pid, dmx_pes_type_t type,
  * See http://linuxtv.org/downloads/v4l-dvb-apis/dvb_demux.html
  * for more details.
  *
+ * @warning Deprecated. Please use dvb_dev_dmx_set_pesfilter() instead.
+ *
  * @return Retuns zero on success, -1 otherwise.
+ *
  */
 int dvb_set_section_filter(int dmxfd, int pid, unsigned filtsize,
 			   unsigned char *filter,
@@ -127,10 +141,12 @@ int dvb_set_section_filter(int dmxfd, int pid, unsigned filtsize,
  * @param dmxfd		File descriptor for the demux device
  * @param sid		Session ID to seeking
  *
- * @warning This function currently assumes that the PAT fits into one session.
+ * @warning Deprecated. Please use dvb_get_pmt_pid() instead.
  *
  * @return At return, it returns a negative value if error or the PID associated with
  * the desired Session ID.
+ *
+ * @warning This function currently assumes that the PAT fits into one session.
  */
 int dvb_get_pmt_pid(int dmxfd, int sid);
 

lib/include/libdvbv5/dvb-dev.h
@@ -20,6 +20,7 @@
 #define _DVB_DEV_H
 
 #include "dvb-fe.h"
+#include <linux/dvb/dmx.h>
 
 /**
  * @file dvb-dev.h
@@ -89,6 +90,13 @@ struct dvb_dev_list {
 	char *product;
 	char *serial;
 };
+
+/**
+ * @struct dvb_open_descriptor
+ *
+ * Opaque struct with a DVB open file descriptor
+ */
+struct dvb_open_descriptor;
 
 /**
  * @struct dvb_device
@@ -223,10 +231,141 @@ void dvb_dev_set_log(struct dvb_device *dvb,
  * @details This function is equivalent to open(2) system call: it opens a
  *	Digital TV given by the dev parameter, using the flags.
  *
- * @return returns a file descriptor on success, a negative value with -errno
- *	on errors.
- *
+ * @return returns a pointer to the dvb_open_descriptor that should be used
+ * 	on further calls if sucess. NULL otherwise.
  */
-int dvb_dev_open(struct dvb_device *d, char *sysname, int flags);
+struct dvb_open_descriptor *dvb_dev_open(struct dvb_device *dvb,
+					 char *sysname, int flags);
+
+/**
+ * @brief Closes a dvb device
+ * @ingroup dvb_device
+ *
+ * @param open_dev	Points to the struct dvb_open_descriptor to be
+ * closed.
+ */
+void dvb_dev_close(struct dvb_open_descriptor *open_dev);
+
+/**
+ * @brief read from a dvb demux or dvr file
+ * @ingroup dvb_device
+ *
+ * @param open_dev	Points to the struct dvb_open_descriptor to be
+ * closed.
+ * @param buf		Buffer to store the data
+ * @param count		number of bytes to read
+ *
+ * @return On success, returns the number of bytes read. Returns -1 on
+ * error.
+ */
+ssize_t dvb_dev_read(struct dvb_open_descriptor *open_dev,
+		     void *buf, size_t count);
+
+/**
+ * @brief Stops the demux filter for a given file descriptor
+ * @ingroup dvb_device
+ *
+ * @param open_dev	Points to the struct dvb_open_descriptor
+ *
+ * This is a wrapper function for DMX_STOP ioctl.
+ *
+ * See http://linuxtv.org/downloads/v4l-dvb-apis/dvb_demux.html
+ * for more details.
+ *
+ * @note valid only for DVB_DEVICE_DEMUX.
+ */
+void dvb_dev_dmx_stop(struct dvb_open_descriptor *open_dev);
+
+/**
+ * @brief Start a demux or dvr buffer size
+ * @ingroup dvb_device
+ *
+ * @param open_dev	Points to the struct dvb_open_descriptor
+ * @param buffersize	Size of the buffer to be allocated to store the filtered data.
+ *
+ * This is a wrapper function for DMX_SET_BUFFER_SIZE ioctl.
+ *
+ * See http://linuxtv.org/downloads/v4l-dvb-apis/dvb_demux.html
+ * for more details.
+ *
+ * @return Retuns zero on success, -1 otherwise.
+ *
+ * @note valid only for DVB_DEVICE_DEMUX or DVB_DEVICE_DVR.
+ */
+int dvb_dev_set_bufsize(struct dvb_open_descriptor *open_dev,
+			int buffersize);
+
+/**
+ * @brief Start a filter for a MPEG-TS Packetized Elementary
+ * 		       Stream (PES)
+ * @ingroup dvb_device
+ *
+ * @param open_dev	Points to the struct dvb_open_descriptor
+ * @param pid		Program ID to filter. Use 0x2000 to select all PIDs
+ * @param type		type of the PID (DMX_PES_VIDEO, DMX_PES_AUDIO,
+ *			DMX_PES_OTHER, etc).
+ * @param output	Where the data will be output (DMX_OUT_TS_TAP,
+ *			DMX_OUT_DECODER, etc).
+ * @param buffersize	Size of the buffer to be allocated to store the filtered data.
+ *
+ * This is a wrapper function for DMX_SET_PES_FILTER and DMX_SET_BUFFER_SIZE
+ * ioctls.
+ *
+ * See http://linuxtv.org/downloads/v4l-dvb-apis/dvb_demux.html
+ * for more details.
+ *
+ * @return Retuns zero on success, -1 otherwise.
+ *
+ * @note valid only for DVB_DEVICE_DEMUX.
+ */
+int dvb_dev_dmx_set_pesfilter(struct dvb_open_descriptor *open_dev,
+			      int pid, dmx_pes_type_t type,
+			      dmx_output_t output, int buffersize);
+
+/**
+ * @brief Sets a MPEG-TS section filter
+ * @ingroup dvb_device
+ *
+ * @param open_dev	Points to the struct dvb_open_descriptor
+ * @param pid		Program ID to filter. Use 0x2000 to select all PIDs
+ * @param filtsize	Size of the filter (up to 18 btyes)
+ * @param filter	data to filter. Can be NULL or should have filtsize length
+ * @param mask		filter mask. Can be NULL or should have filtsize length
+ * @param mode		mode mask. Can be NULL or should have filtsize length
+ * @param flags		flags for set filter (DMX_CHECK_CRC,DMX_ONESHOT,
+ *			DMX_IMMEDIATE_START).
+ *
+ * This is a wrapper function for DMX_SET_FILTER ioctl.
+ *
+ * See http://linuxtv.org/downloads/v4l-dvb-apis/dvb_demux.html
+ * for more details.
+ *
+ * @return Retuns zero on success, -1 otherwise.
+ *
+ * @note valid only for DVB_DEVICE_DEMUX.
+ */
+int dvb_dev_dmx_set_section_filter(struct dvb_open_descriptor *open_dev,
+				   int pid, unsigned filtsize,
+				   unsigned char *filter,
+				   unsigned char *mask,
+				   unsigned char *mode,
+				   unsigned int flags);
+
+/**
+ * @brief read the contents of the MPEG-TS PAT table, seeking for
+ *		      	an specific service ID
+ * @ingroup dvb_device
+ *
+ * @param open_dev	Points to the struct dvb_open_descriptor
+ * @param sid		Session ID to seeking
+ *
+ * @return At return, it returns a negative value if error or the PID
+ * associated with the desired Session ID.
+ *
+ * @warning This function currently assumes that the PAT fits into one session.
+ *
+ * @note valid only for DVB_DEVICE_DEMUX.
+ */
+int dvb_dev_dmx_get_pmt_pid(struct dvb_open_descriptor *open_dev, int sid);
 
 #endif

lib/libdvbv5/dvb-dev.c
@@ -36,6 +36,15 @@
 # define _(string) string
 #endif
 
+struct dvb_device_priv;
+
+struct dvb_open_descriptor {
+	int fd;
+	struct dvb_dev_list *dev;
+	struct dvb_device_priv *dvb;
+	struct dvb_open_descriptor *next;
+};
+
 struct dvb_device_priv {
 	struct dvb_device d;
 
@@ -44,6 +53,8 @@ struct dvb_device_priv {
 	/* udev control fields */
 	struct udev *udev;
 	struct udev_monitor *mon;
+
+	struct dvb_open_descriptor open_list;
 };
 
 static void free_dvb_dev(struct dvb_dev_list *dvb_dev)
@@ -96,6 +107,16 @@ static void dvb_dev_free_devices(struct dvb_device_priv *dvb)
 void dvb_dev_free(struct dvb_device *d)
 {
 	struct dvb_device_priv *dvb = (void *)d;
+	struct dvb_open_descriptor *cur, *next;
+	struct dvb_v5_fe_parms_priv *parms = (void *)dvb->d.fe_parms;
+
+	/* Close all devices */
+	cur = dvb->open_list.next;
+	while (cur) {
+		next = cur->next;
+		dvb_dev_close(cur);
+		cur = next;
+	}
 
 	dvb_dev_free_devices(dvb);
 
@@ -104,7 +125,7 @@ void dvb_dev_free(struct dvb_device *d)
 		dvb->monitor = 0;
 		usleep(1000);
 	}
-	dvb_fe_close(dvb->d.fe_parms);
+	__dvb_fe_close(parms);
 	free(dvb);
 }
 
@@ -429,16 +450,25 @@ void dvb_dev_set_log(struct dvb_device *dvb, unsigned verbose,
 			parms->p.logfunc = logfunc;
 }
 
-int dvb_dev_open(struct dvb_device *d, char *sysname, int flags)
+struct dvb_open_descriptor *dvb_dev_open(struct dvb_device *d,
+					 char *sysname, int flags)
 {
 	struct dvb_device_priv *dvb = (void *)d;
 	struct dvb_v5_fe_parms_priv *parms = (void *)d->fe_parms;
 	struct dvb_dev_list *dev = NULL;
+	struct dvb_open_descriptor *open_dev, *cur;
 	int ret, i;
+
+	open_dev = calloc(1, sizeof(*open_dev));
+	if (!open_dev) {
+		dvb_perror("Can't create file descriptor");
+		return NULL;
+	}
 
 	if (!sysname) {
 		dvb_logerr(_("Device not specified"));
-		return -ENOENT;
+		free(open_dev);
+		return NULL;
 	}
 
 	for (i = 0; i < dvb->d.num_devices; i++) {
@@ -449,20 +479,281 @@ int dvb_dev_open(struct dvb_device *d, char *sysname, int flags)
 	}
 	if (!dev) {
 		dvb_logerr(_("Can't find device %s"), sysname);
-		return -ENOENT;
+		free(open_dev);
+		return NULL;
 	}
 
 	if (dev->dvb_type == DVB_DEVICE_FRONTEND) {
+		/*
+		 * The frontend API was designed for sync frontend access.
+		 * It is not ready to handle async frontend access.
+		 */
+		flags &= ~O_NONBLOCK;
+
 		ret = dvb_fe_open_fname(parms, dev->path, flags);
-		if (ret)
-			return -ret;
-		return parms->fd;
+		if (ret) {
+			free(open_dev);
+			return NULL;
+		}
+		ret = parms->fd;
+	} else {
+		/* We don't need special handling for other DVB device types */
+		ret = open(dev->path, flags);
+		if (ret == -1) {
+			dvb_logerr(_("Can't open %s with flags %d: %d %m"),
+				   dev->path, flags, errno);
+			free(open_dev);
+			return NULL;
+		}
 	}
 
-	/* We don't need special handling for other DVB device types */
-	ret = open(dev->path, flags);
+	/* Add the fd to the open descriptor's list */
+	open_dev->fd = ret;
+	open_dev->dev = dev;
+	open_dev->dvb = dvb;
+
+	cur = &dvb->open_list;
+	while (cur->next)
+		cur = cur->next;
+	cur->next = open_dev;
+
+	return open_dev;
+}
+
+void dvb_dev_close(struct dvb_open_descriptor *open_dev)
+{
+	struct dvb_dev_list *dev = open_dev->dev;
+	struct dvb_device_priv *dvb = open_dev->dvb;
+	struct dvb_v5_fe_parms_priv *parms = (void *)dvb->d.fe_parms;
+	struct dvb_open_descriptor *cur;
+
+	if (dev->dvb_type == DVB_DEVICE_FRONTEND)
+		__dvb_fe_close(parms);
+	else {
+		if (dev->dvb_type == DVB_DEVICE_DEMUX)
+			dvb_dev_dmx_stop(open_dev);
+
+		close(open_dev->fd);
+	}
+
+	for (cur = &dvb->open_list; cur->next; cur = cur->next) {
+		if (cur->next == open_dev) {
+			cur->next = open_dev->next;
+			free(open_dev);
+			return;
+		}
+	}
+
+	/* Should never happen */
+	dvb_logerr(_("Couldn't free device\n"));
+}
+
+#define MAX_TIME		10	/* 1.0 seconds */
+
+#define xioctl(fh, request, arg...) ({					\
+	int __rc;							\
+	struct timespec __start, __end;					\
+									\
+	clock_gettime(CLOCK_MONOTONIC, &__start);			\
+	do {								\
+		__rc = ioctl(fh, request, ##arg);			\
+		if (__rc != -1)						\
+			break;						\
+		if ((errno != EINTR) && (errno != EAGAIN))		\
+			break;						\
+		clock_gettime(CLOCK_MONOTONIC, &__end);			\
+		if (__end.tv_sec * 10 + __end.tv_nsec / 100000000 >	\
+		    __start.tv_sec * 10 + __start.tv_nsec / 100000000 +	\
+		    MAX_TIME)						\
+			break;						\
+	} while (1);							\
+									\
+	__rc;								\
+})
+
+void dvb_dev_dmx_stop(struct dvb_open_descriptor *open_dev)
+{
+	struct dvb_dev_list *dev = open_dev->dev;
+	struct dvb_device_priv *dvb = open_dev->dvb;
+	struct dvb_v5_fe_parms_priv *parms = (void *)dvb->d.fe_parms;
+	int ret, fd = open_dev->fd;
+
+	if (dev->dvb_type != DVB_DEVICE_DEMUX)
+		return;
+
+	ret = xioctl(fd, DMX_STOP);
 	if (ret == -1)
-		return -errno;
+		dvb_perror(_("DMX_STOP failed"));
+}
+
+int dvb_dev_set_bufsize(struct dvb_open_descriptor *open_dev,
+			int buffersize)
+{
+	struct dvb_dev_list *dev = open_dev->dev;
+	struct dvb_device_priv *dvb = open_dev->dvb;
+	struct dvb_v5_fe_parms_priv *parms = (void *)dvb->d.fe_parms;
+	int fd = open_dev->fd;
+
+	if (dev->dvb_type != DVB_DEVICE_DEMUX && dev->dvb_type != DVB_DEVICE_DVR)
+		return -1;
+
+	if (xioctl(fd, DMX_SET_BUFFER_SIZE, buffersize) == -1) {
+		dvb_perror(_("DMX_SET_BUFFER_SIZE failed"));
+		return -1;
+	}
+
+	return 0;
+}
+
+ssize_t dvb_dev_read(struct dvb_open_descriptor *open_dev,
+		     void *buf, size_t count)
+{
+	struct dvb_dev_list *dev = open_dev->dev;
+	struct dvb_device_priv *dvb = open_dev->dvb;
+	struct dvb_v5_fe_parms_priv *parms = (void *)dvb->d.fe_parms;
+	int fd = open_dev->fd;
+	ssize_t ret;
+
+	if (dev->dvb_type != DVB_DEVICE_DEMUX && dev->dvb_type != DVB_DEVICE_DVR)
+		return -1;
+
+	ret = read(fd, buf, count);
+	if (ret == -1)
+		dvb_perror("read()");
 
 	return ret;
+}
+
+int dvb_dev_dmx_set_pesfilter(struct dvb_open_descriptor *open_dev,
+			      int pid, dmx_pes_type_t type,
+			      dmx_output_t output, int bufsize)
+{
+	struct dvb_dev_list *dev = open_dev->dev;
+	struct dvb_device_priv *dvb = open_dev->dvb;
+	struct dvb_v5_fe_parms_priv *parms = (void *)dvb->d.fe_parms;
+	struct dmx_pes_filter_params pesfilter;
+	int fd = open_dev->fd;
+
+	if (dev->dvb_type != DVB_DEVICE_DEMUX)
+		return -1;
+
+	/* Failing here is not fatal, so no need to handle error condition */
+	if (bufsize)
+		dvb_dev_set_bufsize(open_dev, bufsize);
+
+	memset(&pesfilter, 0, sizeof(pesfilter));
+
+	pesfilter.pid = pid;
+	pesfilter.input = DMX_IN_FRONTEND;
+	pesfilter.output = output;
+	pesfilter.pes_type = type;
+	pesfilter.flags = DMX_IMMEDIATE_START;
+
+	if (xioctl(fd, DMX_SET_PES_FILTER, &pesfilter) == -1) {
+		dvb_logerr(_("DMX_SET_PES_FILTER failed (PID = 0x%04x): %d %m"),
+			   pid, errno);
+		return -1;
+	}
+
+	return 0;
+}
+
+int dvb_dev_dmx_set_section_filter(struct dvb_open_descriptor *open_dev,
+				   int pid, unsigned filtsize,
+				   unsigned char *filter,
+				   unsigned char *mask,
+				   unsigned char *mode,
+				   unsigned int flags)
+{
+	struct dvb_dev_list *dev = open_dev->dev;
+	struct dvb_device_priv *dvb = open_dev->dvb;
+	struct dvb_v5_fe_parms_priv *parms = (void *)dvb->d.fe_parms;
+	struct dmx_sct_filter_params sctfilter;
+	int fd = open_dev->fd;
+
+	if (dev->dvb_type != DVB_DEVICE_DEMUX)
+		return -1;
+
+	if (filtsize > DMX_FILTER_SIZE)
+		filtsize = DMX_FILTER_SIZE;
+
+	memset(&sctfilter, 0, sizeof(sctfilter));
+
+	sctfilter.pid = pid;
+
+	if (filter)
+		memcpy(sctfilter.filter.filter, filter, filtsize);
+	if (mask)
+		memcpy(sctfilter.filter.mask, mask, filtsize);
+	if (mode)
+		memcpy(sctfilter.filter.mode, mode, filtsize);
+
+	sctfilter.flags = flags;
+
+	if (xioctl(fd, DMX_SET_FILTER, &sctfilter) == -1) {
+		dvb_logerr(_("DMX_SET_FILTER failed (PID = 0x%04x): %d %m"),
+			pid, errno);
+		return -1;
+	}
+
+	return 0;
+}
+
+int dvb_dev_dmx_get_pmt_pid(struct dvb_open_descriptor *open_dev, int sid)
+{
+	struct dvb_dev_list *dev = open_dev->dev;
+	struct dvb_device_priv *dvb = open_dev->dvb;
+	struct dvb_v5_fe_parms_priv *parms = (void *)dvb->d.fe_parms;
+	struct dmx_sct_filter_params f;
+	unsigned char buft[4096];
+	unsigned char *buf = buft;
+	int fd = open_dev->fd;
+	int count;
+	int pmt_pid = 0;
+	int patread = 0;
+	int section_length;
+
+	if (dev->dvb_type != DVB_DEVICE_DEMUX)
+		return -1;
+
+	memset(&f, 0, sizeof(f));
+	f.pid = 0;
+	f.filter.filter[0] = 0x00;
+	f.filter.mask[0] = 0xff;
+	f.timeout = 0;
+	f.flags = DMX_IMMEDIATE_START | DMX_CHECK_CRC;
+
+	if (xioctl(fd, DMX_SET_FILTER, &f) == -1) {
+		dvb_perror("ioctl DMX_SET_FILTER failed");
+		return -1;
+	}
+
+	while (!patread){
+		if (((count = read(fd, buf, sizeof(buft))) < 0) && errno == EOVERFLOW)
+		count = read(fd, buf, sizeof(buft));
+		if (count < 0) {
+		dvb_perror("read_sections: read error");
+		return -1;
+		}
+
+		section_length = ((buf[1] & 0x0f) << 8) | buf[2];
+		if (count != section_length + 3)
+		continue;
+
+		buf += 8;
+		section_length -= 8;
+
+		patread = 1; /* assumes one section contains the whole pat */
+		while (section_length > 0) {
+		int service_id = (buf[0] << 8) | buf[1];
+		if (service_id == sid) {
+			pmt_pid = ((buf[2] & 0x1f) << 8) | buf[3];
+			section_length = 0;
+		}
+		buf += 4;
+		section_length -= 4;
+		}
+	}
+
+	return pmt_pid;
 }

lib/libdvbv5/dvb-fe-priv.h
@@ -77,5 +77,6 @@ struct dvb_v5_fe_parms_priv {
 int dvb_fe_open_fname(struct dvb_v5_fe_parms_priv *parms, char *fname,
 		      int flags);
 void dvb_v5_free(struct dvb_v5_fe_parms_priv *parms);
+void __dvb_fe_close(struct dvb_v5_fe_parms_priv *parms);
 
 #endif

lib/libdvbv5/dvb-fe.c
@@ -374,6 +374,18 @@ int dvb_fe_is_satellite(uint32_t delivery_system)
 	}
 }
 
+void __dvb_fe_close(struct dvb_v5_fe_parms_priv *parms)
+{
+	if (!parms || parms->fd < 0)
+		return;
+
+	/* Disable LNBf power */
+	if (dvb_fe_is_satellite(parms->p.current_sys))
+		dvb_fe_sec_voltage(&parms->p, 0, 0);
+
+	close(parms->fd);
+	parms->fd = -1;
+}
 
 void dvb_fe_close(struct dvb_v5_fe_parms *p)
 {

utils/dvb/dvb-fe-tool.c
@@ -272,7 +272,7 @@ int main(int argc, char *argv[])
 	struct dvb_device *dvb;
 	struct dvb_dev_list *dvb_dev;
 	struct dvb_v5_fe_parms *parms;
-	int ret, fe_flags = O_RDWR;
+	int fe_flags = O_RDWR;
 
 #ifdef ENABLE_NLS
 	setlocale (LC_ALL, "");
@@ -307,8 +307,7 @@ int main(int argc, char *argv[])
 	if (!dvb_dev)
 		return -1;
 
-	ret = dvb_dev_open(dvb, dvb_dev->sysname, fe_flags);
-	if (ret < 0)
+	if (!dvb_dev_open(dvb, dvb_dev->sysname, fe_flags))
 		return -1;
 
 	if (delsys) {

utils/dvb/dvbv5-zap.c
@@ -488,13 +488,14 @@ static void get_show_stats(struct arguments *args,
 }
 
 #define BUFLEN (188 * 256)
-static void copy_to_file(int in_fd, int out_fd, int timeout, int silent)
+static void copy_to_file(struct dvb_open_descriptor *in_fd, int out_fd,
+			 int timeout, int silent)
 {
 	char buf[BUFLEN];
 	int r;
 	long long int rc = 0LL;
 	while (timeout_flag == 0) {
-		r = read(in_fd, buf, BUFLEN);
+		r = dvb_dev_read(in_fd, buf, BUFLEN);
 		if (r < 0) {
 			if (errno == EOVERFLOW) {
 				fprintf(stderr, _("buffer overrun\n"));
@@ -631,7 +632,7 @@ static error_t parse_opt(int k, char *optarg, struct argp_state *state)
 
 int do_traffic_monitor(struct arguments *args, struct dvb_device *dvb)
 {
-	int fd, dvr_fd;
+	struct dvb_open_descriptor *fd, *dvr_fd;
 	long long unsigned pidt[0x2001], wait;
 	int packets = 0;
 	struct timeval startt;
@@ -642,27 +643,24 @@ int do_traffic_monitor(struct arguments *args, struct dvb_device *dvb)
 	args->exit_after_tuning = 1;
 	check_frontend(args, parms);
 
-	dvr_fd = dvb_dev_open(dvb, args->dvr_dev, O_RDWR);
-	if (dvr_fd < 0) {
-		PERROR(_("failed opening '%s'"), args->dvr_dev);
+	dvr_fd = dvb_dev_open(dvb, args->dvr_dev, O_RDONLY);
+	if (!dvr_fd)
 		return -1;
-	}
 
-	if (ioctl(dvr_fd, DMX_SET_BUFFER_SIZE, 1024 * 1024) == -1)
-		perror(_("DMX_SET_BUFFER_SIZE failed"));
+	dvb_dev_set_bufsize(dvr_fd, 1024 * 1024);
 
 	fd = dvb_dev_open(dvb, args->demux_dev, O_RDWR);
-	if (fd < 0) {
-		PERROR(_("failed opening '%s'"), args->demux_dev);
-		close(dvr_fd);
+	if (!fd) {
+		dvb_dev_close(dvr_fd);
 		return -1;
 	}
 
 	if (args->silent < 2)
 		fprintf(stderr, _("  dvb_set_pesfilter to 0x2000\n"));
-	if (dvb_set_pesfilter(fd, 0x2000, DMX_PES_OTHER,
-			      DMX_OUT_TS_TAP, 0) < 0) {
-		PERROR(_("couldn't set filter"));
+	if (dvb_dev_dmx_set_pesfilter(fd, 0x2000, DMX_PES_OTHER,
+				      DMX_OUT_TS_TAP, 0) < 0) {
+		dvb_dev_close(dvr_fd);
+		dvb_dev_close(fd);
 		return -1;
 	}
 
@@ -678,7 +676,7 @@ int do_traffic_monitor(struct arguments *args, struct dvb_device *dvb)
 		if (timeout_flag)
 			break;
 
-		if ((r = read(dvr_fd, buffer, BSIZE)) <= 0) {
+		if ((r = dvb_dev_read(dvr_fd, buffer, BSIZE)) <= 0) {
 			if (errno == EOVERFLOW) {
 				struct timeval now;
 				int diff;
@@ -689,7 +687,6 @@ int do_traffic_monitor(struct arguments *args, struct dvb_device *dvb)
 				fprintf(stderr, _("%.2fs: buffer overrun\n"), diff / 1000.);
 				continue;
 			}
-			perror(_("read"));
 			break;
 		}
 		if (r != BSIZE) {
@@ -772,8 +769,8 @@ int do_traffic_monitor(struct arguments *args, struct dvb_device *dvb)
 			}
 		}
 	}
-	close(dvr_fd);
-	dvb_dmx_close(fd);
+	dvb_dev_close(dvr_fd);
+	dvb_dev_close(fd);
 	return 0;
 }
 
@@ -795,9 +792,10 @@ int main(int argc, char **argv)
 	int lnb = -1, idx = -1;
 	int vpid = -1, apid = -1, sid = -1;
 	int pmtpid = 0;
-	int pat_fd = -1, pmt_fd = -1, sid_fd = -1;
-	int audio_fd = -1, video_fd = -1;
-	int dvr_fd = -1, file_fd = -1;
+	struct dvb_open_descriptor *pat_fd = NULL, *pmt_fd = NULL;
+	struct dvb_open_descriptor *sid_fd = NULL, *dvr_fd = NULL;
+	struct dvb_open_descriptor *audio_fd = NULL, *video_fd = NULL;
+	int file_fd = -1;
 	int err = -1;
 	int r;
 	struct dvb_v5_fe_parms *parms = NULL;
@@ -903,8 +901,7 @@ int main(int argc, char **argv)
 	if (!dvb_dev)
 		return -1;
 
-	err = dvb_dev_open(dvb, dvb_dev->sysname, O_RDWR);
-	if (err < 0)
+	if (!dvb_dev_open(dvb, dvb_dev->sysname, O_RDWR))
 		goto err;
 	if (lnb >= 0)
 		parms->lnb = dvb_sat_get_lnb(lnb);
@@ -945,12 +942,12 @@ int main(int argc, char **argv)
 		}
 
 		sid_fd = dvb_dev_open(dvb, args.demux_dev, O_RDWR);
-		if (sid_fd < 0) {
+		if (!sid_fd) {
 			perror(_("opening sid demux failed"));
 			return -1;
 		}
-		pmtpid = dvb_get_pmt_pid(sid_fd, sid);
-		dvb_dmx_close(sid_fd);
+		pmtpid = dvb_dev_dmx_get_pmt_pid(sid_fd, sid);
+		dvb_dev_close(sid_fd);
 		if (pmtpid <= 0) {
 			fprintf(stderr, _("couldn't find pmt-pid for sid %04x\n"),
 				sid);
@@ -959,21 +956,21 @@ int main(int argc, char **argv)
 		}
 
 		pat_fd = dvb_dev_open(dvb, args.demux_dev, O_RDWR);
-		if (pat_fd < 0) {
+		if (!pat_fd) {
 			perror(_("opening pat demux failed"));
 			goto err;
 		}
-		if (dvb_set_pesfilter(pat_fd, 0, DMX_PES_OTHER,
+		if (dvb_dev_dmx_set_pesfilter(pat_fd, 0, DMX_PES_OTHER,
 				args.dvr ? DMX_OUT_TS_TAP : DMX_OUT_DECODER,
 				args.dvr ? 64 * 1024 : 0) < 0)
 			goto err;
 
 		pmt_fd = dvb_dev_open(dvb, args.demux_dev, O_RDWR);
-		if (pmt_fd < 0) {
+		if (!pmt_fd) {
 			perror(_("opening pmt demux failed"));
 			goto err;
 		}
-		if (dvb_set_pesfilter(pmt_fd, pmtpid, DMX_PES_OTHER,
+		if (dvb_dev_dmx_set_pesfilter(pmt_fd, pmtpid, DMX_PES_OTHER,
 				args.dvr ? DMX_OUT_TS_TAP : DMX_OUT_DECODER,
 				args.dvr ? 64 * 1024 : 0) < 0)
 			goto err;
@@ -991,7 +988,7 @@ int main(int argc, char **argv)
 				fprintf(stderr, _("video pid %d\n"), vpid);
 		}
 		video_fd = dvb_dev_open(dvb, args.demux_dev, O_RDWR);
-		if (video_fd < 0) {
+		if (!video_fd) {
 			PERROR(_("failed opening '%s'"), args.demux_dev);
 			goto err;
 		}
@@ -999,13 +996,12 @@ int main(int argc, char **argv)
 		if (args.silent < 2)
 			fprintf(stderr, _("  dvb_set_pesfilter %d\n"), vpid);
 		if (vpid == 0x2000) {
-			if (ioctl(video_fd, DMX_SET_BUFFER_SIZE, 1024 * 1024) == -1)
-				perror(_("DMX_SET_BUFFER_SIZE failed"));
-			if (dvb_set_pesfilter(video_fd, vpid, DMX_PES_OTHER,
+			dvb_dev_set_bufsize(video_fd, 1024 * 1024);
+			if (dvb_dev_dmx_set_pesfilter(video_fd, vpid, DMX_PES_OTHER,
 					      DMX_OUT_TS_TAP, 0) < 0)
 				goto err;
 		} else {
-			if (dvb_set_pesfilter(video_fd, vpid, DMX_PES_VIDEO,
+			if (dvb_dev_dmx_set_pesfilter(video_fd, vpid, DMX_PES_VIDEO,
 				args.dvr ? DMX_OUT_TS_TAP : DMX_OUT_DECODER,
 				args.dvr ? 64 * 1024 : 0) < 0)
 				goto err;
@@ -1016,13 +1012,13 @@ int main(int argc, char **argv)
 		if (args.silent < 2)
 			fprintf(stderr, _("audio pid %d\n"), apid);
 		audio_fd = dvb_dev_open(dvb, args.demux_dev, O_RDWR);
-		if (audio_fd < 0) {
+		if (!audio_fd) {
 			PERROR(_("failed opening '%s'"), args.demux_dev);
 			goto err;
 		}
 		if (args.silent < 2)
 			fprintf(stderr, _("  dvb_set_pesfilter %d\n"), apid);
-		if (dvb_set_pesfilter(audio_fd, apid, DMX_PES_AUDIO,
+		if (dvb_dev_dmx_set_pesfilter(audio_fd, apid, DMX_PES_AUDIO,
 				args.dvr ? DMX_OUT_TS_TAP : DMX_OUT_DECODER,
 				args.dvr ? 64 * 1024 : 0) < 0)
 			goto err;
@@ -1060,7 +1056,7 @@ int main(int argc, char **argv)
 
 		if (file_fd >= 0) {
 			dvr_fd = dvb_dev_open(dvb, args.dvr_dev, O_RDWR);
-			if (dvr_fd < 0) {
+			if (!dvr_fd) {
 				PERROR(_("failed opening '%s'"), args.dvr_dev);
 				goto err;
 			}
@@ -1085,18 +1081,6 @@ int main(int argc, char **argv)
 	err = 0;
 
 err:
-	if (file_fd > 0)
-		dvb_dmx_close(file_fd);
-	if (dvr_fd > 0)
-		close(dvr_fd);
-	if (pat_fd > 0)
-		dvb_dmx_close(pat_fd);
-	if (pmt_fd > 0)
-		dvb_dmx_close(pmt_fd);
-	if (audio_fd > 0)
-		dvb_dmx_close(audio_fd);
-	if (video_fd > 0)
-		dvb_dmx_close(video_fd);
 	if (args.confname)
 		free(args.confname);
 	dvb_dev_free(dvb);

commit f1a04ae008c6c3124c9f866c2878cd1009a25caf

dvbv5-scan: fix a regression introduced by da56d6f1578a
changeset da56d6f1578a ("libdvbv5: create a new API for dvb-demux")
introduced a regression at dvbv5-scan table, causing it to fail
opening the DVB demux filter. Fix it.

Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

utils/dvb/dvbv5-scan.c
@@ -59,7 +59,7 @@ const char *argp_program_version = PROGRAM_NAME " version " V4L_UTILS_VERSION;
 const char *argp_program_bug_address = "Mauro Carvalho Chehab <m.chehab@samsung.com>";
 
 struct arguments {
-	char *confname, *lnb_name, *output, *demux_dev;
+	char *confname, *lnb_name, *output, *demux_dev, *demux_fname;
 	unsigned adapter, n_adapter, adapter_fe, adapter_dmx, frontend, demux, get_detected, get_nit;
 	int lna, lnb, sat_number, freq_bpf;
 	unsigned diseqc_wait, dont_add_new_freqs, timeout_multiply;
@@ -250,9 +250,9 @@ static int run_scan(struct arguments *args, struct dvb_device *dvb)
 		return -2;
 
 	/* FIXME: should be replaced by dvb_dev_open() */
-	dmx_fd = open(args->demux_dev, O_RDWR);
+	dmx_fd = open(args->demux_fname, O_RDWR);
 	if (dmx_fd < 0) {
-		perror(_("openening pat demux failed"));
+		perror(_("opening demux failed"));
 		return -3;
 	}
 
@@ -530,6 +530,7 @@ int main(int argc, char **argv)
 		return -1;
 	}
 	args.demux_dev = dvb_dev->sysname;
+	args.demux_fname = dvb_dev->path;
 
 	if (verbose)
 		fprintf(stderr, _("using demux '%s'\n"), args.demux_dev);