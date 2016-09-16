712e88e654b472161e5a53fe61cfd0112197a223
Mauro Carvalho Chehab <mchehab@s-opensource.com>
2016-09-16 04:11:06 -0300

    media-ctl: Fix a compilation bug introduced by changeset 7a21d66983f7
    options.c:56:9: warning: missing terminating " character
    options.c:56:9: error: missing terminating " character
    options.c:57:71: error: expected ')' before ';' token
    options.c:57:2: warning: passing argument 1 of 'printf' makes pointer from integer without a cast [-Wint-conversion]
    options.c:94:1: error: expected ';' before '}' token
    options.c:42:15: warning: unused variable 'i' [-Wunused-variable]
    
    Clearly, nobody tested this patch before merging it.
    
    Fixes: 7a21d66983f7 ("media-ctl: Print information related to a single entity")
    
    Cc: Sakari Ailus <sakari.ailus@linux.intel.com>
    Cc: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- utils/media-ctl/options.c
+++ utils/media-ctl/options.c
@@ -53,7 +53,7 @@ static void usage(const char *argv0)
 	printf("    --known-mbus-fmts	List known media bus formats and their numeric values\n");
 	printf("-p, --print-topology	Print the device topology. If an entity\n");
 	printf("			is specified through the -e option, print\n");
-	printf("			information for that entity only.\n);
+	printf("			information for that entity only.\n");
 	printf("    --print-dot		Print the device topology as a dot graph\n");
 	printf("-r, --reset		Reset all links to inactive\n");
 	printf("-v, --verbose		Be verbose\n");
