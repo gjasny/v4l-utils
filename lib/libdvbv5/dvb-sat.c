14c215d6f2b0dc477aa5026fe12bd72287bd874a
Mauro Carvalho Chehab <mchehab@s-opensource.com>
2016-12-13 18:10:16 -0200

    dvb-sat: add support for several BrasilSat LNBf models
    There are some LNBf manufactured by BrasilSat that are
    widely used in Brazil. Add support for them, based on what's
    described on this document:
    	http://www.brasilsat.com.br/arquivo/DataSheet1.pdf
    
    I tested myself the custom GVT model.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- lib/libdvbv5/dvb-sat.c
+++ lib/libdvbv5/dvb-sat.c
@@ -143,6 +143,55 @@ static const struct dvb_sat_lnb_priv lnb[] = {
 		.freqrange = {
 			{ 11710, 12751, 10678, 0 }
 		}
+	}, {
+		.desc = {
+			.name = N_("BrasilSat Stacked"),
+			.alias = "STACKED-BRASILSAT",
+		},
+		.freqrange = {
+			{ 10700, 11700, 9710, 0, POLARIZATION_H },
+			{ 10700, 11700, 9750, 0, POLARIZATION_H },
+		},
+	}, {
+		.desc = {
+			.name = N_("BrasilSat Oi"),
+			.alias = "OI-BRASILSAT",
+		},
+		.freqrange = {
+			{ 10950, 11200, 10000, 11700 },
+			{ 11800, 12200, 10445, 0 },
+		}
+	}, {
+		.desc = {
+			.name = N_("BrasilSat Amazonas 1/2 - 3 Oscilators"),
+			.alias = "AMAZONAS",
+		},
+		.freqrange = {
+			{ 11037, 11450, 9670, 0, POLARIZATION_V },
+			{ 11770, 12070, 9922, 0, POLARIZATION_H },
+			{ 10950, 11280, 10000, 0, POLARIZATION_H },
+		},
+	}, {
+		.desc = {
+			.name = N_("BrasilSat Amazonas 1/2 - 2 Oscilators"),
+			.alias = "AMAZONAS",
+		},
+		.freqrange = {
+			{ 11037, 11360, 9670, 0, POLARIZATION_V },
+			{ 11780, 12150, 10000, 0, POLARIZATION_H },
+			{ 10950, 11280, 10000, 0, POLARIZATION_H },
+		},
+	}, {
+		.desc = {
+			.name = N_("BrasilSat custom GVT"),
+			.alias = "GVT-BRASILSAT",
+		},
+		.freqrange = {
+			{ 11010.5, 11067.5, 12860, 0, POLARIZATION_V },
+			{ 11704.0, 11941.0, 13435, 0, POLARIZATION_V },
+			{ 10962.5, 11199.5, 13112, 0, POLARIZATION_H },
+			{ 11704.0, 12188.0, 13138, 0, POLARIZATION_H },
+		},
 	},
 };
 
