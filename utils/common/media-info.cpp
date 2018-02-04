gjasny/v4l-utils

commit ce29414c85f565b0ea97804d7e6ee0d6f4dd1d7d

v4l2-ctl/v4l2-compliance: reuse media info code
The code that displays the media information for an interface
is now centralized in common/media-info.cpp and used by both
v4l2-ctl and v4l2-compliance.

In addition, this code now uses G_TOPOLOGY if available.

Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>

utils/common/media-info.cpp
@@ -1,0 +1,472 @@
+/*
+ * Copyright 2018 Cisco Systems, Inc. and/or its affiliates. All rights reserved.
+ *
+ * This program is free software; you may redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <inttypes.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <ctype.h>
+#include <errno.h>
+#include <sys/ioctl.h>
+#include <sys/time.h>
+#include <sys/sysmacros.h>
+#include <dirent.h>
+
+#include <linux/media.h>
+
+#include <fstream>
+
+static std::string num2s(unsigned num)
+{
+	char buf[10];
+
+	sprintf(buf, "%08x", num);
+	return buf;
+}
+
+typedef struct {
+	unsigned flag;
+	const char *str;
+} flag_def;
+
+static std::string flags2s(unsigned val, const flag_def *def)
+{
+	std::string s;
+
+	while (def->flag) {
+		if (val & def->flag) {
+			if (s.length()) s += ", ";
+			s += def->str;
+			val &= ~def->flag;
+		}
+		def++;
+	}
+	if (val) {
+		if (s.length()) s += ", ";
+		s += num2s(val);
+	}
+	return s;
+}
+
+int mi_get_media_fd(int fd)
+{
+	struct stat sb;
+	int media_fd = -1;
+
+	if (fstat(fd, &sb) == -1) {
+		fprintf(stderr, "failed to stat file\n");
+		return -1;
+	}
+
+	char media_path[100];
+	if (snprintf(media_path, sizeof(media_path), "/sys/dev/char/%d:%d/device",
+		     major(sb.st_rdev), minor(sb.st_rdev)) == -1) {
+		fprintf(stderr, "failed to create media file path\n");
+		return -1;
+	}
+	DIR *dp;
+	struct dirent *ep;
+	dp = opendir(media_path);
+	if (dp == NULL)
+		return -1;
+	media_path[0] = 0;
+	while ((ep = readdir(dp))) {
+		if (!memcmp(ep->d_name, "media", 5) && isdigit(ep->d_name[5])) {
+			std::string devname("/dev/");
+
+			devname += ep->d_name;
+			media_fd = open(devname.c_str(), O_RDWR);
+			break;
+		}
+	}
+	closedir(dp);
+	return media_fd;
+}
+
+bool mi_is_subdevice(int fd)
+{
+	struct stat sb;
+	if (fstat(fd, &sb) == -1) {
+		fprintf(stderr, "failed to stat file\n");
+		exit(1);
+	}
+
+	char uevent_path[100];
+	if (snprintf(uevent_path, sizeof(uevent_path), "/sys/dev/char/%d:%d/uevent",
+		     major(sb.st_rdev), minor(sb.st_rdev)) == -1) {
+		fprintf(stderr, "failed to create uevent file path\n");
+		exit(1);
+	}
+
+	std::ifstream uevent_file(uevent_path);
+	if (uevent_file.fail()) {
+		fprintf(stderr, "failed to open %s\n", uevent_path);
+		exit(1);
+	}
+
+	std::string line;
+
+	while (std::getline(uevent_file, line)) {
+		if (line.compare(0, 8, "DEVNAME="))
+			continue;
+
+		static const char * devnames[] = {
+			"v4l-subdev",
+			"video",
+			"vbi",
+			"radio",
+			"swradio",
+			"v4l-touch",
+			NULL
+		};
+
+		for (size_t i = 0; devnames[i]; i++) {
+			size_t len = strlen(devnames[i]);
+
+			if (!line.compare(8, len, devnames[i]) && isdigit(line[8+len])) {
+				uevent_file.close();
+				return i == 0;
+			}
+		}
+	}
+
+	uevent_file.close();
+
+	fprintf(stderr, "unknown device name\n");
+	exit(1);
+}
+
+static const flag_def entity_flags_def[] = {
+	{ MEDIA_ENT_FL_DEFAULT, "default" },
+	{ MEDIA_ENT_FL_CONNECTOR, "connector" },
+	{ 0, NULL }
+};
+
+static std::string entflags2s(__u32 flags)
+{
+	return flags2s(flags, entity_flags_def);
+}
+
+static const flag_def interface_types_def[] = {
+	{ MEDIA_INTF_T_DVB_FE, "DVB Front End" },
+	{ MEDIA_INTF_T_DVB_DEMUX, "DVB Demuxer" },
+	{ MEDIA_INTF_T_DVB_DVR, "DVB DVR" },
+	{ MEDIA_INTF_T_DVB_CA, "DVB Conditional Access" },
+	{ MEDIA_INTF_T_DVB_NET, "DVB Net" },
+
+	{ MEDIA_INTF_T_V4L_VIDEO, "V4L Video" },
+	{ MEDIA_INTF_T_V4L_VBI, "V4L VBI" },
+	{ MEDIA_INTF_T_V4L_RADIO, "V4L Radio" },
+	{ MEDIA_INTF_T_V4L_SUBDEV, "V4L Sub-Device" },
+	{ MEDIA_INTF_T_V4L_SWRADIO, "V4L Software Defined Radio" },
+	{ MEDIA_INTF_T_V4L_TOUCH, "V4L Touch" },
+
+	{ MEDIA_INTF_T_ALSA_PCM_CAPTURE, "ALSA PCM Capture" },
+	{ MEDIA_INTF_T_ALSA_PCM_PLAYBACK, "ALSA PCM Playback" },
+	{ MEDIA_INTF_T_ALSA_CONTROL, "ALSA Control" },
+	{ MEDIA_INTF_T_ALSA_COMPRESS, "ALSA Compress" },
+	{ MEDIA_INTF_T_ALSA_RAWMIDI, "ALSA Raw MIDI" },
+	{ MEDIA_INTF_T_ALSA_HWDEP, "ALSA HWDEP" },
+	{ MEDIA_INTF_T_ALSA_SEQUENCER, "ALSA Sequencer" },
+	{ MEDIA_INTF_T_ALSA_TIMER, "ALSA Timer" },
+	{ 0, NULL }
+};
+
+static std::string ifacetype2s(__u32 type)
+{
+	for (unsigned i = 0; interface_types_def[i].str; i++)
+		if (type == interface_types_def[i].flag)
+			return interface_types_def[i].str;
+	return "Unknown (" + num2s(type) + ")";
+}
+
+static const flag_def entity_types_def[] = {
+	{ MEDIA_ENT_F_UNKNOWN, "Unknown" },
+	{ MEDIA_ENT_F_DTV_DEMOD, "Digital TV Demodulator" },
+	{ MEDIA_ENT_F_TS_DEMUX, "Transport Stream Demuxer" },
+	{ MEDIA_ENT_F_DTV_CA, "Digital TV Conditional Access" },
+	{ MEDIA_ENT_F_DTV_NET_DECAP, "Digital TV Network ULE/MLE Desencapsulation" },
+	{ MEDIA_ENT_F_IO_DTV, "Digital TV I/O" },
+	{ MEDIA_ENT_F_IO_VBI, "VBI I/O" },
+	{ MEDIA_ENT_F_IO_SWRADIO, "Software Radio I/O" },
+	{ MEDIA_ENT_F_IF_VID_DECODER, "IF-PLL Video Decoder" },
+	{ MEDIA_ENT_F_IF_AUD_DECODER, "IF-PLL Audio Decoder" },
+	{ MEDIA_ENT_F_AUDIO_CAPTURE, "Audio Capture" },
+	{ MEDIA_ENT_F_AUDIO_PLAYBACK, "Audio Playback" },
+	{ MEDIA_ENT_F_AUDIO_MIXER, "Audio Mixer" },
+	{ MEDIA_ENT_F_PROC_VIDEO_COMPOSER, "Video Composer" },
+	{ MEDIA_ENT_F_PROC_VIDEO_PIXEL_FORMATTER, "Video Pixel Formatter" },
+	{ MEDIA_ENT_F_PROC_VIDEO_PIXEL_ENC_CONV, "Video Pixel Encoding Converter" },
+	{ MEDIA_ENT_F_PROC_VIDEO_LUT, "Video Look-Up Table" },
+	{ MEDIA_ENT_F_PROC_VIDEO_SCALER, "Video Scaler" },
+	{ MEDIA_ENT_F_PROC_VIDEO_STATISTICS, "Video Statistics" },
+	{ MEDIA_ENT_F_VID_MUX, "Video Muxer" },
+	{ MEDIA_ENT_F_VID_IF_BRIDGE, "Video Interface Bridge" },
+
+	{ MEDIA_ENT_F_IO_V4L, "V4L2 I/O" },
+	{ MEDIA_ENT_F_CAM_SENSOR, "Camera Sensor" },
+	{ MEDIA_ENT_F_FLASH, "Flash Controller" },
+	{ MEDIA_ENT_F_LENS, "Lens Controller" },
+	{ MEDIA_ENT_F_ATV_DECODER, "Analog Video Decoder" },
+	{ MEDIA_ENT_F_TUNER, "Tuner" },
+	{ MEDIA_ENT_F_V4L2_SUBDEV_UNKNOWN, "Unknown" },
+	{ 0, NULL }
+};
+
+static std::string enttype2s(__u32 type)
+{
+	for (unsigned i = 0; entity_types_def[i].str; i++)
+		if (type == entity_types_def[i].flag)
+			return entity_types_def[i].str;
+	return "Unknown (" + num2s(type) + ")";
+}
+
+static const flag_def pad_flags_def[] = {
+	{ MEDIA_PAD_FL_SINK, "Sink" },
+	{ MEDIA_PAD_FL_SOURCE, "Source" },
+	{ MEDIA_PAD_FL_MUST_CONNECT, "Must Connect" },
+	{ 0, NULL }
+};
+
+static std::string padflags2s(__u32 flags)
+{
+	return flags2s(flags, pad_flags_def);
+}
+
+static const flag_def link_flags_def[] = {
+	{ MEDIA_LNK_FL_ENABLED, "Enabled" },
+	{ MEDIA_LNK_FL_IMMUTABLE, "Immutable" },
+	{ MEDIA_LNK_FL_DYNAMIC, "Dynamic" },
+	{ 0, NULL }
+};
+
+static std::string linkflags2s(__u32 flags)
+{
+	std::string s = flags2s(flags, link_flags_def);
+
+	if (!s.empty())
+		s = ", " + s;
+	switch (flags & MEDIA_LNK_FL_LINK_TYPE) {
+	case MEDIA_LNK_FL_DATA_LINK:
+		return "Data" + s;
+	case MEDIA_LNK_FL_INTERFACE_LINK:
+		return "Interface" + s;
+	default:
+		return "Unknown" + s;
+	}
+}
+
+static bool read_topology(int media_fd, __u32 major, __u32 minor)
+{
+	media_v2_topology topology;
+	unsigned i, j;
+
+	memset(&topology, 0, sizeof(topology));
+	if (ioctl(media_fd, MEDIA_IOC_G_TOPOLOGY, &topology))
+		return false;
+
+	media_v2_entity v2_ents[topology.num_entities];
+	media_v2_interface v2_ifaces[topology.num_interfaces];
+	media_v2_pad v2_pads[topology.num_pads];
+	media_v2_link v2_links[topology.num_links];
+
+	topology.ptr_entities = (__u64)v2_ents;
+	topology.ptr_interfaces = (__u64)v2_ifaces;
+	topology.ptr_pads = (__u64)v2_pads;
+	topology.ptr_links = (__u64)v2_links;
+	if (ioctl(media_fd, MEDIA_IOC_G_TOPOLOGY, &topology))
+		return false;
+	for (i = 0; i < topology.num_interfaces; i++)
+		if (v2_ifaces[i].devnode.major == major &&
+		    v2_ifaces[i].devnode.minor == minor)
+			break;
+	if (i == topology.num_interfaces) {
+		fprintf(stderr, "could not find %d:%d device in topology\n",
+			major, minor);
+		return false;
+	}
+	media_v2_interface &iface = v2_ifaces[i];
+	for (i = 0; i < topology.num_links; i++) {
+		__u32 type = v2_links[i].flags & MEDIA_LNK_FL_LINK_TYPE;
+
+		if (type != MEDIA_LNK_FL_INTERFACE_LINK)
+			continue;
+		if (v2_links[i].source_id == iface.id)
+			break;
+	}
+	if (i == topology.num_links) {
+		fprintf(stderr, "could not find link for interface %u in topology\n",
+			iface.id);
+		return false;
+	}
+	__u32 ent_id = v2_links[i].sink_id;
+	for (i = 0; i < topology.num_entities; i++) {
+		if (v2_ents[i].id == ent_id)
+			break;
+	}
+	if (i == topology.num_entities) {
+		fprintf(stderr, "could not find entity %u in topology\n",
+			ent_id);
+		return false;
+	}
+	media_v2_entity &ent = v2_ents[i];
+
+	printf("Interface Info:\n");
+	printf("\tID               : 0x%08x\n", iface.id);
+	printf("\tType             : %s\n", ifacetype2s(iface.intf_type).c_str());
+	printf("Entity Info:\n");
+	printf("\tID               : 0x%08x (%u)\n", ent.id, ent.id);
+	printf("\tName             : %s\n", ent.name);
+	printf("\tFunction         : %s\n", enttype2s(ent.function).c_str());
+
+	// Yes, I know, lots of nested for-loops. If we get really complex
+	// devices with such large topologies that this becomes too inefficient
+	// then this needs to be changed and we'd need some maps to quickly
+	// look up values.
+	for (i = 0; i < topology.num_pads; i++) {
+		const media_v2_pad &pad = v2_pads[i];
+
+		if (pad.entity_id != ent.id)
+			continue;
+		printf("\tPad 0x%08x   : %s\n",
+		       pad.id, padflags2s(pad.flags).c_str());
+		for (j = 0; j < topology.num_links; j++) {
+			const media_v2_link &link = v2_links[j];
+			__u32 type = link.flags & MEDIA_LNK_FL_LINK_TYPE;
+			__u32 remote_pad;
+			__u32 remote_ent_id = 0;
+			const media_v2_entity *remote_ent = NULL;
+
+			if (type != MEDIA_LNK_FL_DATA_LINK ||
+			    (link.source_id != pad.id && link.sink_id != pad.id))
+				continue;
+			bool is_sink = link.sink_id == pad.id;
+			remote_pad = is_sink ? link.source_id : link.sink_id;
+			for (unsigned k = 0; k < topology.num_pads; k++)
+				if (v2_pads[k].id == remote_pad) {
+					remote_ent_id = v2_pads[k].entity_id;
+					break;
+				}
+			if (!remote_ent_id) {
+				fprintf(stderr, "could not find remote pad 0x%08x in topology\n",
+					remote_pad);
+				return true;
+			}
+			for (unsigned k = 0; k < topology.num_entities; k++)
+				if (v2_ents[k].id == remote_ent_id) {
+					remote_ent = &v2_ents[k];
+					break;
+				}
+			if (!remote_ent) {
+				fprintf(stderr, "could not find remote entity 0x%08x in topology\n",
+					remote_ent_id);
+				return true;
+			}
+			printf("\t  Link 0x%08x: %s remote pad 0x%x of entity '%s': %s\n",
+			       link.id, is_sink ? "from" : "to", remote_pad,
+			       remote_ent->name, linkflags2s(link.flags).c_str());
+		}
+	}
+	return true;
+}
+
+void mi_media_info_for_fd(int media_fd, int fd)
+{
+	struct media_device_info mdinfo;
+	struct stat sb;
+
+	if (ioctl(media_fd, MEDIA_IOC_DEVICE_INFO, &mdinfo))
+		return;
+
+	struct media_entity_desc ent;
+	bool found = false;
+
+	printf("Media Driver Info:\n");
+	printf("\tDriver name      : %s\n", mdinfo.driver);
+	printf("\tModel            : %s\n", mdinfo.model);
+	printf("\tSerial           : %s\n", mdinfo.serial);
+	printf("\tBus info         : %s\n", mdinfo.bus_info);
+	printf("\tMedia version    : %d.%d.%d\n",
+	       mdinfo.media_version >> 16,
+	       (mdinfo.media_version >> 8) & 0xff,
+	       mdinfo.media_version & 0xff);
+	printf("\tHardware revision: 0x%08x (%u)\n",
+	       mdinfo.hw_revision, mdinfo.hw_revision);
+	printf("\tDriver version   : %d.%d.%d\n",
+	       mdinfo.driver_version >> 16,
+	       (mdinfo.driver_version >> 8) & 0xff,
+	       mdinfo.driver_version & 0xff);
+
+	if (fd < 0)
+		return;
+
+	if (fstat(fd, &sb) == -1) {
+		fprintf(stderr, "failed to stat file\n");
+		exit(1);
+	}
+
+	if (read_topology(media_fd, major(sb.st_rdev), minor(sb.st_rdev)))
+		return;
+
+	memset(&ent, 0, sizeof(ent));
+	ent.id = MEDIA_ENT_ID_FLAG_NEXT;
+	while (!ioctl(media_fd, MEDIA_IOC_ENUM_ENTITIES, &ent)) {
+		if (ent.dev.major == major(sb.st_rdev) &&
+		    ent.dev.minor == minor(sb.st_rdev)) {
+			found = true;
+			break;
+		}
+		ent.id |= MEDIA_ENT_ID_FLAG_NEXT;
+	}
+	if (!found)
+		return;
+
+	printf("Entity Info:\n");
+	printf("\tID               : %u\n", ent.id);
+	printf("\tName             : %s\n", ent.name);
+	printf("\tType             : %s\n", enttype2s(ent.type).c_str());
+	if (ent.flags)
+		printf("\tFlags            : %s\n", entflags2s(ent.flags).c_str());
+	if (ent.flags & MEDIA_ENT_FL_DEFAULT) {
+		printf("\tMajor            : %u\n", ent.dev.major);
+		printf("\tMinor            : %u\n", ent.dev.minor);
+	}
+
+	struct media_links_enum links_enum;
+	struct media_pad_desc pads[ent.pads];
+	struct media_link_desc links[ent.links];
+
+	memset(&links_enum, 0, sizeof(links_enum));
+	links_enum.entity = ent.id;
+	links_enum.pads = pads;
+	links_enum.links = links;
+	if (ioctl(media_fd, MEDIA_IOC_ENUM_LINKS, &links_enum))
+		return;
+
+	for (unsigned i = 0; i < ent.pads; i++)
+		printf("\tPad              : %u: %s\n", pads[i].index,
+		       padflags2s(pads[i].flags).c_str());
+	for (unsigned i = 0; i < ent.links; i++)
+		printf("\tLink             : %u->%u: %s\n",
+		       links[i].source.entity,
+		       links[i].sink.entity,
+		       linkflags2s(links[i].flags).c_str());
+}

utils/common/media-info.h
@@ -1,0 +1,33 @@
+/*
+ * Copyright 2018 Cisco Systems, Inc. and/or its affiliates. All rights reserved.
+ *
+ * This program is free software; you may redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef _MEDIA_INFO_H
+#define _MEDIA_INFO_H
+
+/*
+ * For a given device fd return the corresponding media device
+ * or -1 if there is none.
+ */
+int mi_get_media_fd(int fd);
+
+/*
+ * Test if the given fd corresponds to a sub-device.
+ */
+bool mi_is_subdevice(int fd);
+void mi_media_info_for_fd(int media_fd, int fd);
+
+#endif

commit 0b0cb3188d953c3a8f706ebf49da83b98be5dce6

v4l2-compliance/v4l2-ctl: more device detection improvements
More improvements in detecting the type of a device.

Also fix a bug introduced in commit ce29414c85
("v4l2-ctl/v4l2-compliance: reuse media info code") that broke
the v4l-subdev tests in v4l2-compliance.

Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>

utils/common/media-info.cpp
@@ -223,14 +223,14 @@ static std::string linkflags2s(__u32 flags)
 	}
 }
 
-static bool read_topology(int media_fd, __u32 major, __u32 minor)
+static __u32 read_topology(int media_fd, __u32 major, __u32 minor)
 {
 	media_v2_topology topology;
 	unsigned i, j;
 
 	memset(&topology, 0, sizeof(topology));
 	if (ioctl(media_fd, MEDIA_IOC_G_TOPOLOGY, &topology))
-		return false;
+		return 0;
 
 	media_v2_entity v2_ents[topology.num_entities];
 	media_v2_interface v2_ifaces[topology.num_interfaces];
@@ -242,7 +242,7 @@ static bool read_topology(int media_fd, __u32 major, __u32 minor)
 	topology.ptr_pads = (__u64)v2_pads;
 	topology.ptr_links = (__u64)v2_links;
 	if (ioctl(media_fd, MEDIA_IOC_G_TOPOLOGY, &topology))
-		return false;
+		return 0;
 	for (i = 0; i < topology.num_interfaces; i++)
 		if (v2_ifaces[i].devnode.major == major &&
 		    v2_ifaces[i].devnode.minor == minor)
@@ -250,7 +250,7 @@ static bool read_topology(int media_fd, __u32 major, __u32 minor)
 	if (i == topology.num_interfaces) {
 		fprintf(stderr, "could not find %d:%d device in topology\n",
 			major, minor);
-		return false;
+		return 0;
 	}
 	media_v2_interface &iface = v2_ifaces[i];
 	for (i = 0; i < topology.num_links; i++) {
@@ -264,7 +264,7 @@ static bool read_topology(int media_fd, __u32 major, __u32 minor)
 	if (i == topology.num_links) {
 		fprintf(stderr, "could not find link for interface %u in topology\n",
 			iface.id);
-		return false;
+		return 0;
 	}
 	__u32 ent_id = v2_links[i].sink_id;
 	for (i = 0; i < topology.num_entities; i++) {
@@ -274,7 +274,7 @@ static bool read_topology(int media_fd, __u32 major, __u32 minor)
 	if (i == topology.num_entities) {
 		fprintf(stderr, "could not find entity %u in topology\n",
 			ent_id);
-		return false;
+		return 0;
 	}
 	media_v2_entity &ent = v2_ents[i];
 
@@ -317,7 +317,7 @@ static bool read_topology(int media_fd, __u32 major, __u32 minor)
 			if (!remote_ent_id) {
 				fprintf(stderr, "could not find remote pad 0x%08x in topology\n",
 					remote_pad);
-				return true;
+				return ent.id;
 			}
 			for (unsigned k = 0; k < topology.num_entities; k++)
 				if (v2_ents[k].id == remote_ent_id) {
@@ -327,23 +327,24 @@ static bool read_topology(int media_fd, __u32 major, __u32 minor)
 			if (!remote_ent) {
 				fprintf(stderr, "could not find remote entity 0x%08x in topology\n",
 					remote_ent_id);
-				return true;
+				return ent.id;
 			}
 			printf("\t  Link 0x%08x: %s remote pad 0x%x of entity '%s': %s\n",
 			       link.id, is_sink ? "from" : "to", remote_pad,
 			       remote_ent->name, linkflags2s(link.flags).c_str());
 		}
 	}
-	return true;
+	return ent.id;
 }
 
-void mi_media_info_for_fd(int media_fd, int fd)
+__u32 mi_media_info_for_fd(int media_fd, int fd)
 {
 	struct media_device_info mdinfo;
 	struct stat sb;
+	__u32 ent_id = 0;
 
 	if (ioctl(media_fd, MEDIA_IOC_DEVICE_INFO, &mdinfo))
-		return;
+		return 0;
 
 	struct media_entity_desc ent;
 	bool found = false;
@@ -365,15 +366,16 @@ void mi_media_info_for_fd(int media_fd, int fd)
 	       mdinfo.driver_version & 0xff);
 
 	if (fd < 0)
-		return;
+		return 0;
 
 	if (fstat(fd, &sb) == -1) {
 		fprintf(stderr, "failed to stat file\n");
 		exit(1);
 	}
 
-	if (read_topology(media_fd, major(sb.st_rdev), minor(sb.st_rdev)))
-		return;
+	ent_id = read_topology(media_fd, major(sb.st_rdev), minor(sb.st_rdev));
+	if (ent_id)
+		return ent_id;
 
 	memset(&ent, 0, sizeof(ent));
 	ent.id = MEDIA_ENT_ID_FLAG_NEXT;
@@ -386,7 +388,7 @@ void mi_media_info_for_fd(int media_fd, int fd)
 		ent.id |= MEDIA_ENT_ID_FLAG_NEXT;
 	}
 	if (!found)
-		return;
+		return 0;
 
 	printf("Entity Info:\n");
 	printf("\tID               : %u\n", ent.id);
@@ -408,7 +410,7 @@ void mi_media_info_for_fd(int media_fd, int fd)
 	links_enum.pads = pads;
 	links_enum.links = links;
 	if (ioctl(media_fd, MEDIA_IOC_ENUM_LINKS, &links_enum))
-		return;
+		return ent.id;
 
 	for (unsigned i = 0; i < ent.pads; i++)
 		printf("\tPad              : %u: %s\n", pads[i].index,
@@ -418,4 +420,5 @@ void mi_media_info_for_fd(int media_fd, int fd)
 		       links[i].source.entity,
 		       links[i].sink.entity,
 		       linkflags2s(links[i].flags).c_str());
+	return ent.id;
 }

utils/common/media-info.h
@@ -27,7 +27,8 @@ int mi_get_media_fd(int fd);
 /*
  * Show media controller information media_fd and (if >= 0) the
  * corresponsing entity/interface information for the fd.
+ * Return the entity ID of fd.
  */
-void mi_media_info_for_fd(int media_fd, int fd);
+__u32 mi_media_info_for_fd(int media_fd, int fd);
 
 #endif

utils/common/v4l2-info.cpp
@@ -30,7 +30,7 @@
 #include <sys/sysmacros.h>
 #include <dirent.h>
 
-#include <linux/videodev2.h>
+#include <v4l2-info.h>
 
 #include <fstream>
 
@@ -45,13 +45,12 @@ static std::string num2s(unsigned num, bool is_hex = true)
 	return buf;
 }
 
-bool v4l2_is_subdevice(int fd)
+v4l2_type v4l2_detect_type(const char *device)
 {
 	struct stat sb;
-	if (fstat(fd, &sb) == -1) {
-		fprintf(stderr, "failed to stat file\n");
-		exit(1);
-	}
+
+	if (stat(device, &sb) == -1)
+		return V4L2_TYPE_CANT_STAT;
 
 	std::string uevent_path("/sys/dev/char/");
 
@@ -59,10 +58,8 @@ bool v4l2_is_subdevice(int fd)
 		num2s(minor(sb.st_rdev), false) + "/uevent";
 
 	std::ifstream uevent_file(uevent_path);
-	if (uevent_file.fail()) {
-		fprintf(stderr, "failed to open %s\n", uevent_path.c_str());
-		exit(1);
-	}
+	if (uevent_file.fail())
+		return V4L2_TYPE_UNKNOWN;
 
 	std::string line;
 
@@ -70,30 +67,32 @@ bool v4l2_is_subdevice(int fd)
 		if (line.compare(0, 8, "DEVNAME="))
 			continue;
 
-		static const char *devnames[] = {
-			"v4l-subdev",
-			"video",
-			"vbi",
-			"radio",
-			"swradio",
-			"v4l-touch",
-			NULL
+		static struct {
+			const char *devname;
+			enum v4l2_type type;
+		} devtypes[] = {
+			{ "video", V4L2_TYPE_VIDEO },
+			{ "vbi", V4L2_TYPE_VBI },
+			{ "radio", V4L2_TYPE_RADIO },
+			{ "swradio", V4L2_TYPE_SDR },
+			{ "v4l-subdev", V4L2_TYPE_SUBDEV },
+			{ "v4l-touch", V4L2_TYPE_TOUCH },
+			{ NULL, V4L2_TYPE_UNKNOWN }
 		};
 
-		for (size_t i = 0; devnames[i]; i++) {
-			size_t len = strlen(devnames[i]);
+		for (size_t i = 0; devtypes[i].devname; i++) {
+			const char *devname = devtypes[i].devname;
+			size_t len = strlen(devname);
 
-			if (!line.compare(8, len, devnames[i]) && isdigit(line[8+len])) {
+			if (!line.compare(8, len, devname) && isdigit(line[8+len])) {
 				uevent_file.close();
-				return i == 0;
+				return devtypes[i].type;
 			}
 		}
 	}
 
 	uevent_file.close();
-
-	fprintf(stderr, "unknown device name\n");
-	exit(1);
+	return V4L2_TYPE_UNKNOWN;
 }
 
 typedef struct {

utils/common/v4l2-info.h
@@ -18,10 +18,25 @@
 #ifndef _V4L2_INFO_H
 #define _V4L2_INFO_H
 
+#include <string>
+#include <linux/videodev2.h>
+
+enum v4l2_type {
+	V4L2_TYPE_CANT_STAT,
+	V4L2_TYPE_UNKNOWN,
+	V4L2_TYPE_VIDEO,
+	V4L2_TYPE_VBI,
+	V4L2_TYPE_RADIO,
+	V4L2_TYPE_SDR,
+	V4L2_TYPE_TOUCH,
+	V4L2_TYPE_SUBDEV,
+	V4L2_TYPE_MEDIA,
+};
+
 /*
- * Test if the given fd corresponds to a sub-device.
+ * Detect what type the device is.
  */
-bool v4l2_is_subdevice(int fd);
+v4l2_type v4l2_detect_type(const char *device);
 
 /* Print capability information */
 void v4l2_info_capability(const v4l2_capability &cap);

utils/v4l2-compliance/v4l2-compliance.cpp
@@ -626,28 +626,17 @@ static const char *make_devname(const char *device, const char *devname)
 	return device;
 }
 
-enum node_type {
-	TYPE_UNKNOWN,
-	TYPE_VIDEO,
-	TYPE_VBI,
-	TYPE_RADIO,
-	TYPE_SDR,
-	TYPE_TOUCH,
-	TYPE_SUBDEV,
-	TYPE_MEDIA,
-};
-
-static void test(struct node &node, struct node &expbuf_node, node_type type,
+static void test(struct node &node, struct node &expbuf_node, v4l2_type type,
 		 unsigned frame_count)
 {
 	struct node node2;
 	struct v4l2_capability vcap;		/* list_cap */
 
-	node.is_video = type == TYPE_VIDEO;
-	node.is_vbi = type == TYPE_VBI;
-	node.is_radio = type == TYPE_RADIO;
-	node.is_sdr = type == TYPE_SDR;
-	node.is_touch = type == TYPE_TOUCH;
+	node.is_video = type == V4L2_TYPE_VIDEO;
+	node.is_vbi = type == V4L2_TYPE_VBI;
+	node.is_radio = type == V4L2_TYPE_RADIO;
+	node.is_sdr = type == V4L2_TYPE_SDR;
+	node.is_touch = type == V4L2_TYPE_TOUCH;
 
 	if (node.is_v4l2())
 		doioctl(&node, VIDIOC_QUERYCAP, &vcap);
@@ -704,10 +693,32 @@ static void test(struct node &node, struct node &expbuf_node, node_type type,
 			}
 		}
 	}
+
+	__u32 ent_id = 0;
+
 	if (node.is_media())
-		mi_media_info_for_fd(node.g_fd(), -1);
+		ent_id = mi_media_info_for_fd(node.g_fd(), -1);
 	else if (media_fd >= 0)
-		mi_media_info_for_fd(media_fd, node.g_fd());
+		ent_id = mi_media_info_for_fd(media_fd, node.g_fd());
+
+	if (ent_id) {
+		memset(&node.entity, 0, sizeof(node.entity));
+		node.entity.id = ent_id;
+		if (!ioctl(media_fd, MEDIA_IOC_ENUM_ENTITIES, &node.entity)) {
+			struct media_links_enum links_enum;
+
+			node.pads = new media_pad_desc[node.entity.pads];
+			node.links = new media_link_desc[node.entity.links];	
+			memset(&links_enum, 0, sizeof(links_enum));
+			links_enum.entity = ent_id;
+			links_enum.pads = node.pads;
+			links_enum.links = node.links;
+			if (ioctl(media_fd, MEDIA_IOC_ENUM_LINKS, &links_enum))
+				node.entity.id = 0;
+		} else {
+			node.entity.id = 0;
+		}
+	}
 
 	printf("\nCompliance test for device %s%s:\n\n",
 			node.device, node.g_direct() ? "" : " (using libv4l2)");
@@ -732,11 +743,11 @@ static void test(struct node &node, struct node &expbuf_node, node_type type,
 	printf("Allow for multiple opens:\n");
 	node2 = node;
 	switch (type) {
-	case TYPE_SUBDEV:
+	case V4L2_TYPE_SUBDEV:
 		printf("\ttest second %s open: %s\n", node.device,
 		       ok(node2.subdev_open(node.device, false) >= 0 ? 0 : errno));
 		break;
-	case TYPE_MEDIA:
+	case V4L2_TYPE_MEDIA:
 		printf("\ttest second %s open: %s\n", node.device,
 		       ok(node2.media_open(node.device, false) >= 0 ? 0 : errno));
 		if (node2.g_fd() >= 0)
@@ -1038,7 +1049,7 @@ int main(int argc, char **argv)
 {
 	int i;
 	struct node node;
-	enum node_type type = TYPE_UNKNOWN;
+	v4l2_type type = V4L2_TYPE_UNKNOWN;
 	struct node expbuf_node;
 
 	/* command args */
@@ -1077,31 +1088,25 @@ int main(int argc, char **argv)
 			return 0;
 		case OptSetDevice:
 			device = make_devname(optarg, "video");
-			type = TYPE_VIDEO;
 			break;
 		case OptSetVbiDevice:
 			device = make_devname(optarg, "vbi");
-			type = TYPE_VBI;
 			break;
 		case OptSetRadioDevice:
 			device = make_devname(optarg, "radio");
-			type = TYPE_RADIO;
 			break;
 		case OptSetSWRadioDevice:
 			device = make_devname(optarg, "swradio");
-			type = TYPE_SDR;
 			break;
 		case OptSetTouchDevice:
 			device = make_devname(optarg, "v4l-touch");
-			type = TYPE_TOUCH;
 			break;
 		case OptSetSubDevDevice:
 			device = make_devname(optarg, "v4l-subdev");
-			type = TYPE_SUBDEV;
 			break;
 		case OptSetMediaDevice:
 			device = make_devname(optarg, "media");
-			type = TYPE_MEDIA;
+			type = V4L2_TYPE_MEDIA;
 			break;
 		case OptSetExpBufDevice:
 			expbuf_device = optarg;
@@ -1174,24 +1179,37 @@ int main(int argc, char **argv)
 		}
 	}
 	if (optind < argc) {
-		printf("unknown arguments: ");
+		fprintf(stderr, "unknown arguments: ");
 		while (optind < argc)
-			printf("%s ", argv[optind++]);
-		printf("\n");
+			fprintf(stderr, "%s ", argv[optind++]);
+		fprintf(stderr, "\n");
 		usage();
 		return 1;
 	}
 	bool direct = !options[OptUseWrapper];
 	int fd;
 
+	if (type == V4L2_TYPE_UNKNOWN)
+		type = v4l2_detect_type(device);
+	if (type == V4L2_TYPE_CANT_STAT) {
+		fprintf(stderr, "Cannot open device %s, exiting.\n",
+			device);
+		exit(1);
+	}
+	if (type == V4L2_TYPE_UNKNOWN) {
+		fprintf(stderr, "Unable to detect what device %s is, exiting.\n",
+			device);
+		exit(1);
+	}
+
 	node.device = device;
 	node.s_trace(options[OptTrace]);
 	switch (type) {
-	case TYPE_MEDIA:
+	case V4L2_TYPE_MEDIA:
 		node.s_direct(true);
 		fd = node.media_open(device, false);
 		break;
-	case TYPE_SUBDEV:
+	case V4L2_TYPE_SUBDEV:
 		node.s_direct(true);
 		fd = node.subdev_open(device, false);
 		break;

utils/v4l2-ctl/v4l2-ctl.cpp
@@ -976,13 +976,26 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
+	v4l2_type type = v4l2_detect_type(device);
+	if (type == V4L2_TYPE_CANT_STAT) {
+		fprintf(stderr, "Cannot open device %s, exiting.\n",
+			device);
+		exit(1);
+	}
+	if (type == V4L2_TYPE_UNKNOWN) {
+		fprintf(stderr, "Unable to detect what device %s is, exiting.\n",
+			device);
+		exit(1);
+	}
+	is_subdev = type == V4L2_TYPE_SUBDEV;
+
 	if ((fd = open(device, O_RDWR)) < 0) {
 		fprintf(stderr, "Failed to open %s: %s\n", device,
 			strerror(errno));
 		exit(1);
 	}
 	verbose = options[OptVerbose];
-	is_subdev = v4l2_is_subdevice(fd);
+
 	if (is_subdev)
 		options[OptUseWrapper] = 0;
 