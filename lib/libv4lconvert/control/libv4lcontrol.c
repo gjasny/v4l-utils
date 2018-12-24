gjasny/v4l-utils

commit 380fe7d4548a99bfcfc1594b6f0b3dd2369978f1

libv4lconvert: fix compiler warning
  CC       control/libv4lconvert_la-libv4lcontrol.lo
control/libv4lcontrol.c: In function ‘v4lcontrol_get_usb_info’:
control/libv4lcontrol.c:391:13: warning: In the GNU C Library, "minor" is defined
 by <sys/sysmacros.h>. For historical compatibility, it is
 currently defined by <sys/types.h> as well, but we plan to
 remove this soon. To use "minor", include <sys/sysmacros.h>
 directly. If you did not intend to use a system-defined macro
 "minor", you should undefine it after including <sys/types.h>.
       c == '\n' && minor_dev == minor(st.st_rdev))
             ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

Dropped the '#if defined(MAJOR_IN_SYSMACROS)' to ensure that sysmacros.h
is always included. It seems that #if is no longer needed. At least,
nobody complained when I removed it in other places earlier :-)

Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>

lib/libv4lconvert/control/libv4lcontrol.c
@@ -20,9 +20,7 @@
  */
 
 #include <sys/types.h>
-#if defined(MAJOR_IN_SYSMACROS)
 #include <sys/sysmacros.h>
-#endif
 #include <sys/mman.h>
 #include <fcntl.h>
 #include <sys/stat.h>

commit 49cfdcff966f578aa831086332c5d1ef974459e2

Conditional sys/sysmacros.h inclusion
Commit 380fe7d4548a99bfcfc1594b6f0b3dd2369978f1 broke the functionality on
OpenBSD as it has no sys/sysmacros.h. So replicate commit
08572e7db2120bc45db732d02409dfd3346b8e51 but use explicit OS checks instead of
AC_HEADER_MAJOR.

Signed-off-by: Ingo Feinerer <feinerer@logic.at>
Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>

lib/libv4lconvert/control/libv4lcontrol.c
@@ -20,7 +20,9 @@
  */
 
 #include <sys/types.h>
+#ifndef __OpenBSD__
 #include <sys/sysmacros.h>
+#endif
 #include <sys/mman.h>
 #include <fcntl.h>
 #include <sys/stat.h>