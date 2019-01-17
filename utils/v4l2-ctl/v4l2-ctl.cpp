gjasny/v4l-utils

commit b3f1cf6b85198e9da3029dcc20654ab7bb85c577

v4l2-compliance/v4l2-ctl: support V4L2_CTRL_FLAG_MODIFY_LAYOUT and metadata
Add support for V4L2_CTRL_FLAG_MODIFY_LAYOUT and for the new meta
data capability/buffertype.

Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>

utils/v4l2-ctl/v4l2-ctl.cpp
@@ -81,6 +81,7 @@ static struct option long_options[] = {
 	{"help-overlay", no_argument, 0, OptHelpOverlay},
 	{"help-vbi", no_argument, 0, OptHelpVbi},
 	{"help-sdr", no_argument, 0, OptHelpSdr},
+	{"help-meta", no_argument, 0, OptHelpMeta},
 	{"help-selection", no_argument, 0, OptHelpSelection},
 	{"help-misc", no_argument, 0, OptHelpMisc},
 	{"help-streaming", no_argument, 0, OptHelpStreaming},
@@ -112,6 +113,7 @@ static struct option long_options[] = {
 	{"list-formats-sdr", no_argument, 0, OptListSdrFormats},
 	{"list-formats-sdr-out", no_argument, 0, OptListSdrOutFormats},
 	{"list-formats-out", no_argument, 0, OptListOutFormats},
+	{"list-formats-meta", no_argument, 0, OptListMetaFormats},
 	{"list-fields-out", no_argument, 0, OptListOutFields},
 	{"clear-clips", no_argument, 0, OptClearClips},
 	{"clear-bitmap", no_argument, 0, OptClearBitmap},
@@ -158,6 +160,9 @@ static struct option long_options[] = {
 	{"get-fmt-sdr-out", no_argument, 0, OptGetSdrOutFormat},
 	{"set-fmt-sdr-out", required_argument, 0, OptSetSdrOutFormat},
 	{"try-fmt-sdr-out", required_argument, 0, OptTrySdrOutFormat},
+	{"get-fmt-meta", no_argument, 0, OptGetMetaFormat},
+	{"set-fmt-meta", required_argument, 0, OptSetMetaFormat},
+	{"try-fmt-meta", required_argument, 0, OptTryMetaFormat},
 	{"get-sliced-vbi-cap", no_argument, 0, OptGetSlicedVbiCap},
 	{"get-sliced-vbi-out-cap", no_argument, 0, OptGetSlicedVbiOutCap},
 	{"get-fbuf", no_argument, 0, OptGetFBuf},
@@ -213,6 +218,7 @@ static struct option long_options[] = {
 	{"list-buffers-sliced-vbi-out", no_argument, 0, OptListBuffersSlicedVbiOut},
 	{"list-buffers-sdr", no_argument, 0, OptListBuffersSdr},
 	{"list-buffers-sdr-out", no_argument, 0, OptListBuffersSdrOut},
+	{"list-buffers-meta", no_argument, 0, OptListBuffersMeta},
 	{"stream-count", required_argument, 0, OptStreamCount},
 	{"stream-skip", required_argument, 0, OptStreamSkip},
 	{"stream-loop", no_argument, 0, OptStreamLoop},
@@ -259,6 +265,7 @@ static void usage_all(void)
        overlay_usage();
        vbi_usage();
        sdr_usage();
+       meta_usage();
        selection_usage();
        misc_usage();
        streaming_usage();
@@ -332,6 +339,8 @@ std::string buftype2s(int type)
 		return "SDR Capture";
 	case V4L2_BUF_TYPE_SDR_OUTPUT:
 		return "SDR Output";
+	case V4L2_BUF_TYPE_META_CAPTURE:
+		return "Metadata Capture";
 	default:
 		return "Unknown (" + num2s(type) + ")";
 	}
@@ -698,6 +707,10 @@ void printfmt(const struct v4l2_format &vfmt)
 		printf("\tSample Format   : %s\n", fcc2s(vfmt.fmt.sdr.pixelformat).c_str());
 		printf("\tBuffer Size     : %u\n", vfmt.fmt.sdr.buffersize);
 		break;
+	case V4L2_BUF_TYPE_META_CAPTURE:
+		printf("\tSample Format   : %s\n", fcc2s(vfmt.fmt.meta.dataformat).c_str());
+		printf("\tBuffer Size     : %u\n", vfmt.fmt.meta.buffersize);
+		break;
 	}
 }
 
@@ -767,6 +780,8 @@ static std::string cap2s(unsigned cap)
 		s += "\t\tSDR Capture\n";
 	if (cap & V4L2_CAP_SDR_OUTPUT)
 		s += "\t\tSDR Output\n";
+	if (cap & V4L2_CAP_META_CAPTURE)
+		s += "\t\tMetadata Capture\n";
 	if (cap & V4L2_CAP_TUNER)
 		s += "\t\tTuner\n";
 	if (cap & V4L2_CAP_TOUCH)
@@ -1202,6 +1217,9 @@ int main(int argc, char **argv)
 		case OptHelpSdr:
 			sdr_usage();
 			return 0;
+		case OptHelpMeta:
+			meta_usage();
+			return 0;
 		case OptHelpSelection:
 			selection_usage();
 			return 0;
@@ -1268,6 +1286,7 @@ int main(int argc, char **argv)
 			overlay_cmd(ch, optarg);
 			vbi_cmd(ch, optarg);
 			sdr_cmd(ch, optarg);
+			meta_cmd(ch, optarg);
 			selection_cmd(ch, optarg);
 			misc_cmd(ch, optarg);
 			streaming_cmd(ch, optarg);
@@ -1406,6 +1425,7 @@ int main(int argc, char **argv)
 	overlay_set(fd);
 	vbi_set(fd);
 	sdr_set(fd);
+	meta_set(fd);
 	selection_set(fd);
 	streaming_set(fd, out_fd);
 	misc_set(fd);
@@ -1422,6 +1442,7 @@ int main(int argc, char **argv)
 	overlay_get(fd);
 	vbi_get(fd);
 	sdr_get(fd);
+	meta_get(fd);
 	selection_get(fd);
 	misc_get(fd);
 	edid_get(fd);
@@ -1436,6 +1457,7 @@ int main(int argc, char **argv)
 	overlay_list(fd);
 	vbi_list(fd);
 	sdr_list(fd);
+	meta_list(fd);
 	streaming_list(fd, out_fd);
 
 	if (options[OptWaitForEvent]) {

utils/common/v4l-helpers.h
@@ -389,6 +389,11 @@ static inline bool v4l_has_sdr_out(const struct v4l_fd *f)
 	return v4l_g_caps(f) & V4L2_CAP_SDR_OUTPUT;
 }
 
+static inline bool v4l_has_meta_cap(const struct v4l_fd *f)
+{
+	return v4l_g_caps(f) & V4L2_CAP_META_CAPTURE;
+}
+
 static inline bool v4l_has_touch(const struct v4l_fd *f)
 {
 	return v4l_g_caps(f) & V4L2_CAP_TOUCH;
@@ -435,6 +440,8 @@ static inline __u32 v4l_determine_type(const struct v4l_fd *f)
 		return V4L2_BUF_TYPE_SDR_CAPTURE;
 	if (v4l_has_sdr_out(f))
 		return V4L2_BUF_TYPE_SDR_OUTPUT;
+	if (v4l_has_meta_cap(f))
+		return V4L2_BUF_TYPE_META_CAPTURE;
 	return 0;
 }
 
@@ -582,6 +589,9 @@ static inline void v4l_format_s_pixelformat(struct v4l2_format *fmt, __u32 pixel
 	case V4L2_BUF_TYPE_VBI_OUTPUT:
 		fmt->fmt.vbi.sample_format = pixelformat;
 		break;
+	case V4L2_BUF_TYPE_META_CAPTURE:
+		fmt->fmt.meta.dataformat = pixelformat;
+		break;
 	}
 }
 
@@ -600,6 +610,8 @@ static inline __u32 v4l_format_g_pixelformat(const struct v4l2_format *fmt)
 	case V4L2_BUF_TYPE_VBI_CAPTURE:
 	case V4L2_BUF_TYPE_VBI_OUTPUT:
 		return fmt->fmt.vbi.sample_format;
+	case V4L2_BUF_TYPE_META_CAPTURE:
+		return fmt->fmt.meta.dataformat;
 	default:
 		return 0;
 	}
@@ -928,6 +940,8 @@ v4l_format_g_sizeimage(const struct v4l2_format *fmt, unsigned plane)
 	case V4L2_BUF_TYPE_SDR_CAPTURE:
 	case V4L2_BUF_TYPE_SDR_OUTPUT:
 		return plane ? 0 : fmt->fmt.sdr.buffersize;
+	case V4L2_BUF_TYPE_META_CAPTURE:
+		return plane ? 0 : fmt->fmt.meta.buffersize;
 	default:
 		return 0;
 	}
@@ -1044,6 +1058,11 @@ static inline bool v4l_type_is_sdr(unsigned type)
 {
        return type == V4L2_BUF_TYPE_SDR_CAPTURE ||
 	      type == V4L2_BUF_TYPE_SDR_OUTPUT;
+}
+
+static inline bool v4l_type_is_meta(unsigned type)
+{
+       return type == V4L2_BUF_TYPE_META_CAPTURE;
 }
 
 static inline unsigned v4l_type_invert(unsigned type)

utils/v4l2-compliance/v4l2-compliance.cpp
@@ -214,6 +214,8 @@ std::string cap2s(unsigned cap)
 		s += "\t\tSDR Capture\n";
 	if (cap & V4L2_CAP_SDR_OUTPUT)
 		s += "\t\tSDR Output\n";
+	if (cap & V4L2_CAP_META_CAPTURE)
+		s += "\t\tMetadata Capture\n";
 	if (cap & V4L2_CAP_TOUCH)
 		s += "\t\tTouch Device\n";
 	if (cap & V4L2_CAP_TUNER)
@@ -279,6 +281,8 @@ std::string buftype2s(int type)
 		return "SDR Capture";
 	case V4L2_BUF_TYPE_SDR_OUTPUT:
 		return "SDR Output";
+	case V4L2_BUF_TYPE_META_CAPTURE:
+		return "Metadata Capture";
 	case V4L2_BUF_TYPE_PRIVATE:
 		return "Private";
 	default:
@@ -517,7 +521,7 @@ static int testCap(struct node *node)
 	const __u32 input_caps = V4L2_CAP_VIDEO_CAPTURE | V4L2_CAP_VIDEO_OVERLAY |
 			V4L2_CAP_VBI_CAPTURE | V4L2_CAP_SLICED_VBI_CAPTURE |
 			V4L2_CAP_VIDEO_CAPTURE_MPLANE | V4L2_CAP_HW_FREQ_SEEK |
-			V4L2_CAP_TUNER | V4L2_CAP_SDR_CAPTURE;
+			V4L2_CAP_TUNER | V4L2_CAP_SDR_CAPTURE | V4L2_CAP_META_CAPTURE;
 	const __u32 output_caps = V4L2_CAP_VIDEO_OUTPUT | V4L2_CAP_VIDEO_OUTPUT_MPLANE |
 			V4L2_CAP_VIDEO_OUTPUT_OVERLAY | V4L2_CAP_VBI_OUTPUT |
 			V4L2_CAP_SDR_OUTPUT | V4L2_CAP_SLICED_VBI_OUTPUT |
@@ -995,7 +999,8 @@ int main(int argc, char **argv)
 	if (node.g_caps() & (V4L2_CAP_VIDEO_CAPTURE | V4L2_CAP_VBI_CAPTURE |
 			 V4L2_CAP_VIDEO_CAPTURE_MPLANE | V4L2_CAP_VIDEO_M2M_MPLANE |
 			 V4L2_CAP_VIDEO_M2M | V4L2_CAP_SLICED_VBI_CAPTURE |
-			 V4L2_CAP_RDS_CAPTURE | V4L2_CAP_SDR_CAPTURE))
+			 V4L2_CAP_RDS_CAPTURE | V4L2_CAP_SDR_CAPTURE |
+			 V4L2_CAP_META_CAPTURE))
 		node.can_capture = true;
 	if (node.g_caps() & (V4L2_CAP_VIDEO_OUTPUT | V4L2_CAP_VBI_OUTPUT |
 			 V4L2_CAP_VIDEO_OUTPUT_MPLANE | V4L2_CAP_VIDEO_M2M_MPLANE |
@@ -1005,6 +1010,10 @@ int main(int argc, char **argv)
 	if (node.g_caps() & (V4L2_CAP_VIDEO_CAPTURE_MPLANE | V4L2_CAP_VIDEO_OUTPUT_MPLANE |
 			 V4L2_CAP_VIDEO_M2M_MPLANE))
 		node.is_planar = true;
+	if (node.g_caps() & V4L2_CAP_META_CAPTURE) {
+		node.is_video = false;
+		node.is_meta = true;
+	}
 
 	/* Information Opts */
 

utils/v4l2-compliance/v4l2-compliance.h
@@ -61,13 +61,14 @@ typedef std::map<__u32, unsigned> frmsizes_count_map;
 
 struct base_node;
 
-#define V4L2_BUF_TYPE_LAST V4L2_BUF_TYPE_SDR_OUTPUT
+#define V4L2_BUF_TYPE_LAST V4L2_BUF_TYPE_META_CAPTURE
 
 struct base_node {
 	bool is_video;
 	bool is_radio;
 	bool is_vbi;
 	bool is_sdr;
+	bool is_meta;
 	bool is_touch;
 	bool is_m2m;
 	bool is_planar;

utils/v4l2-compliance/v4l2-test-formats.cpp
@@ -46,6 +46,7 @@ static const __u32 buftype2cap[] = {
 	V4L2_CAP_VIDEO_OUTPUT_MPLANE | V4L2_CAP_VIDEO_M2M_MPLANE,
 	V4L2_CAP_SDR_CAPTURE,
 	V4L2_CAP_SDR_OUTPUT,
+	V4L2_CAP_META_CAPTURE,
 };
 
 static int testEnumFrameIntervals(struct node *node, __u32 pixfmt,
@@ -296,6 +297,7 @@ int testEnumFormats(struct node *node)
 		case V4L2_BUF_TYPE_VIDEO_OUTPUT_MPLANE:
 		case V4L2_BUF_TYPE_SDR_CAPTURE:
 		case V4L2_BUF_TYPE_SDR_OUTPUT:
+		case V4L2_BUF_TYPE_META_CAPTURE:
 			if (ret && (node->g_caps() & buftype2cap[type]))
 				return fail("%s cap set, but no %s formats defined\n",
 						buftype2s(type).c_str(), buftype2s(type).c_str());
@@ -407,6 +409,7 @@ static int testFormatsType(struct node *node, int ret,  unsigned type, struct v4
 	struct v4l2_vbi_format &vbi = fmt.fmt.vbi;
 	struct v4l2_sliced_vbi_format &sliced = fmt.fmt.sliced;
 	struct v4l2_sdr_format &sdr = fmt.fmt.sdr;
+	struct v4l2_meta_format &meta = fmt.fmt.meta;
 	unsigned min_data_samples;
 	unsigned min_sampling_rate;
 	v4l2_std_id std;
@@ -538,9 +541,15 @@ static int testFormatsType(struct node *node, int ret,  unsigned type, struct v4
 	case V4L2_BUF_TYPE_SDR_OUTPUT:
 		if (map.find(sdr.pixelformat) == map.end())
 			return fail("pixelformat %08x (%s) for buftype %d not reported by ENUM_FMT\n",
-					pix.pixelformat, fcc2s(pix.pixelformat).c_str(), type);
+					sdr.pixelformat, fcc2s(sdr.pixelformat).c_str(), type);
 		fail_on_test(sdr.buffersize == 0);
 		fail_on_test(check_0(sdr.reserved, sizeof(sdr.reserved)));
+		break;
+	case V4L2_BUF_TYPE_META_CAPTURE:
+		if (map.find(meta.dataformat) == map.end())
+			return fail("dataformat %08x (%s) for buftype %d not reported by ENUM_FMT\n",
+					meta.dataformat, fcc2s(meta.dataformat).c_str(), type);
+		fail_on_test(meta.buffersize == 0);
 		break;
 	case V4L2_BUF_TYPE_PRIVATE:
 		break;
@@ -575,6 +584,7 @@ int testGetFormats(struct node *node)
 		case V4L2_BUF_TYPE_VIDEO_OUTPUT_MPLANE:
 		case V4L2_BUF_TYPE_SDR_CAPTURE:
 		case V4L2_BUF_TYPE_SDR_OUTPUT:
+		case V4L2_BUF_TYPE_META_CAPTURE:
 			if (ret && (node->g_caps() & buftype2cap[type]))
 				return fail("%s cap set, but no %s formats defined\n",
 					buftype2s(type).c_str(), buftype2s(type).c_str());
@@ -630,6 +640,8 @@ static bool matchFormats(const struct v4l2_format &f1, const struct v4l2_format 
 	case V4L2_BUF_TYPE_SDR_CAPTURE:
 	case V4L2_BUF_TYPE_SDR_OUTPUT:
 		return !memcmp(&f1.fmt.sdr, &f2.fmt.sdr, sizeof(f1.fmt.sdr));
+	case V4L2_BUF_TYPE_META_CAPTURE:
+		return !memcmp(&f1.fmt.meta, &f2.fmt.meta, sizeof(f1.fmt.meta));
 
 	}
 	return false;
@@ -705,6 +717,9 @@ int testTryFormats(struct node *node)
 			case V4L2_BUF_TYPE_SDR_OUTPUT:
 				pixelformat = fmt.fmt.sdr.pixelformat;
 				break;
+			case V4L2_BUF_TYPE_META_CAPTURE:
+				pixelformat = fmt.fmt.meta.dataformat;
+				break;
 			case V4L2_BUF_TYPE_VIDEO_CAPTURE_MPLANE:
 			case V4L2_BUF_TYPE_VIDEO_OUTPUT_MPLANE:
 				pixelformat = fmt.fmt.pix_mp.pixelformat;
@@ -722,6 +737,8 @@ int testTryFormats(struct node *node)
 			createInvalidFmt(fmt, clip, type);
 			if (node->is_sdr)
 				fmt.fmt.sdr.pixelformat = pixelformat;
+			else if (node->is_meta)
+				fmt.fmt.meta.dataformat = pixelformat;
 			else if (is_mplane)
 				fmt.fmt.pix_mp.pixelformat = pixelformat;
 			else
@@ -952,6 +969,9 @@ int testSetFormats(struct node *node)
 			doioctl(node, VIDIOC_G_FMT, &fmt_set);
 
 			switch (type) {
+			case V4L2_BUF_TYPE_META_CAPTURE:
+				pixelformat = fmt_set.fmt.meta.dataformat;
+				break;
 			case V4L2_BUF_TYPE_SDR_CAPTURE:
 			case V4L2_BUF_TYPE_SDR_OUTPUT:
 				pixelformat = fmt_set.fmt.sdr.pixelformat;
@@ -982,6 +1002,8 @@ int testSetFormats(struct node *node)
 			createInvalidFmt(fmt_set, clip_set, type);
 			if (node->is_sdr)
 				fmt_set.fmt.sdr.pixelformat = pixelformat;
+			else if (node->is_meta)
+				fmt_set.fmt.meta.dataformat = pixelformat;
 			else if (is_mplane)
 				fmt_set.fmt.pix_mp.pixelformat = pixelformat;
 			else

utils/v4l2-ctl/Makefile.am
@@ -6,7 +6,7 @@ v4l2_ctl_SOURCES = v4l2-ctl.cpp v4l2-ctl.h v4l2-ctl-common.cpp v4l2-ctl-tuner.cp
 	v4l2-ctl-io.cpp v4l2-ctl-stds.cpp v4l2-ctl-vidcap.cpp v4l2-ctl-vidout.cpp \
 	v4l2-ctl-overlay.cpp v4l2-ctl-vbi.cpp v4l2-ctl-selection.cpp v4l2-ctl-misc.cpp \
 	v4l2-ctl-streaming.cpp v4l2-ctl-sdr.cpp v4l2-ctl-edid.cpp v4l2-ctl-modes.cpp \
-	v4l2-tpg-colors.c v4l2-tpg-core.c v4l-stream.c
+	v4l2-tpg-colors.c v4l2-tpg-core.c v4l-stream.c v4l2-ctl-meta.cpp
 v4l2_ctl_CPPFLAGS = -I$(top_srcdir)/utils/common
 
 if WITH_V4L2_CTL_LIBV4L

utils/v4l2-ctl/v4l2-ctl-common.cpp
@@ -296,6 +296,7 @@ static std::string ctrlflags2s(__u32 flags)
 		{ V4L2_CTRL_FLAG_VOLATILE,   "volatile" },
 		{ V4L2_CTRL_FLAG_HAS_PAYLOAD,"has-payload" },
 		{ V4L2_CTRL_FLAG_EXECUTE_ON_WRITE, "execute-on-write" },
+		{ V4L2_CTRL_FLAG_MODIFY_LAYOUT, "modify-layout" },
 		{ 0, NULL }
 	};
 	return flags2s(flags, def);

utils/v4l2-ctl/v4l2-ctl-meta.cpp
@@ -1,0 +1,110 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <inttypes.h>
+#include <getopt.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <ctype.h>
+#include <errno.h>
+#include <sys/ioctl.h>
+#include <sys/time.h>
+#include <dirent.h>
+#include <math.h>
+
+#include "v4l2-ctl.h"
+
+static struct v4l2_format vfmt;	/* set_format/get_format */
+
+void meta_usage(void)
+{
+	printf("\nMetadata Formats options:\n"
+	       "  --list-formats-meta display supported metadata capture formats [VIDIOC_ENUM_FMT]\n"
+	       "  --get-fmt-meta      query the metadata capture format [VIDIOC_G_FMT]\n"
+	       "  --set-fmt-meta=<f>  set the metadata capture format [VIDIOC_S_FMT]\n"
+	       "                     parameter is either the format index as reported by\n"
+	       "                     --list-formats-meta-cap, or the fourcc value as a string\n"
+	       "  --try-fmt-meta=<f>  try the metadata capture format [VIDIOC_TRY_FMT]\n"
+	       "                     parameter is either the format index as reported by\n"
+	       "                     --list-formats-meta-cap, or the fourcc value as a string\n"
+	       "  --list-formats-meta-out\n"
+	       "                     display supported metadata output formats [VIDIOC_ENUM_FMT]\n"
+	       "  --get-fmt-meta-out  query the metadata output format [VIDIOC_G_FMT]\n"
+	       "  --set-fmt-meta-out=<f>\n"
+	       "                     set the metadata output format [VIDIOC_S_FMT]\n"
+	       "                     parameter is either the format index as reported by\n"
+	       "                     --list-formats-meta-out, or the fourcc value as a string\n"
+	       "  --try-fmt-meta-out=<f>\n"
+	       "                     try the metadata output format [VIDIOC_TRY_FMT]\n"
+	       "                     parameter is either the format index as reported by\n"
+	       "                     --list-formats-meta-out, or the fourcc value as a string\n"
+	       );
+}
+
+void meta_cmd(int ch, char *optarg)
+{
+	switch (ch) {
+	case OptSetMetaFormat:
+	case OptTryMetaFormat:
+		if (strlen(optarg) == 0) {
+			meta_usage();
+			exit(1);
+		} else if (strlen(optarg) == 4) {
+			vfmt.fmt.meta.dataformat = v4l2_fourcc(optarg[0],
+					optarg[1], optarg[2], optarg[3]);
+		} else {
+			vfmt.fmt.meta.dataformat = strtol(optarg, 0L, 0);
+		}
+		break;
+	}
+}
+
+void meta_set(int fd)
+{
+	int ret;
+
+	if (options[OptSetMetaFormat] || options[OptTryMetaFormat]) {
+		struct v4l2_format in_vfmt;
+
+		in_vfmt.type = V4L2_BUF_TYPE_META_CAPTURE;
+		in_vfmt.fmt.meta.dataformat = vfmt.fmt.meta.dataformat;
+
+		if (in_vfmt.fmt.meta.dataformat < 256) {
+			struct v4l2_fmtdesc fmt;
+
+			fmt.index = in_vfmt.fmt.meta.dataformat;
+			fmt.type = V4L2_BUF_TYPE_META_CAPTURE;
+
+			if (doioctl(fd, VIDIOC_ENUM_FMT, &fmt))
+				fmt.pixelformat = 0;
+
+			in_vfmt.fmt.meta.dataformat = fmt.pixelformat;
+		}
+
+		if (options[OptSetMetaFormat])
+			ret = doioctl(fd, VIDIOC_S_FMT, &in_vfmt);
+		else
+			ret = doioctl(fd, VIDIOC_TRY_FMT, &in_vfmt);
+		if (ret == 0 && (verbose || options[OptTryMetaFormat]))
+			printfmt(in_vfmt);
+	}
+}
+
+void meta_get(int fd)
+{
+	if (options[OptGetMetaFormat]) {
+		vfmt.type = V4L2_BUF_TYPE_META_CAPTURE;
+		if (doioctl(fd, VIDIOC_G_FMT, &vfmt) == 0)
+			printfmt(vfmt);
+	}
+}
+
+void meta_list(int fd)
+{
+	if (options[OptListMetaFormats]) {
+		printf("ioctl: VIDIOC_ENUM_FMT\n");
+		print_video_formats(fd, V4L2_BUF_TYPE_META_CAPTURE);
+	}
+}

commit 6de7a0df7e8c06af36ed609d87649e704279ea2d

v4l2-ctl: Print metadata capture formats on --all
Printing metadata formats was supported separately with
--list-formats-meta but no meta formats were printed with --all option.
Fix this.

Fixes: b3f1cf6b8519 ("v4l2-compliance/v4l2-ctl: support V4L2_CTRL_FLAG_MODIFY_LAYOUT and metadata")

Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>

utils/v4l2-ctl/v4l2-ctl.cpp
@@ -1246,6 +1246,7 @@ int main(int argc, char **argv)
 		options[OptGetSlicedVbiOutFormat] = 1;
 		options[OptGetSdrFormat] = 1;
 		options[OptGetSdrOutFormat] = 1;
+		options[OptGetMetaFormat] = 1;
 		options[OptGetFBuf] = 1;
 		options[OptGetCropCap] = 1;
 		options[OptGetOutputCropCap] = 1;