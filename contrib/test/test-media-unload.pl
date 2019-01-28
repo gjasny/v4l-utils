0caa3c7d126cc4c2ee53e075cda16368ff295255
Hans Verkuil <hverkuil-cisco@xs4all.nl>
2019-01-28 12:09:24 +0100

    v4l-utils: add test-media regression tests
    This is the first version of the regression tests.
    
    To do:
    
    - It assumes that all video devices are at the same number
      (e.g. video0 is always the first vivid capture device). Instead
      it should discover this based on media driver names and bus_info.
      Once this in place it is likely that the test-media-unload.pl
      script can be dropped, and that these drivers can also be
      compiled directly into the kernel instead of as modules.
    
    - There are still a few fails due to kernel code that has not yet
      been merged.
    
    - The vicodec driver isn't included in the tests as it needs more
      work, both in the driver itself and in the compliance tests.
    
    - vivid generates a few warnings. Work is progressing to fix those.
    
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>

---
+++ contrib/test/test-media-unload.pl
@@ -0,0 +1,160 @@
+#!/usr/bin/perl
+
+# Copyright (c) 2006-2010 by Mauro Carvalho Chehab <mchehab@redhat.com>
+#
+# Licensed under the terms of the GNU GPL License version 2
+
+use strict;
+use File::Find;
+
+my %depend = ();
+my %depend2 = ();
+my %rmlist = ();
+my @nodep;
+my @modlist;
+my @allmodules;
+my %reqmodules;
+my %loaded = ();
+my $i=0;
+
+sub findprog($)
+{
+	foreach(split(/:/, $ENV{PATH}),qw(/sbin /usr/sbin /usr/local/sbin)) {
+		return "$_/$_[0]" if(-x "$_/$_[0]");
+	}
+	die "Can't find needed utility '$_[0]'";
+}
+
+sub parse_dir {
+	my $file = $File::Find::name;
+	my $modinfo = findprog('modinfo');
+
+	if (!($file =~ /[.]ko$/)) {
+		return;
+	}
+
+	my $module = $file;
+	$module =~ s|.*\/([^/]+)\.ko|\1|;
+
+	open IN, "$modinfo $module.ko|" or die "can't run $modinfo $file";
+	while (<IN>) {
+		if (m/depends:\s*(.*)/) {
+			my $deps = $1;
+			$deps =~ s/\n//;
+			$deps =~ s/[,]/ /g;
+			$deps = " $deps ";
+			$depend{$module} = $deps;
+			push @allmodules, $module;
+			$i++;
+		}
+	}
+	close IN;
+}
+
+sub parse_loaded {
+	open IN,  "/proc/modules";
+	while (<IN>) {
+		m/^([\w\d_-]+)/;
+		$loaded{$1}=1;
+	}
+	close IN;
+}
+
+sub cleandep()
+{
+	my $dep;
+
+	while ( my ($k, $v) = each(%depend) ) {
+		my $arg=$v;
+		my $arg2=" ";
+		while (!($arg =~ m/^\s*$/)) {
+			if ($arg =~ m/^ ([^ ]+) /) {
+				my $val=$1;
+				if (exists($depend{$val})) {
+					$arg2="$arg2 $val ";
+				} else {
+					$reqmodules{$val}=1;
+				}
+			}
+			$arg =~ s/^ [^ ]+//;
+			$arg2 =~ s/\s\s+/ /;
+		}
+		$depend2 { $k } = $arg2;
+	}
+
+}
+
+sub rmdep()
+{
+	my $dep;
+
+	while ($dep=pop @nodep) {
+		while ( my ($k, $v) = each(%depend2) ) {
+			if ($v =~ m/\s($dep)\s/) {
+				$v =~ s/\s${dep}\s/ /;
+				$v =~ s/\s${dep}\s/ /;
+				$v =~ s/\s${dep}\s/ /;
+				$depend2 {$k} = $v;
+			}
+		}
+	}
+}
+
+sub orderdep ()
+{
+	my $old;
+	do {
+		$old=$i;
+		while ( my ($key, $value) = each(%depend2) ) {
+			if ($value =~ m/^\s*$/) {
+				push @nodep, $key;
+				push @modlist, $key;
+				$i=$i-1;
+				delete $depend2 {$key};
+			}
+		}
+		rmdep();
+	} until ($old==$i);
+	while ( my ($key, $value) = each(%depend2) ) {
+		printf "ERROR: bad dependencies - $key ($value)\n";
+	}
+}
+
+sub rmmod(@)
+{
+	my $rmmod = findprog('rmmod');
+	my @not;
+	foreach (reverse @_) {
+		s/-/_/g;
+		if (exists ($loaded{$_})) {
+			print "$rmmod $_\n";
+			unshift @not, $_ if (system "$rmmod $_");
+		}
+	}
+	return @not;
+}
+
+sub prepare_cmd()
+{
+	my $ver=qx(uname -r);
+	$ver =~ s/\s+$//;
+	die "Couldn't get kernel version" if (!$ver);
+
+	print "Seeking media drivers at /lib/modules/$ver/kernel/drivers/media/\n";
+	find(\&parse_dir, "/lib/modules/$ver/kernel/drivers/media/");
+	print "Seeking media drivers at /lib/modules/$ver/kernel/drivers/staging/media/\n";
+	find(\&parse_dir, "/lib/modules/$ver/kernel/drivers/staging/media/");
+	printf "found $i modules\n";
+
+	cleandep();
+	orderdep();
+}
+
+prepare_cmd;
+parse_loaded;
+
+my @notunloaded = rmmod(@modlist);
+@notunloaded = rmmod(@notunloaded) if (@notunloaded);
+if (@notunloaded) {
+	print "Couldn't unload: ", join(' ', @notunloaded), "\n";
+}
