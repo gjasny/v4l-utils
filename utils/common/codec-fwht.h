8126f62727f9d79e17447f26b7eba1918d6ddc0b
Hans Verkuil <hverkuil-cisco@xs4all.nl>
2019-03-20 09:31:22 +0100

    v4l-utils: sync with latest media master
    Synchronize with the latest headers from the media master tree.
    
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>

--- utils/common/codec-fwht.h
+++ utils/common/codec-fwht.h
@@ -21,6 +21,7 @@
 #define pr_err(arg...)
 #define __round_mask(x, y) ((__typeof__(x))((y)-1))
 #define round_up(x, y) ((((x)-1) | __round_mask(x, y))+1)
+#define noinline_for_stack
 
 
 typedef __u32 u32;
