b2f9060d87189e0ec798ad304704b3ffb971b8d1
Mauro Carvalho Chehab <mchehab+samsung@kernel.org>
2019-03-24 17:52:13 -0300

    Update my e-mail on all places
    My e-mail has changed over the years. Due to that, people still
    send me bug reports to e-mails I don't use anymore for upstream
    stuff, or even send e-mails to addresses that doesn't exist
    anymore.
    
    So, let's put an order at the house, using just the canonical
    e-mail mchehab@kernel.org, as this e-mail should always point
    to my current e-mail preference.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>

--- contrib/parse_tcpdump_log.pl
+++ contrib/parse_tcpdump_log.pl
@@ -744,7 +744,7 @@ The provided info are:
 
 =head1 BUGS
 
-Report bugs to Mauro Carvalho Chehab <m.chehab@samsung.com>
+Report bugs to Mauro Carvalho Chehab <mchehab@kernel.org>
 
 =head1 COPYRIGHT
 
