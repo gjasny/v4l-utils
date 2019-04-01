gjasny/v4l-utils

commit 7ee189ecae0193fc2f98a09ed087f9f2cf30def7

v4l2-compliance: fix request API test
The vicodec driver's media controller supports requests, but the
stateful en/decoder video nodes do not: improve the request API
test to handle this somewhat uncommon case.

Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>

utils/v4l2-compliance/v4l2-test-buffers.cpp
@@ -1727,8 +1727,7 @@ int testRequests(struct node *node, bool test_streaming)
 		fail_on_test(node->buf_caps & V4L2_BUF_CAP_SUPPORTS_REQUESTS);
 		return ENOTTY;
 	}
-
-	fail_on_test(!(node->buf_caps & V4L2_BUF_CAP_SUPPORTS_REQUESTS));
+	bool supports_requests = node->buf_caps & V4L2_BUF_CAP_SUPPORTS_REQUESTS;
 
 	buffer_info.clear();
 
@@ -1812,7 +1811,25 @@ int testRequests(struct node *node, bool test_streaming)
 			buf.s_flags(buf.g_flags() | V4L2_BUF_FLAG_REQUEST_FD);
 			buf.s_request_fd(buf_req_fds[i]);
 		}
-		fail_on_test(buf.qbuf(node));
+		int err = buf.qbuf(node);
+		if (!err) {
+			fail_on_test(!supports_requests);
+			fail_on_test(buf.qbuf(node));
+		} else {
+			fail_on_test(supports_requests);
+			fail_on_test(err != EBADR);
+		}
+		if (err) {
+			fail_on_test(node->streamoff(q.g_type()));
+			fail_on_test(q.reqbufs(node, 0));
+			if (node->is_m2m) {
+				fail_on_test(node->streamoff(m2m_q.g_type()));
+				m2m_q.munmap_bufs(node);
+				fail_on_test(m2m_q.reqbufs(node, 0));
+			}
+			node->reopen();
+			return ENOTTY;
+		}
 		fail_on_test(buf.g_flags() & V4L2_BUF_FLAG_DONE);
 		fail_on_test(!(buf.g_flags() & V4L2_BUF_FLAG_IN_REQUEST));
 		fail_on_test(!(buf.g_flags() & V4L2_BUF_FLAG_REQUEST_FD));

commit 0efd87d4058697347797239cad13cb4231438f19

v4l2-compliance: fix and comment out qbuf test
The fail_on_test(qbuf()) check was missing a !.

But if after this correctly-failing qbuf() the querybuf ioctl is
called again, all the request information is dropped from the result.

So comment this out while a investigation this.

Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
Fixes: 7ee189ecae01 ("v4l2-compliance: fix request API test")

utils/v4l2-compliance/v4l2-test-buffers.cpp
@@ -1814,7 +1814,9 @@ int testRequests(struct node *node, bool test_streaming)
 		int err = buf.qbuf(node);
 		if (!err) {
 			fail_on_test(!supports_requests);
-			fail_on_test(buf.qbuf(node));
+			// After this call querybuf won't set
+			// V4L2_BUF_FLAG_REQUEST_FD. Disable this test for now.
+			// fail_on_test(!buf.qbuf(node));
 		} else {
 			fail_on_test(supports_requests);
 			fail_on_test(err != EBADR);