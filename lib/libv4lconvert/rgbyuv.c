d59fa43a9b531f80ebdb971484da6ae80120e0ed
Hans Petter Selasky <hps@selasky.org>
2020-04-08 19:17:42 +0200

    Fix broken for loop in libv4l
    Wrong variable was used in the loop.
    
    Signed-off-by: Hans Petter Selasky <hps@selasky.org>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>

--- lib/libv4lconvert/rgbyuv.c
+++ lib/libv4lconvert/rgbyuv.c
@@ -893,7 +893,7 @@ void v4lconvert_nv12_to_yuv420(const unsigned char *src, unsigned char *dest,
 	}
 
 	for (i = 0; i < height; i++)
-		for (j = 0; i < width; j++) {
+		for (j = 0; j < width; j++) {
 			*ydst++ = *ysrc++;
 			if (((i % 2) == 0) && ((j % 2) == 0)) {
 				*udst++ = *uvsrc++;
