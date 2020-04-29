ca3afee1ce741a92d27ba7ad98a50727eaf4ece4
Rosen Penev <rosenp@gmail.com>
2020-04-28 21:06:26 -0700

    compiler.h: fix compilation with clang + std=c++98 + glibcxx
    It seems clang does not like these attributes in c++98 mode.
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>

--- include/compiler.h
+++ include/compiler.h
@@ -1,14 +1,17 @@
-#ifdef __cplusplus
-
 #ifdef _LIBCPP_VERSION
 #define fallthrough _LIBCPP_FALLTHROUGH()
 #else
+
+#if __cplusplus >= 201103L
 
 #ifdef __clang__
 #define fallthrough [[clang::fallthrough]]
 #else
 #define fallthrough [[gnu::fallthrough]]
-#endif
+#endif // __clang__
 
-#endif
-#endif
+#else
+#define fallthrough ((void)0)
+
+#endif // __cplusplus
+#endif // _LIBCPP_VERSION
