815ad0518dcdc69211658c5517669e2c41726dbb
Rosen Penev <rosenp@gmail.com>
2020-04-28 21:06:25 -0700

    compiler.h: fix compilation with libcxx
    The fallthrough define doesn't work properly with libcxx. Use the included
    macro.
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>

--- include/compiler.h
+++ include/compiler.h
@@ -1,9 +1,14 @@
 #ifdef __cplusplus
 
+#ifdef _LIBCPP_VERSION
+#define fallthrough _LIBCPP_FALLTHROUGH()
+#else
+
 #ifdef __clang__
 #define fallthrough [[clang::fallthrough]]
 #else
 #define fallthrough [[gnu::fallthrough]]
 #endif
 
+#endif
 #endif
