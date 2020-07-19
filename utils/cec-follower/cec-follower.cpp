gjasny/v4l-utils

commit 3f35a7bcf82b58e778401034edd9e036b18afe97

cec-follower: Add version command
Add a --version option to cec-follower to retrieve the version of
cec-follower. Whilte at it, factor out printing the SHA.

Signed-off-by: Paul Elder <paul.elder@ideasonboard.com>
Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>

utils/cec-follower/Makefile.am
@@ -2,7 +2,7 @@ bin_PROGRAMS = cec-follower
 man_MANS = cec-follower.1
 
 cec_follower_SOURCES = cec-follower.cpp cec-follower.h cec-processing.cpp cec-tuner.cpp
-cec_follower_CPPFLAGS = -I$(top_srcdir)/utils/libcecutil
+cec_follower_CPPFLAGS = -I$(top_srcdir)/utils/libcecutil $(GIT_COMMIT_CNT)
 cec_follower_LDADD = -lrt ../libcecutil/libcecutil.la
 
 cec-follower.cpp: version.h

utils/cec-follower/cec-follower.cpp
@@ -46,6 +46,7 @@ enum Option {
 	OptShowState = 's',
 	OptWallClock = 'w',
 	OptServiceByDigID = 128,
+	OptVersion,
 	OptLast = 256
 };
 
@@ -70,9 +71,26 @@ static struct option long_options[] = {
 	{ "wall-clock", no_argument, 0, OptWallClock },
 	{ "service-by-dig-id", no_argument, 0, OptServiceByDigID },
 	{ "ignore", required_argument, 0, OptIgnore },
+	{ "version", no_argument, 0, OptVersion },
 
 	{ 0, 0, 0, 0 }
 };
+
+static void print_sha()
+{
+#ifdef SHA
+#define STR(x) #x
+#define STRING(x) STR(x)
+	printf("cec-follower SHA                   : %s\n", STRING(SHA));
+#else
+	printf("cec-follower SHA                   : not available\n");
+#endif
+}
+
+static void print_version()
+{
+	printf("cec-follower %s%s\n", PACKAGE_VERSION, STRING(GIT_COMMIT_CNT));
+}
 
 static void usage()
 {
@@ -93,6 +111,7 @@ static void usage()
 	       "                      Ignore messages from logical address <la> and opcode\n"
 	       "                      <opcode>. 'all' can be used for <la> or <opcode> to match\n"
 	       "                      all logical addresses or opcodes.\n"
+	       "  --version           Show version information\n"
 	       );
 }
 
@@ -427,6 +446,9 @@ int main(int argc, char **argv)
 			show_msgs = true;
 			show_state = true;
 			break;
+		case OptVersion:
+			print_version();
+			std::exit(EXIT_SUCCESS);
 		case ':':
 			fprintf(stderr, "Option '%s' requires a value\n",
 				argv[optind]);
@@ -475,13 +497,7 @@ int main(int argc, char **argv)
 	node.state.service_by_dig_id = options[OptServiceByDigID];
 	state_init(node);
 
-#ifdef SHA
-#define STR(x) #x
-#define STRING(x) STR(x)
-	printf("cec-follower SHA                   : %s\n", STRING(SHA));
-#else
-	printf("cec-follower SHA                   : not available\n");
-#endif
+	print_sha();
 
 	doioctl(&node, CEC_ADAP_G_PHYS_ADDR, &node.phys_addr);
 

utils/cec-follower/cec-follower.1.in
@@ -61,6 +61,9 @@ having to rely on the device node number.
 \fB\-v\fR, \fB\-\-verbose\fR
 Turn on verbose reporting.
 .TP
+\fB\-\-version\fR
+Show version information.
+.TP
 \fB\-w\fR, \fB\-\-wall\-clock\fR
 Show timestamps as wall-clock time. This also turns on verbose reporting. 
 .TP

commit 82079781a8b64120acfa714337bb3b87bcd960a5

cec-follower: fix logging the SHA
Commit 3f35a7bcf82b ("cec-follower: Add version command") broke
the v4l-utils debian build, and broke logging the SHA.

Fix this.

Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
Fixes: 3f35a7bcf82b ("cec-follower: Add version command")

utils/cec-follower/.gitignore
@@ -1,3 +1,2 @@
 cec-follower
 cec-follower.1
-version.h

utils/cec-follower/Makefile.am
@@ -2,20 +2,7 @@ bin_PROGRAMS = cec-follower
 man_MANS = cec-follower.1
 
 cec_follower_SOURCES = cec-follower.cpp cec-follower.h cec-processing.cpp cec-tuner.cpp
-cec_follower_CPPFLAGS = -I$(top_srcdir)/utils/libcecutil $(GIT_COMMIT_CNT)
+cec_follower_CPPFLAGS = -I$(top_srcdir)/utils/libcecutil $(GIT_SHA) $(GIT_COMMIT_CNT)
 cec_follower_LDADD = -lrt ../libcecutil/libcecutil.la
-
-cec-follower.cpp: version.h
-
-version.h:
-	@if [ -d $(top_srcdir)/.git ]; then \
-		echo -n "#define SHA " >$@ ; \
-		git -C $(top_srcdir) rev-parse HEAD >>$@ ; \
-	else \
-		touch $@ ; \
-	fi
-
-clean-local:
-	-rm -vf version.h
 
 EXTRA_DIST = cec-follower.1

utils/cec-follower/cec-follower.cpp
@@ -22,9 +22,6 @@
 
 #include "cec-follower.h"
 #include "compiler.h"
-#ifndef ANDROID
-#include "version.h"
-#endif
 
 /* Short option list
 
@@ -76,21 +73,8 @@ static struct option long_options[] = {
 	{ 0, 0, 0, 0 }
 };
 
-static void print_sha()
-{
-#ifdef SHA
 #define STR(x) #x
 #define STRING(x) STR(x)
-	printf("cec-follower SHA                   : %s\n", STRING(SHA));
-#else
-	printf("cec-follower SHA                   : not available\n");
-#endif
-}
-
-static void print_version()
-{
-	printf("cec-follower %s%s\n", PACKAGE_VERSION, STRING(GIT_COMMIT_CNT));
-}
 
 static void usage()
 {
@@ -447,7 +431,8 @@ int main(int argc, char **argv)
 			show_state = true;
 			break;
 		case OptVersion:
-			print_version();
+			printf("cec-follower %s%s\n", PACKAGE_VERSION, STRING(GIT_COMMIT_CNT));
+			printf("cec-follower SHA: %s\n", STRING(GIT_SHA));
 			std::exit(EXIT_SUCCESS);
 		case ':':
 			fprintf(stderr, "Option '%s' requires a value\n",
@@ -497,7 +482,7 @@ int main(int argc, char **argv)
 	node.state.service_by_dig_id = options[OptServiceByDigID];
 	state_init(node);
 
-	print_sha();
+	printf("cec-follower SHA                   : %s\n", STRING(GIT_SHA));
 
 	doioctl(&node, CEC_ADAP_G_PHYS_ADDR, &node.phys_addr);
 
@@ -505,8 +490,6 @@ int main(int argc, char **argv)
 	doioctl(&node, CEC_ADAP_G_LOG_ADDRS, &laddrs);
 	node.adap_la_mask = laddrs.log_addr_mask;
 	node.cec_version = laddrs.cec_version;
-
-	printf("\n");
 
 	struct cec_connector_info conn_info = {};
 