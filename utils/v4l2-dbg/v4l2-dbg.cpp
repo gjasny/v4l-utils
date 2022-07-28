a46e57aea3860eb984f6ce0532d0961a51e59a59
Hans Verkuil <hverkuil-cisco@xs4all.nl>
2022-07-28 17:22:52 +0200

    v4l2-dbg: drop cap2s(), use v4l2_info_capability() instead
    Use the helper function v4l2_info_capability() from v4l2-info.cpp
    to show the driver info instead of duplicating that code.
    
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>

--- utils/v4l2-dbg/v4l2-dbg.cpp
+++ utils/v4l2-dbg/v4l2-dbg.cpp
@@ -40,6 +40,8 @@
 #include <linux/videodev2.h>
 #include <v4l-getsubopt.h>
 
+#include <v4l2-info.h>
+
 #include "v4l2-dbg-bttv.h"
 #include "v4l2-dbg-saa7134.h"
 #include "v4l2-dbg-em28xx.h"
@@ -171,65 +173,6 @@ static void usage()
 	       "		     Sets step between two registers\n"
 	       "  --list-symbols     List the symbolic register names you can use, if any\n"
 	       "  --log-status       Log the board status in the kernel log [VIDIOC_LOG_STATUS]\n");
-}
-
-static std::string cap2s(unsigned cap)
-{
-	std::string s;
-
-	if (cap & V4L2_CAP_VIDEO_CAPTURE)
-		s += "\t\tVideo Capture\n";
-	if (cap & V4L2_CAP_VIDEO_CAPTURE_MPLANE)
-		s += "\t\tVideo Capture Multiplanar\n";
-	if (cap & V4L2_CAP_VIDEO_OUTPUT)
-		s += "\t\tVideo Output\n";
-	if (cap & V4L2_CAP_VIDEO_OUTPUT_MPLANE)
-		s += "\t\tVideo Output Multiplanar\n";
-	if (cap & V4L2_CAP_VIDEO_M2M)
-		s += "\t\tVideo Memory-to-Memory\n";
-	if (cap & V4L2_CAP_VIDEO_M2M_MPLANE)
-		s += "\t\tVideo Memory-to-Memory Multiplanar\n";
-	if (cap & V4L2_CAP_VIDEO_OVERLAY)
-		s += "\t\tVideo Overlay\n";
-	if (cap & V4L2_CAP_VIDEO_OUTPUT_OVERLAY)
-		s += "\t\tVideo Output Overlay\n";
-	if (cap & V4L2_CAP_VBI_CAPTURE)
-		s += "\t\tVBI Capture\n";
-	if (cap & V4L2_CAP_VBI_OUTPUT)
-		s += "\t\tVBI Output\n";
-	if (cap & V4L2_CAP_SLICED_VBI_CAPTURE)
-		s += "\t\tSliced VBI Capture\n";
-	if (cap & V4L2_CAP_SLICED_VBI_OUTPUT)
-		s += "\t\tSliced VBI Output\n";
-	if (cap & V4L2_CAP_RDS_CAPTURE)
-		s += "\t\tRDS Capture\n";
-	if (cap & V4L2_CAP_RDS_OUTPUT)
-		s += "\t\tRDS Output\n";
-	if (cap & V4L2_CAP_SDR_CAPTURE)
-		s += "\t\tSDR Capture\n";
-	if (cap & V4L2_CAP_TOUCH)
-		s += "\t\tTouch Device\n";
-	if (cap & V4L2_CAP_TUNER)
-		s += "\t\tTuner\n";
-	if (cap & V4L2_CAP_HW_FREQ_SEEK)
-		s += "\t\tHW Frequency Seek\n";
-	if (cap & V4L2_CAP_MODULATOR)
-		s += "\t\tModulator\n";
-	if (cap & V4L2_CAP_AUDIO)
-		s += "\t\tAudio\n";
-	if (cap & V4L2_CAP_RADIO)
-		s += "\t\tRadio\n";
-	if (cap & V4L2_CAP_READWRITE)
-		s += "\t\tRead/Write\n";
-	if (cap & V4L2_CAP_ASYNCIO)
-		s += "\t\tAsync I/O\n";
-	if (cap & V4L2_CAP_STREAMING)
-		s += "\t\tStreaming\n";
-	if (cap & V4L2_CAP_EXT_PIX_FORMAT)
-		s += "\t\tExtended Pix Format\n";
-	if (cap & V4L2_CAP_DEVICE_CAPS)
-		s += "\t\tDevice Capabilities\n";
-	return s;
 }
 
 static void print_regs(int fd, struct v4l2_dbg_register *reg, unsigned long min, unsigned long max, int stride)
@@ -543,16 +486,8 @@ int main(int argc, char **argv)
 	/* Information Opts */
 
 	if (options[OptGetDriverInfo]) {
-		printf("Driver info:\n");
-		printf("\tDriver name   : %s\n", vcap.driver);
-		printf("\tCard type     : %s\n", vcap.card);
-		printf("\tBus info      : %s\n", vcap.bus_info);
-		printf("\tDriver version: %d.%d.%d\n",
-				vcap.version >> 16,
-				(vcap.version >> 8) & 0xff,
-				vcap.version & 0xff);
-		printf("\tCapabilities  : 0x%08X\n", vcap.capabilities);
-		printf("%s", cap2s(vcap.capabilities).c_str());
+		printf("Driver Info:\n");
+		v4l2_info_capability(vcap);
 	}
 
 	chip_info.name[0] = '\0';
