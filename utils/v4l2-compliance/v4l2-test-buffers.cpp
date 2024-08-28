gjasny/v4l-utils

commit 37d02fb3e909237320b0bca6edf736870677fb1f

v4l2-compliance: testRequests(): drop V4L2_BUF_FLAG_QUEUED check
After queueing the request it checks that the buffer is no longer
'IN_REQUEST' and it also checks that it is in state QUEUED, but that
last check is too strict since the buffer might already be marked DONE.

Just drop it.

Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>

utils/v4l2-compliance/v4l2-test-buffers.cpp
@@ -2700,10 +2700,9 @@ int testRequests(struct node *node, bool test_streaming)
 		}
 		fail_on_test_val(ret, ret);
 		fail_on_test(buf.querybuf(node, i));
-		// Check that the buffer is now queued up
+		// Check that the buffer is now queued up (i.e. no longer 'IN_REQUEST')
 		fail_on_test(buf.g_flags() & V4L2_BUF_FLAG_IN_REQUEST);
 		fail_on_test(!(buf.g_flags() & V4L2_BUF_FLAG_REQUEST_FD));
-		fail_on_test(!(buf.g_flags() & V4L2_BUF_FLAG_QUEUED));
 		// Re-initing or requeuing the request is no longer possible
 		fail_on_test(doioctl_fd(buf_req_fds[i], MEDIA_REQUEST_IOC_REINIT, nullptr) != EBUSY);
 		fail_on_test(doioctl_fd(buf_req_fds[i], MEDIA_REQUEST_IOC_QUEUE, nullptr) != EBUSY);

commit e87fefa5d0fea252dc128d6d3fb9340056dde3c3

v4l2-compliance: only do request-queued checks while streaming is off
The checks that verify if the queued request (and associated buffer) is
actually in the queued state should only be done while streamon hasn't
been called. Otherwise the request might already been processed and is
no longer in the queued state, which causes hard to reproduce failures.

This also reinstates the V4L2_BUF_FLAG_QUEUED check as that failed for
the same reason. That test is fine as long as streamon wasn't called.

Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
Fixes: 37d02fb3e909 ("v4l2-compliance: testRequests(): drop V4L2_BUF_FLAG_QUEUED check")

utils/v4l2-compliance/v4l2-test-buffers.cpp
@@ -2699,13 +2699,16 @@ int testRequests(struct node *node, bool test_streaming)
 			break;
 		}
 		fail_on_test_val(ret, ret);
-		fail_on_test(buf.querybuf(node, i));
-		// Check that the buffer is now queued up (i.e. no longer 'IN_REQUEST')
-		fail_on_test(buf.g_flags() & V4L2_BUF_FLAG_IN_REQUEST);
-		fail_on_test(!(buf.g_flags() & V4L2_BUF_FLAG_REQUEST_FD));
-		// Re-initing or requeuing the request is no longer possible
-		fail_on_test(doioctl_fd(buf_req_fds[i], MEDIA_REQUEST_IOC_REINIT, nullptr) != EBUSY);
-		fail_on_test(doioctl_fd(buf_req_fds[i], MEDIA_REQUEST_IOC_QUEUE, nullptr) != EBUSY);
+		if (i < min_bufs) {
+			fail_on_test(buf.querybuf(node, i));
+			// Check that the buffer is now queued up (i.e. no longer 'IN_REQUEST')
+			fail_on_test(buf.g_flags() & V4L2_BUF_FLAG_IN_REQUEST);
+			fail_on_test(!(buf.g_flags() & V4L2_BUF_FLAG_REQUEST_FD));
+			fail_on_test(!(buf.g_flags() & V4L2_BUF_FLAG_QUEUED));
+			// Re-initing or requeuing the request is no longer possible
+			fail_on_test(doioctl_fd(buf_req_fds[i], MEDIA_REQUEST_IOC_REINIT, nullptr) != EBUSY);
+			fail_on_test(doioctl_fd(buf_req_fds[i], MEDIA_REQUEST_IOC_QUEUE, nullptr) != EBUSY);
+		}
 		if (i >= min_bufs) {
 			// Close some of the request fds to check that this
 			// is safe to do