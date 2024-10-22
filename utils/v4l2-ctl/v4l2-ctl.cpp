gjasny/v4l-utils

commit c78d63933445915b22e69c7702e64feb6c3d8dab

v4l2-ctl: add --list-devices-input/output options
With these options you can filter the listed devices by
the input or output name of the currently selected input
or output of the device.

Also move the handling of the list-devices options to the
beginning and exit right after. This fixes an issue if there
is no /dev/video0 (e.g. if the first device is /dev/video2),
and v4l2-ctl --list-devices would fail with an error in that
case (can't open video0).

Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>

utils/v4l2-ctl/v4l2-ctl.cpp
@@ -207,6 +207,8 @@ static struct option long_options[] = {
 	{"overlay", required_argument, nullptr, OptOverlay},
 	{"sleep", required_argument, nullptr, OptSleep},
 	{"list-devices", no_argument, nullptr, OptListDevices},
+	{"list-devices-input", required_argument, nullptr, OptListDevicesInput},
+	{"list-devices-output", required_argument, nullptr, OptListDevicesOutput},
 	{"list-dv-timings", optional_argument, nullptr, OptListDvTimings},
 	{"query-dv-timings", no_argument, nullptr, OptQueryDvTimings},
 	{"get-dv-timings", no_argument, nullptr, OptGetDvTimings},
@@ -1307,6 +1309,11 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
+	verbose = options[OptVerbose];
+
+	if (common_list_devices(media_bus_info, c_fd))
+		return 0;
+
 	media_type type = mi_media_detect_type(device);
 	if (type == MEDIA_TYPE_CANT_STAT) {
 		fprintf(stderr, "Cannot open device %s, exiting.\n",
@@ -1340,6 +1347,8 @@ int main(int argc, char **argv)
 	c_out_fd.s_direct(!options[OptUseWrapper]);
 	c_exp_fd.s_direct(!options[OptUseWrapper]);
 
+	c_fd.s_trace(options[OptSilent] ? 0 : (verbose ? 2 : 1));
+
 	if (is_subdev)
 		fd = c_fd.subdev_open(device);
 	else
@@ -1350,8 +1359,6 @@ int main(int argc, char **argv)
 			strerror(errno));
 		std::exit(EXIT_FAILURE);
 	}
-	verbose = options[OptVerbose];
-	c_fd.s_trace(options[OptSilent] ? 0 : (verbose ? 2 : 1));
 
 	if (!is_subdev && doioctl(fd, VIDIOC_QUERYCAP, &vcap)) {
 		fprintf(stderr, "%s: not a v4l2 node\n", device);
@@ -1514,7 +1521,7 @@ int main(int argc, char **argv)
 
 	/* List options */
 
-	common_list(media_bus_info, c_fd);
+	common_list(c_fd);
 	io_list(c_fd);
 	stds_list(c_fd);
 	vidcap_list(c_fd);

utils/v4l2-ctl/v4l2-ctl-common.cpp
@@ -42,6 +42,8 @@ static ctrl_set_list set_ctrls;
 using dev_vec = std::vector<std::string>;
 using dev_map = std::map<std::string, std::string>;
 
+static std::string match_input, match_output;
+
 static enum v4l2_priority prio = V4L2_PRIORITY_UNSET;
 
 static bool have_query_ext_ctrl;
@@ -104,6 +106,12 @@ void common_usage()
 	       "  --list-devices     list all v4l devices. If -z was given, then list just the\n"
 	       "                     devices of the media device with the bus info string as\n"
 	       "                     specified by the -z option.\n"
+	       "  --list-devices-input <name>\n"
+	       "                     same as --list-devices, but only show devices with a current\n"
+	       "                     input name that matches <name>\n"
+	       "  --list-devices-output <name>\n"
+	       "                     same as --list-devices, but only show devices with a current\n"
+	       "                     output name that matches <name>\n"
 	       "  --log-status       log the board status in the kernel log [VIDIOC_LOG_STATUS]\n"
 	       "  --get-priority     query the current access priority [VIDIOC_G_PRIORITY]\n"
 	       "  --set-priority <prio>\n"
@@ -166,12 +174,14 @@ static bool sort_on_device_name(const std::string &s1, const std::string &s2)
 	return n1 < n2;
 }
 
-static void list_media_devices(const std::string &media_bus_info)
+static void list_media_devices(const std::string &media_bus_info,
+			       const std::string &input, const std::string &output)
 {
 	DIR *dp;
 	struct dirent *ep;
 	int media_fd = -1;
 	std::map<dev_t, std::string> devices;
+	bool match_io = !input.empty() || !output.empty();
 
 	dp = opendir("/dev");
 	if (dp == nullptr) {
@@ -200,7 +210,8 @@ static void list_media_devices(const std::string &media_bus_info)
 		if (!ioctl(fd, MEDIA_IOC_DEVICE_INFO, &mdi) &&
 		    media_bus_info == mdi.bus_info) {
 			media_fd = fd;
-			printf("%s\n", s.c_str());
+			if (!match_io)
+				printf("%s\n", s.c_str());
 		} else {
 			close(fd);
 		}
@@ -234,30 +245,39 @@ static void list_media_devices(const std::string &media_bus_info)
 			if (fd < 0)
 				continue;
 
-			if (verbose) {
+			bool match = !match_io;
+
+			if (verbose || match_io) {
 				unsigned idx;
 
 				if (!ioctl(fd, VIDIOC_G_INPUT, &idx)) {
 					struct v4l2_input in = {
 						.index = idx
 					};
-					if (!ioctl(fd, VIDIOC_ENUMINPUT, &in))
+					if (!ioctl(fd, VIDIOC_ENUMINPUT, &in)) {
 						extra = std::string(" (input: ") + (const char *)in.name + ")";
+						match = !match_io || !strcmp(match_input.c_str(), (const char *)in.name);
+					}
 				} else if (!ioctl(fd, VIDIOC_G_OUTPUT, &idx)) {
 					struct v4l2_output out = {
 						.index = idx
 					};
-					if (!ioctl(fd, VIDIOC_ENUMOUTPUT, &out))
+					if (!ioctl(fd, VIDIOC_ENUMOUTPUT, &out)) {
 						extra = std::string(" (output: ") + (const char *)out.name + ")";
+						match = !match_io || !strcmp(match_output.c_str(), (const char *)out.name);
+					}
 				}
+				if (!verbose)
+					extra.clear();
 			}
 			close(fd);
-			printf("%s%s\n", devices[dev].c_str(), extra.c_str());
+			if (match)
+				printf("%s%s\n", devices[dev].c_str(), extra.c_str());
 		}
 	close(media_fd);
 }
 
-static void list_devices()
+static void list_devices(const std::string &input, const std::string &output)
 {
 	DIR *dp;
 	struct dirent *ep;
@@ -265,6 +285,7 @@ static void list_devices()
 	dev_map links;
 	dev_map cards;
 	struct v4l2_capability vcap;
+	bool match_io = !input.empty() || !output.empty();
 
 	dp = opendir("/dev");
 	if (dp == nullptr) {
@@ -314,6 +335,7 @@ static void list_devices()
 		std::string bus_info;
 		std::string card;
 		std::string extra;
+		bool match = !match_io;
 
 		if (fd < 0)
 			continue;
@@ -341,30 +363,41 @@ static void list_devices()
 				struct v4l2_input in = {
 					.index = idx
 				};
-				if (!ioctl(fd, VIDIOC_ENUMINPUT, &in))
+				if (!ioctl(fd, VIDIOC_ENUMINPUT, &in)) {
 					extra = std::string(" (input: ") + (const char *)in.name + ")";
+					match = !match_io || !strcmp(match_input.c_str(), (const char *)in.name);
+				}
 			} else if (!ioctl(fd, VIDIOC_G_OUTPUT, &idx)) {
 				struct v4l2_output out = {
 					.index = idx
 				};
-				if (!ioctl(fd, VIDIOC_ENUMOUTPUT, &out))
+				if (!ioctl(fd, VIDIOC_ENUMOUTPUT, &out)) {
 					extra = std::string(" (output: ") + (const char *)out.name + ")";
+					match = !match_io || !strcmp(match_output.c_str(), (const char *)out.name);
+				}
 			}
 		}
 		close(fd);
 		if (err)
 			continue;
-		if (cards[bus_info].empty())
+		if (!match) continue;
+		if (cards[bus_info].empty() && !match_io)
 			cards[bus_info] += card + " (" + bus_info + "):\n";
-		cards[bus_info] += "\t" + file;
+		if (!match_io)
+			cards[bus_info] += "\t";
+		cards[bus_info] += file;
 		if (!(links[file].empty()))
 			cards[bus_info] += " <- " + links[file];
 		if (verbose)
 			cards[bus_info] += extra;
 		cards[bus_info] += "\n";
 	}
+	bool first = true;
 	for (const auto &card : cards) {
-		printf("%s\n", card.second.c_str());
+		if (!first && !match_io)
+			printf("\n");
+		first = false;
+		printf("%s", card.second.c_str());
 	}
 }
 
@@ -1129,6 +1162,12 @@ void common_cmd(int ch, char *optarg)
 	case OptSetPriority:
 		prio = static_cast<enum v4l2_priority>(strtoul(optarg, nullptr, 0));
 		break;
+	case OptListDevicesInput:
+		match_input = optarg;
+		break;
+	case OptListDevicesOutput:
+		match_output = optarg;
+		break;
 	}
 }
 
@@ -1395,14 +1434,21 @@ void common_get(cv4l_fd &_fd)
 	}
 }
 
-void common_list(const std::string &media_bus_info, cv4l_fd &fd)
+bool common_list_devices(const std::string &media_bus_info, cv4l_fd &fd)
 {
-	if (options[OptListDevices]) {
+	if (options[OptListDevices] || options[OptListDevicesInput] ||
+	    options[OptListDevicesOutput]) {
 		if (media_bus_info.empty())
-			list_devices();
+			list_devices(match_input, match_output);
 		else
-			list_media_devices(media_bus_info);
+			list_media_devices(media_bus_info,match_input, match_output);
+		return true;
 	}
+	return false;
+}
+
+void common_list(cv4l_fd &fd)
+{
 	if (options[OptListCtrls] || options[OptListCtrlsMenus]) {
 		list_controls(fd.g_fd(), options[OptListCtrlsMenus]);
 	}

utils/v4l2-ctl/v4l2-ctl.h
@@ -173,6 +173,8 @@ enum Option {
 	OptSetModulator,
 	OptListFreqBands,
 	OptListDevices,
+	OptListDevicesInput,
+	OptListDevicesOutput,
 	OptGetOutputParm,
 	OptSetOutputParm,
 	OptQueryStandard,
@@ -348,7 +350,8 @@ void common_usage(void);
 void common_cmd(int ch, char *optarg);
 void common_set(cv4l_fd &fd);
 void common_get(cv4l_fd &fd);
-void common_list(const std::string &media_bus_info, cv4l_fd &fd);
+bool common_list_devices(const std::string &media_bus_info, cv4l_fd &fd);
+void common_list(cv4l_fd &fd);
 void common_process_controls(cv4l_fd &fd);
 void common_control_event(int fd, const struct v4l2_event *ev);
 int common_find_ctrl_id(const char *name);

commit 0354746ec16404547bfcb83b4ea2d351ac70aeec

v4l2-ctl: move s_trace back to where it was before
A previous commit moved the c_fd.s_trace() call to before the
open, but that's wrong. It should come after the open, otherwise
you will get unexpected output like this:

$ v4l2-ctl -d2
                VIDIOC_G_SELECTION returned -1 (No data available)

Fixes: c78d63933445 ("v4l2-ctl: add --list-devices-input/output options")
Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>

utils/v4l2-ctl/v4l2-ctl.cpp
@@ -1347,8 +1347,6 @@ int main(int argc, char **argv)
 	c_out_fd.s_direct(!options[OptUseWrapper]);
 	c_exp_fd.s_direct(!options[OptUseWrapper]);
 
-	c_fd.s_trace(options[OptSilent] ? 0 : (verbose ? 2 : 1));
-
 	if (is_subdev)
 		fd = c_fd.subdev_open(device);
 	else
@@ -1359,6 +1357,8 @@ int main(int argc, char **argv)
 			strerror(errno));
 		std::exit(EXIT_FAILURE);
 	}
+
+	c_fd.s_trace(options[OptSilent] ? 0 : (verbose ? 2 : 1));
 
 	if (!is_subdev && doioctl(fd, VIDIOC_QUERYCAP, &vcap)) {
 		fprintf(stderr, "%s: not a v4l2 node\n", device);