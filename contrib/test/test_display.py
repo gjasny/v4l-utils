382fdfdb803cafc423efd583da5b3e3f9395af00
Hans Verkuil <hverkuil@xs4all.nl>
2024-12-10 11:05:23 +0100

    test_display.py: add log options, checked with linter
    Renamed to test_display.py ('-' in names is not recommended in
    python), fixed a lot of pylint issues, add support to set the
    top level log directory, or pass the log directory explicitly
    (i.e. do not generate the log directory name). Also add an option
    to skip creating the tar archive.
    
    Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>

--- contrib/test/test_display.py
+++ contrib/test/test_display.py
@@ -1,62 +1,73 @@
 #!/usr/bin/env python3
-#
-# SPDX-License-Identifier: GPL-2.0-only
-#
-# Copyright 2024 Cisco Systems, Inc. and/or its affiliates. All rights reserved.
-#
-# This script tests EDID and CEC compliance of a connected display.
-# The actual compliance tests are performed by the edid-decode and cec-ctl,
-# cec-compliance and cec-follower utilities, and this script is a convenient
-# frontend for that.
-#
-# The CEC tests must be performed with root permissions as that is needed to
-# run the monitor mode of cec-ctl: this will produce the monitor.log during
-# the CEC tests, which is very useful when debugging problems.
-#
-# The edid-compliance test can be used as follows:
-#
-#	test-display.py edid-compliance
-#
-#   will just list the available EDID files found in /sys/class/drm/, and you
-#   can select one and give it to edid-compliance as follows:
-#
-#	test-display.py edid-compliance --edid-file /sys/class/drm/card1-HDMI-A-1/edid
-#
-#   The results of the test are written in /tmp/test-display.
-#
-# The remainder of the tests are all CEC tests. You can list the available CEC
-# devices by running 'cec-ctl -A'.
-#
-# All CEC tests run with cec-follower and cec-ctl -M (monitoring CEC traffic)
-# in the background. This ensures that the CEC output acts like a standard
-# Playback device and that all CEC traffic is monitored, which is helpful
-# for debugging. The script assumes no other CEC applications are active
-# since that can interfere with the tests.
-#
-# - cec-compliance: this runs the cec-compliance test. It runs a subset of
-#   all the possible compliance tests, meant to test the core CEC commands
-#   and anything related to the basic display functionality.
-# - cec-stress: this runs cec-ctl --stress-test-standby-wakeup-cycle for
-#   10000 cycles.
-# - cec-stress-sleep: this runs cec-ctl --stress-test-standby-wakeup-cycle max-sleep=5
-#   for 10000 cycles.
-# - cec-stress-random: this runs cec-ctl --stress-test-random-standby-wakeup-cycle
-#   for 4000 cycles.
-#
-# These CEC stress tests do a very good job uncovering race conditions in the
-# display's firmware.
+"""
+SPDX-License-Identifier: GPL-2.0-only
+
+Copyright 2024 Cisco Systems, Inc. and/or its affiliates. All rights reserved.
+
+This script tests EDID and CEC compliance of a connected display.
+The actual compliance tests are performed by the edid-decode and cec-ctl,
+cec-compliance and cec-follower utilities, and this script is a convenient
+frontend for that.
+
+The CEC tests must be performed with root permissions as that is needed to
+run the monitor mode of cec-ctl: this will produce the monitor.log during
+the CEC tests, which is very useful when debugging problems.
+
+The edid-compliance test can be used as follows:
+
+  test_display.py edid-compliance
+
+  will just list the available EDID files found in /sys/class/drm/, and you
+  can select one and give it to edid-compliance as follows:
+
+  test_display.py edid-compliance --edid-file /sys/class/drm/card1-HDMI-A-1/edid
+
+  The results of the test are written in /tmp/test_display.
+
+The remainder of the tests are all CEC tests. You can list the available CEC
+devices by running 'cec-ctl -A'.
+
+All CEC tests run with cec-follower and cec-ctl -M (monitoring CEC traffic)
+in the background. This ensures that the CEC output acts like a standard
+Playback device and that all CEC traffic is monitored, which is helpful
+for debugging. The script assumes no other CEC applications are active
+since that can interfere with the tests.
+
+- cec-compliance: this runs the cec-compliance test. It runs a subset of
+  all the possible compliance tests, meant to test the core CEC commands
+  and anything related to the basic display functionality.
+- cec-stress: this runs cec-ctl --stress-test-standby-wakeup-cycle for
+  10000 cycles.
+- cec-stress-sleep: this runs cec-ctl --stress-test-standby-wakeup-cycle max-sleep=5
+  for 10000 cycles.
+- cec-stress-random: this runs cec-ctl --stress-test-random-standby-wakeup-cycle
+  for 4000 cycles.
+
+These CEC stress tests do a very good job uncovering race conditions in the
+display's firmware.
+"""
 import argparse
 import contextlib
 import datetime
 import logging
 import os
 import pathlib
-import re
 import sys
 import time
 from subprocess import run, PIPE, STDOUT, CalledProcessError, Popen
 
 log = logging.getLogger(__file__)
+
+class FollowerSetupError(Exception):
+    """Exception when follower setup fails."""
+
+
+class MonitorSetupError(Exception):
+    """Exception when monitor setup fails."""
+
+
+class EdidSetupError(Exception):
+    """Exception when DDC reader setup fails."""
 
 
 def run_cmd(cmd, file=None, shell=False):
@@ -71,10 +82,10 @@ def run_cmd(cmd, file=None, shell=False):
         str: Output from the cmd
     """
     cmd = cmd.strip()
-    log.debug(f"Running cmd: {cmd}\n")
+    log.debug("Running cmd: %s", cmd)
     try:
         if file:
-            stdout = open(file, "w")
+            stdout = open(file, "w", encoding = "utf-8")
             if not shell:
                 version = run_cmd("cec-compliance --version")
                 stdout.write(f"Running cmd: {cmd}\n\n")
@@ -84,17 +95,17 @@ def run_cmd(cmd, file=None, shell=False):
             stdout = PIPE
         cmd = cmd.split() if not shell else cmd
         output = run(args=cmd, stdout=stdout,
-                     stderr=STDOUT, shell=shell)
+                     stderr=STDOUT, shell=shell, check=False, encoding="utf-8")
         output.check_returncode()
     except CalledProcessError:
-        log.error(output.stdout.decode("utf-8"))
+        log.error(output.stdout)
         raise
     finally:
         if file:
             stdout.close()
-            return
-    log.debug(f"Output: {output.stdout.decode('utf-8')}")
-    return output.stdout.decode("utf-8")
+
+    log.debug("Output: %s", output.stdout)
+    return output.stdout
 
 
 @contextlib.contextmanager
@@ -106,9 +117,9 @@ def follower_context(device, logpath):
         logpath (str): path to logfile
     """
     follower_cmd = f"cec-follower -d{device} -i 0,0x36 -w -v"
-    log.info(f"Starting follower: {follower_cmd}")
-    fdesc = open(logpath, "w")
-    proc = Popen(follower_cmd.split(), stderr=fdesc, stdout=fdesc)
+    log.info("Starting follower: %s", follower_cmd)
+    fdesc = open(logpath, "w", encoding = "utf-8")
+    proc = Popen(follower_cmd.split(), stderr=fdesc, stdout=fdesc, encoding = "utf-8")
     try:
         if proc.poll():
             raise FollowerSetupError("Failed to set up follower")
@@ -130,15 +141,15 @@ def monitor_context(device, logpath, soak_time_exit):
         soak_time_exit (int): seconds to wait for msg after test
     """
     monitor_cmd = f"cec-ctl -d{device} -M -w -v"
-    log.info(f"Starting monitor: {monitor_cmd}")
-    fdesc = open(logpath, "w")
-    proc = Popen(monitor_cmd.split(), stderr=fdesc, stdout=fdesc)
+    log.info("Starting monitor: %s", monitor_cmd)
+    fdesc = open(logpath, "w", encoding = "utf-8")
+    proc = Popen(monitor_cmd.split(), stderr=fdesc, stdout=fdesc, encoding = "utf-8")
     try:
         if proc.poll():
             raise MonitorSetupError("Failed to set up monitor")
         yield
     finally:
-        log.info(f"Soaking monitor log for {soak_time_exit} seconds")
+        log.info("Soaking monitor log for %s seconds", soak_time_exit)
         try:
             time.sleep(soak_time_exit)
         finally:
@@ -157,12 +168,12 @@ def edid_context(device, duration, logpath):
         logpath (str): path to logfile
     """
     edid_cmd = f"edid-decode -a{device} --i2c-test-reliability duration={duration}"
-    log.info(f"Starting DDC reader: {edid_cmd}")
-    fdesc = open(logpath, "w")
-    proc = Popen(edid_cmd.split(), stderr=fdesc, stdout=fdesc)
+    log.info("Starting DDC reader: %s", edid_cmd)
+    fdesc = open(logpath, "w", encoding = "utf-8")
+    proc = Popen(edid_cmd.split(), stderr=fdesc, stdout=fdesc, encoding = "utf-8")
     try:
         if proc.poll():
-            raise MonitorSetupError("Failed to set up DDC reader")
+            raise EdidSetupError("Failed to set up DDC reader")
         yield
     finally:
         log.info("Terminating DDC reader")
@@ -179,7 +190,7 @@ def write_header(cmd, log_file):
     """
     version = run_cmd("cec-compliance --version")
     cmd = cmd.strip("\n")
-    with open(log_file, "a+") as f:
+    with open(log_file, "a+", encoding = "utf-8") as f:
         f.write(f"Running cmd: {cmd}\n\n")
         f.write(f"{version}\n")
         f.flush()
@@ -196,15 +207,15 @@ def execute_cmd(command, log_file):
         CalledProcessError: [description]
     """
 
-    log.info(f"Executing: {command}")
+    log.info("Executing: %s", command)
     write_header(command, log_file)
     print("")
 
-    with open(log_file, "a+") as f:
-        proc = Popen(command.split(), stdout=PIPE, stderr=STDOUT)
+    with open(log_file, "a+", encoding = "utf-8") as f:
+        proc = Popen(command.split(), stdout=PIPE, stderr=STDOUT, encoding = "utf-8")
         for line in proc.stdout:
-            f.write(line.decode("utf-8"))
-            print(line.decode("utf-8").strip("\n"))
+            f.write(line)
+            print(line.strip("\n"))
 
         proc.stdout.close()
         proc.wait()
@@ -222,56 +233,6 @@ def setup_cec_device(args):
         dev = "/dev/cec" + dev
     run_cmd(f"cec-ctl -d {dev} --playback -V {args.cec_vendor_id}")
     return dev
-
-
-def set_to_state(device, state, timeout=5):
-    """Send standby | image-view-on to display and assert power status.
-
-    This will ask for power status every second.
-
-    Args:
-        device (str): CEC device number from /dev/cec<device>
-        state (str): Set state to assert from give power status
-        timeout (int, optional): Set timeout for power status assertion. Defaults to 5.
-
-    Raises:
-        PowerStatusTimeout: Power status request timed out.
-    """
-    state = state.lower()
-    assert state in ["standby", "on"], "Invalid power state"
-    cmd = "--image-view-on" if state == "on" else "--standby"
-    run_cmd(f"cec-ctl -d{device} -t0 {cmd} -v -w")
-
-    start = time.time()
-    while (time.time() - start < timeout):
-        if state == power_status(device).lower():
-            return state
-        time.sleep(1)
-    else:
-        error_msg = f"Display never returned power state {state}"
-        raise PowerStatusTimeout(error_msg)
-
-
-def power_status(device):
-    """Check power status on display.
-
-    Args:
-        device (str): CEC device number from /dev/cec<device>
-
-    Returns:
-        str: Current power state
-    """
-    power_status_cmd = f"cec-ctl -d{device} -t0 --give-device-power-status -v -w"
-    power_status_pattern = r"pwr-state:\s(to-on|on|to-standby|standby)"
-    power_status_output = run_cmd(power_status_cmd)
-    match = re.search(power_status_pattern, power_status_output)
-
-    if match:
-        state = match.group(1).lower()
-        log.info(f"Display's power state is {state}")
-    else:
-        state = ""
-    return state
 
 
 def run_edid_test(args, log_dir = None):
@@ -304,13 +265,13 @@ def run_edid_test(args, log_dir = None):
             cmd = f"edid-decode -a {args.edid_ddc} --i2c-edid --check -p -n"
         run_cmd(cmd, std_log, shell=True)
     finally:
-        archive_files = os.path.relpath(std_log, log_dir.parent)
-        tar_cmd = f"tar -C {log_dir.parent} -czvf {log_dir}.tar.gz {archive_files}"
-        run_cmd(tar_cmd)
         out = run_cmd(f"cat {std_log}", shell=True)
         print(f"{out}")
         print(f"Logs directory: {log_dir}")
-        print(f"Archive file: {log_dir}.tar.gz\n")
+        if not args.no_tar:
+            tar_cmd = f"tar -C {log_dir.parent} -czvf {log_dir}.tar.gz {log_dir.name}"
+            run_cmd(tar_cmd)
+            print(f"Archive file: {log_dir}.tar.gz\n")
 
 def main(args):
     """Entry point for command line interface.
@@ -337,13 +298,16 @@ def main(args):
             time.sleep(15)  # wait for the display to wake up
             port = run_cmd(f"cec-ctl -d {device} -x -s").strip("\n")
             if port == 'f.f.f.f':
-                exit(f"The CEC device {device} has no physical address. Is a display connected?\n")
+                sys.exit(f"The CEC device {device} has no physical address. Is a display connected?\n")
 
     port = port.replace(".0.0.0", "")
     port = "-hdmi-" + port
     date = datetime.datetime.now().strftime("%Y.%m.%d-%H.%M.%S")
     folder_name = args.command + port + "-" + date
-    log_dir = pathlib.Path(f"{args.log_dir}/{folder_name}")
+    if args.log_dir:
+        log_dir = pathlib.Path(args.log_dir)
+    else:
+        log_dir = pathlib.Path(f"{args.top_log_dir}/tmp/test_display/{folder_name}")
     os.makedirs(log_dir)
 
     logging.basicConfig(
@@ -363,7 +327,7 @@ def main(args):
         return
 
     if os.geteuid() != 0:
-        exit("The CEC tests require root privileges, run this program with sudo.\n")
+        sys.exit("The CEC tests require root privileges, run this program with sudo.\n")
 
     if args.command == "cec-stress":
         std_log = os.path.join(log_dir, "cec-stress-stdout.log")
@@ -375,6 +339,8 @@ def main(args):
         std_log = os.path.join(log_dir, "cec-compliance-stdout.log")
     elif args.command == "cec-ddc-reliability":
         std_log = os.path.join(log_dir, "cec-ddc-reliability-stdout.log")
+    else:
+        sys.exit(f"Unknown command {args.command}")
 
     follower_log = os.path.join(log_dir, "follower.log")
     monitor_log = os.path.join(log_dir, "monitor.log")
@@ -403,7 +369,7 @@ def main(args):
             if args.command == "cec-ddc-reliability":
                 outer_stack.enter_context(edid_context(args.edid_ddc, args.duration, edid_log))
 
-            if cec_console:
+            if args.console:
                 # Desktop managers can get very confused by displays appearing and
                 # disappearing due to Hotplug Detect toggles that happen while going
                 # into and out of standby.
@@ -414,8 +380,10 @@ def main(args):
                 # almost indistinguishable to just black video). The console drm/kms
                 # code is much better at handling HPD toggles.
                 run_cmd('systemctl start multi-user.target', std_log, shell=True)
-                for i in range(150):
+                cnt = 0
+                while cnt < 150:
                     run_cmd('date +"<2> %D %T" >/dev/kmsg', std_log, shell=True)
+                    cnt = cnt + 1
 
             if args.command == "cec-stress":
                 command = (f"cec-ctl -d{device} -t0"
@@ -433,67 +401,62 @@ def main(args):
                 execute_cmd(command, std_log)
 
     finally:
-        if cec_console:
+        if args.console:
             run_cmd('systemctl start graphical.target', std_log, shell=True)
 
-        archive_files = [
-            os.path.basename(monitor_log),
-            os.path.basename(follower_log),
-            os.path.basename(std_log), ]
-        if args.command == "cec-ddc-reliability":
-            archive_files.append(os.path.basename(edid_log))
-
-        archive_files = [os.path.join(folder_name, f) for f in archive_files]
-        archive_files = " ".join(archive_files)
-        tar_cmd = f"tar -C {log_dir.parent} -czvf {log_dir}.tar.gz {archive_files}"
-
-        run_cmd(tar_cmd)
         print(f"\nLogs directory: {log_dir}")
-        print(f"Archive file: {log_dir}.tar.gz\n")
+        if not args.no_tar:
+            tar_cmd = f"tar -C {log_dir.parent} -czvf {log_dir}.tar.gz {log_dir.name}"
+            run_cmd(tar_cmd)
+            print(f"Archive file: {log_dir}.tar.gz\n")
 
 
-def add_cec_args(subparser, soak_time, have_console):
+def add_cec_args(cecparser, soak_time, have_console):
     """Add standard CEC options to subparser.
 
     Args:
-        subparser (argparse): CEC subparser
+        cecparser (argparse): CEC subparser
         soak_time (int): CEC monitor soak time in seconds
         have_console (bool): if true, add --console option
     """
 
-    subparser.add_argument("-d", "--cec-device",
+    cecparser.add_argument("-d", "--cec-device",
                            dest="cec_device",
                            type=str,
                            default="/dev/cec0",
                            help="/dev/cecX device for the output connected to the display, default is /dev/cec0")
-    subparser.add_argument("-V", "--vendor-id",
+    cecparser.add_argument("-V", "--vendor-id",
                            dest="cec_vendor_id",
                            type=str,
                            default='0x000c03',
                            help="CEC Vendor ID to use, default is 0x000c03")
-    subparser.add_argument("-s", "--log-soak-time",
+    cecparser.add_argument("-s", "--log-soak-time",
 			   metavar="WAIT",
 			   type=int,
 			   default=soak_time,
 			   help=f"Seconds to wait before closing the monitor log, default is {soak_time}s")
     if have_console:
-        subparser.add_argument("-C", "--console",
-                               dest="cec_console",
+        cecparser.add_argument("-C", "--console",
                                action='store_true',
                                help="Switch to console mode before running CEC test")
 
 
 if __name__ == "__main__":
-    cec_console = False
     parser = argparse.ArgumentParser(
         description="Display Test",
         formatter_class=argparse.RawTextHelpFormatter)
 
-    parser.add_argument("--log-dir",
-                        dest="log_dir",
+    parser.add_argument("--top-log-dir",
                         type=str,
-                        default='/tmp/test-display',
-                        help="Name of the log directory that is created, default is '/tmp/test-display'")
+                        default='/tmp/test_display',
+                        help="Name of the top log directory that is created, default is '/tmp/test_display'")
+    parser.add_argument("--log-dir",
+                        type=str,
+                        default=None,
+                        help="All logs are created in this directory. Overrides --top-log-dir")
+    parser.add_argument("--no-tar",
+			action='store_true',
+			help="Do not create the tar archive for the results")
 
     subparser = parser.add_subparsers(help=" ", dest="command")
 
