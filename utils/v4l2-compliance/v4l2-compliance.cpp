gjasny/v4l-utils

commit 60e0120f0ef55d6599ea1c11e151a78625254bf6

v4l2-compliance: Don't perform blocking DQBUF test on MC_IO devices
Streaming on MC_IO devices can't be tested by v4l2-compliance as it
requires setting up a valid pipeline first. The blocking DQBUF test
therefore fails to start streaming on those devices. Disable it.

Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
Reviewed-by: Sakari Ailus <sakari.ailus@linux.intel.com>
Reviewed-by: Hans Verkuil <hverkuil@xs4all.nl>

utils/v4l2-compliance/v4l2-test-buffers.cpp
@@ -3119,7 +3119,7 @@ int testBlockingWait(struct node *node)
 	bool can_stream = node->g_caps() & V4L2_CAP_STREAMING;
 	int type;
 
-	if (!can_stream || !node->valid_buftypes)
+	if (!can_stream || node->is_io_mc || !node->valid_buftypes)
 		return ENOTTY;
 
 	buffer_info.clear();

commit dc51d9daa9ff994bc8fc33ab88a331f5303a15f2

v4l2-compliance: run testBlockingWait when streaming
This fixes the previous commit which would skip the test for all
media-centric devices. Instead, only skip for media-centric devices
when not streaming.

Fixes: 60e0120f0ef5 ("v4l2-compliance: Don't perform blocking DQBUF test on MC_IO devices")
Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>

utils/v4l2-compliance/v4l2-compliance.cpp
@@ -1495,7 +1495,9 @@ void testNode(struct node &node, struct node &node_m2m_cap, struct node &expbuf_
 		printf("\ttest Requests: %s\n", ok(testRequests(&node, options[OptStreaming])));
 		if (sizeof(void *) == 4)
 			printf("\ttest TIME32/64: %s\n", ok(testTime32_64(&node)));
-		printf("\ttest blocking wait: %s\n", ok(testBlockingWait(&node)));
+		// If this is a media-centric device, then only run when streaming
+		if (!node.is_io_mc || options[OptStreaming])
+			printf("\ttest blocking wait: %s\n", ok(testBlockingWait(&node)));
 
 		// Reopen after each streaming test to reset the streaming state
 		// in case of any errors in the preceeding test.

utils/v4l2-compliance/v4l2-test-buffers.cpp
@@ -3119,7 +3119,7 @@ int testBlockingWait(struct node *node)
 	bool can_stream = node->g_caps() & V4L2_CAP_STREAMING;
 	int type;
 
-	if (!can_stream || node->is_io_mc || !node->valid_buftypes)
+	if (!can_stream || !node->valid_buftypes)
 		return ENOTTY;
 
 	buffer_info.clear();