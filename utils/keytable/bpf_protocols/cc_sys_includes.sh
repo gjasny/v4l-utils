48316b8a20aac35f982991b617f84fb3c104e7b0
James Le Cuirot <chewi@gentoo.org>
2025-08-10 15:20:06 +0100

    meson: Allow BPF code to be built with GCC
    GCC can also target BPF, but it does not understand the "-target bpf"
    arguments passed to Clang.
    
    Detect it as either bpf-gcc, bpf-none-gcc, or bpf-unknown-none-gcc, the
    same as systemd does.
    
    Determine the include paths with the compiler used by the rest of the
    build rather than Clang, which might not be installed or might not give
    the right answer, especially when cross-compiling.
    
    Check whether Clang actually supports the BPF target so that
    auto-detection doesn't cause the build to fail when it doesn't.
    
    Signed-off-by: James Le Cuirot <chewi@gentoo.org>
    Signed-off-by: Sean Young <sean@mess.org>

---
+++ utils/keytable/bpf_protocols/cc_sys_includes.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# Get C compiler's default includes on this system, as the BPF toolchain
+# generally doesn't see the Linux headers. This fixes "missing" files on some
+# architectures/distros, such as asm/byteorder.h, asm/socket.h, asm/sockios.h,
+# sys/cdefs.h etc.
+#
+# Use '-idirafter': Don't interfere with include mechanics except where the
+# build would have failed anyways.
+"$@" -v -E - </dev/null 2>&1 \
+	| sed -n '/<...> search starts here:/,/End of search list./{ s| \(/.*\)|-idirafter \1|p }'
